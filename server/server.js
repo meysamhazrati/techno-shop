319b5872e7d1a8d541d61bbde0def38753b8369c
iFelani <iMeysamHazrati@gmail.com>
2024-01-06 21:51:29 +0330

    Fix app log

--- server/server.js
+++ server/server.js
@@ -16,4 +16,4 @@ const port = process.env.PORT || 3000;
   }
 })();
 
-app.listen(port, () => console.log(`The server is running on port ${3000}.`));
+app.listen(port, () => console.log(`The server is running on port ${port}.`));
