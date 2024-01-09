7d6829b705157e21a6e5cdba4448b36b6c324262
iFelani <iMeysamHazrati@gmail.com>
2024-01-09 21:04:04 +0330

    Add public directory

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
-import express, { json, urlencoded } from "express";
+import express, { json, urlencoded, static as static_ } from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 import cors from "./middlewares/cors.js";
 import notFoundHandler from "./middlewares/notFound.js";
@@ -9,6 +11,8 @@ const app = express();
 app.use(cors);
 app.use(json());
 app.use(urlencoded({ extended: true }));
+
+app.use(static_(join(dirname(fileURLToPath(import.meta.url)), "public")));
 
 app.use(notFoundHandler);
 app.use(errorHandler);
