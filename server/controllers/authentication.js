6d2c5a94dd16b20c0d38fc725a64cc4e1669a4ee
iFelani <iMeysamHazrati@gmail.com>
2024-01-11 22:40:54 +0330

    Update authentication controller

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -10,33 +10,39 @@ const register = async (request, response, next) => {
 
     const { firstName, lastName, phone, email, password } = request.body;
 
-    const isUserExists = await userModel.findOne({ phone }).lean();
+    const isExists = await userModel.findOne({ $or: [{ phone }, { email }] }).lean();
 
-    if (isUserExists) {
-      response.status(409).json({ message: "This phone already exists." });
+    if (isExists) {
+      response.status(409).json({ message: "This phone or email already exists." });
     } else {
-      const usersCount = await userModel.countDocuments();
-      const hashedPassword = await hash(password, 12);
+      const isBanned = await bannedUserModel.findOne({ $or: [{ phone }, { email }] }).lean();
 
-      const { _id: id } = await userModel.create({
-        firstName,
-        lastName,
-        phone,
-        email,
-        password: hashedPassword,
-        role: usersCount ? "USER" : "ADMIN",
-      });
+      if (isBanned) {
+        response.status(403).json({ message: "This phone number or email has been banned." });
+      } else {
+        const usersCount = await userModel.countDocuments();
+        const hashedPassword = await hash(password, 12);
 
-      const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30 day" });
+        const { _id: id } = await userModel.create({
+          firstName,
+          lastName,
+          phone,
+          email,
+          password: hashedPassword,
+          role: usersCount ? "USER" : "ADMIN",
+        });
 
-      response
-        .cookie("token", token, {
-          path: "/",
-          maxAge: 1000 * 60 * 60 * 24 * 30,
-          httpOnly: true,
-        })
-        .status(201)
-        .json({ message: "You have successfully registered." });
+        const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+        response
+          .cookie("token", token, {
+            path: "/",
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+            httpOnly: true,
+          })
+          .status(201)
+          .json({ message: "You have successfully registered." });
+      }
     }
   } catch (error) {
     error.status = 400;
@@ -55,17 +61,15 @@ const login = async (request, response, next) => {
     const user = await userModel.findOne({ $or: [{ phone: identifier }, { email: identifier }] }).lean();
 
     if (user) {
-      const { _id: id, password: userPassword } = user;
+      const { _id: id, password: userPassword, isBanned } = user;
 
-      const isUserBanned = await bannedUserModel.findOne({ $or: [{ phone: identifier }, { email: identifier }] }).lean();
-
-      if (isUserBanned) {
+      if (isBanned) {
         response.status(403).json({ message: "This phone number or email has been banned." });
       } else {
         const isPasswordValid = await compare(password, userPassword);
 
         if (isPasswordValid) {
-          const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30 day" });
+          const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 
           response
             .cookie("token", token, {
@@ -76,7 +80,7 @@ const login = async (request, response, next) => {
             .status(201)
             .json({ message: "You have successfully logged in." });
         } else {
-          response.status(403).json({ message: "The password is incorrect." });
+          response.status(401).json({ message: "The password is incorrect." });
         }
       }
     } else {
