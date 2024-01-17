52be62849cd851095002c8e00d144b3f5b6b3491
iFelani <iMeysamHazrati@gmail.com>
2024-01-17 21:25:49 +0330

    Add mobile validations

--- server/models/mobile.js
+++ server/models/mobile.js
@@ -1,6 +1,7 @@
 import { Schema } from "mongoose";
 
 import productModel from "./product.js";
+import validator from "../validators/mobile.js";
 
 const schema = new Schema(
   {
@@ -166,5 +167,7 @@ const schema = new Schema(
   },
   { timestamps: true }
 );
+
+schema.statics.validation = (body) => validator.validate(body);
 
 export default productModel.discriminator("Mobile", schema);
