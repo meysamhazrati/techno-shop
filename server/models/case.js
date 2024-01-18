9aa5853fcb6a693f26db8a806b73ceaf14aa7dab
iFelani <iMeysamHazrati@gmail.com>
2024-01-18 23:50:44 +0330

    Create case model

---
+++ server/models/case.js
@@ -0,0 +1,120 @@
+import { Schema } from "mongoose";
+
+import productModel from "./product.js";
+
+const schema = new Schema(
+  {
+    length: {
+      type: Number,
+      required: true,
+      min: 300,
+      max: 700,
+    },
+    width: {
+      type: Number,
+      required: true,
+      min: 100,
+      max: 500,
+    },
+    height: {
+      type: Number,
+      required: true,
+      min: 300,
+      max: 800,
+    },
+    weight: {
+      type: Number,
+      required: true,
+      min: 4000,
+      max: 30000,
+    },
+    motherboard: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    CPU: {
+      type: String,
+      required: true,
+      enum: ["Intel", "AMD"],
+    },
+    CPUSeries: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 50,
+    },
+    CPUGeneration: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+    CPUFrequency: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10,
+    },
+    CPUCore: {
+      type: Number,
+      required: true,
+      min: 2,
+      max: 20,
+    },
+    GPU: {
+      type: String,
+      required: true,
+      enum: ["Intel", "AMD", "NVIDIA"],
+    },
+    GPUModel: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+    VRAM: {
+      type: Number,
+      required: true,
+      min: 2,
+      max: 30,
+    },
+    operatingSystem: {
+      type: String,
+      required: true,
+      enum: ["Windows", "بدون سیستم عامل"],
+    },
+    operatingSystemVersion: {
+      type: Number,
+      required: false,
+      enum: [10, 11],
+    },
+    internalMemory: [
+      {
+        type: Number,
+        enum: [128, 256, 512, 1024, 2048],
+      },
+    ],
+    internalMemoryType: {
+      type: String,
+      required: true,
+      enum: ["HDD", "SSD"],
+    },
+    RAM: [
+      {
+        type: Number,
+        min: 2,
+        max: 128,
+      },
+    ],
+    RAMType: {
+      type: String,
+      required: true,
+      enum: ["DDR3", "DDR4", "DDR5"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default productModel.discriminator("Case", schema);
