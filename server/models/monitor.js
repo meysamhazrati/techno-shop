b72191eb4fcc8c61d34b6879e887285c803eca69
iFelani <iMeysamHazrati@gmail.com>
2024-01-18 17:05:39 +0330

    Create monitor model

---
+++ server/models/monitor.js
@@ -0,0 +1,71 @@
+import { Schema } from "mongoose";
+
+import productModel from "./product.js";
+
+const schema = new Schema(
+  {
+    weight: {
+      type: Number,
+      required: true,
+      min: 1000,
+      max: 50000,
+    },
+    screen: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    screenSize: {
+      type: Number,
+      required: true,
+      min: 10,
+      max: 60,
+    },
+    resolutionWidth: {
+      type: Number,
+      required: true,
+      min: 500,
+      max: 10000,
+    },
+    resolutionHeight: {
+      type: Number,
+      required: true,
+      min: 500,
+      max: 10000,
+    },
+    minimumResponseTime: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10,
+    },
+    maximumResponseTime: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10,
+    },
+    panel: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    backlight: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    colors: {
+      type: Number,
+      required: true,
+      min: 1000000,
+      max: 1000000000,
+    },
+  },
+  { timestamps: true }
+);
+
+export default productModel.discriminator("Monitor", schema);
