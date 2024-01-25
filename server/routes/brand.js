bcee21875dcc3399ecf7a12ad0961edc6034af87
iFelani <iMeysamHazrati@gmail.com>
2024-01-25 13:43:59 +0330

    Update brand routes

--- server/routes/brand.js
+++ server/routes/brand.js
@@ -1,28 +1,14 @@
 import { Router } from "express";
-import multer, { diskStorage } from "multer";
-import { extname } from "path";
 
+import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
 import { create, getAll, update, remove } from "../controllers/brand.js";
 
 const router = Router();
-const uploader = multer({
-  storage: diskStorage({
-    destination: (request, file, callback) => callback(null, "public/brands/"),
-    filename: (request, file, callback) => callback(null, Date.now() + extname(file.originalname)),
-  }),
-  limits: { fileSize: 20 * 1024 * 1024 },
-  fileFilter: (request, file, callback) => {
-    if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
-      callback(null, true);
-    } else {
-      callback(new Error("The file extension must be png, jpg or jpeg."), false);
-    }
-  },
-});
+const uploader = multer("brands");
 
-router.route("/").post(uploader.single("logo"), authentication, isAdmin, create).get(getAll);
-router.route("/:id").put(uploader.single("logo"), authentication, isAdmin, update).delete(authentication, isAdmin, remove);
+router.route("/").post(authentication, isAdmin, uploader.single("logo"), create).get(getAll);
+router.route("/:id").put(authentication, isAdmin, uploader.single("logo"), update).delete(authentication, isAdmin, remove);
 
 export default router;
