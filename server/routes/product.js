9f6e192d8106c63d431cd4aad6b49a9cc0245512
iFelani <iMeysamHazrati@gmail.com>
2024-01-25 12:56:19 +0330

    Update product routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,28 +1,15 @@
 import { Router } from "express";
-import multer, { diskStorage } from "multer";
-import { extname } from "path";
 
+import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, getAll, get, update, remove } from "../controllers/product.js";
+import { create, getAll, get, getByCategory, update, remove } from "../controllers/product.js";
 
 const router = Router();
-const uploader = multer({
-  storage: diskStorage({
-    destination: (request, file, callback) => callback(null, "public/products/"),
-    filename: (request, file, callback) => callback(null, Date.now() + Math.floor(Math.random() * 1000000000) + extname(file.originalname)),
-  }),
-  limits: { fileSize: 20 * 1024 * 1024 },
-  fileFilter: (request, file, callback) => {
-    if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
-      callback(null, true);
-    } else {
-      callback(new Error("The file extension must be png, jpg or jpeg."), false);
-    }
-  },
-});
+const uploader = multer("products");
 
-router.route("/").post(uploader.array("covers", 4), authentication, isAdmin, create).get(getAll);
+router.route("/").post(authentication, isAdmin, uploader.array("covers", 4), create).get(getAll);
 router.route("/:id").get(get).put(authentication, isAdmin, update).delete(authentication, isAdmin, remove);
+router.get("/categories/:id", getByCategory);
 
 export default router;
