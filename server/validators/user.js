8d1806ca571ef582de763772de0be8d716a05258
iFelani <iMeysamHazrati@gmail.com>
2024-01-25 10:53:33 +0330

    Update user validations

--- server/validators/user.js
+++ server/validators/user.js
@@ -1,14 +1,10 @@
-import { object, string, number } from "yup";
+import { object, string } from "yup";
 
 const edit = object({
   firstName: string().required().min(3).max(70),
   lastName: string().required().min(4).max(70),
   currentPassword: string().min(8).max(20).matches(/^[\w?!$._-]{8,20}$/),
   newPassword: string().min(8).max(20).matches(/^[\w?!$._-]{8,20}$/),
-  avatar: object({
-    mimetype: string().oneOf(["image/png", "image/jpg", "image/jpeg"]),
-    size: number().max(20 * 1024 * 1024),
-  }),
 });
 
 const update = object({
@@ -18,10 +14,6 @@ const update = object({
   email: string().required().min(10).max(100).matches(/^\w+([.-]?\w)*@\w+([.-]?\w)*\.[a-zA-Z]{2,4}$/),
   password: string().min(8).max(20).matches(/^[\w?!$._-]{8,20}$/),
   role: string().required().oneOf(["USER", "ADMIN"]),
-  avatar: object({
-    mimetype: string().oneOf(["image/png", "image/jpg", "image/jpeg"]),
-    size: number().max(20 * 1024 * 1024),
-  }),
 });
 
 export { edit, update };
