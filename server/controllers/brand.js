47c9f11f2006a1ef00c0acf8eafdb5bc59e050d9
iFelani <iMeysamHazrati@gmail.com>
2024-01-25 13:41:11 +0330

    Update brand controller

--- server/controllers/brand.js
+++ server/controllers/brand.js
@@ -5,18 +5,15 @@ import brandModel from "../models/brand.js";
 const create = async (request, response, next) => {
   try {
     const logo = request.file;
-    await brandModel.validation({ ...request.body, logo });
+    await brandModel.createValidation({ ...request.body, logo });
 
     const { name, englishName } = request.body;
 
-    await brandModel.create({ name, englishName, logo: logo?.filename });
+    await brandModel.create({ name, englishName, logo: logo.filename });
 
     response.status(201).json({ message: "The brand has been successfully added." });
   } catch (error) {
-    error.status = 400;
-    error.message = "The entered information is not valid.";
-
-    request.file && unlink(request.file.path, (error) => error && next(error));
+    request.file && unlink(request.file.path, (error) => error && console.error(error));
 
     next(error);
   }
@@ -26,7 +23,11 @@ const getAll = async (request, response, next) => {
   try {
     const brands = await brandModel.find({}, "-__v").lean();
 
-    response.status(brands.length ? 200 : 404).json(brands.length ? brands : { message: "No brand found." });
+    if (brands.length) {
+      response.json(brands);
+    } else {
+      throw Object.assign(new Error("No brand found."), { status: 404 });
+    }
   } catch (error) {
     next(error);
   }
@@ -34,26 +35,23 @@ const getAll = async (request, response, next) => {
 
 const update = async (request, response, next) => {
   try {
-    const logo = request.file;
-    await brandModel.validation({ ...request.body, logo });
+    await brandModel.updateValidation(request.body);
 
     const { id } = request.params;
+    const logo = request.file;
     const { name, englishName } = request.body;
 
     const result = await brandModel.findByIdAndUpdate(id, { name, englishName, logo: logo?.filename });
 
-    if(result) {
-      unlink(`public/brands/${result.logo}`, (error) => error && next(error));
-    } else {
-      request.file && unlink(request.file.path, (error) => error && next(error));
-    }
-    
-    response.status(result ? 200 : 404).json({ message: result ? "The brand has been successfully edited." : "The brand was not found." });
-  } catch (error) {
-    error.status = 400;
-    error.message = "The entered information is not valid.";
+    if (result) {
+      logo && unlink(`public/brands/${result.logo}`, (error) => error && console.error(error));
 
-    request.file && unlink(request.file.path, (error) => error && next(error));
+      response.json({ message: "The brand has been successfully edited." });
+    } else {
+      throw Object.assign(new Error("The brand was not found."), { status: 404 });
+    }
+  } catch (error) {
+    request.file && unlink(request.file.path, (error) => error && console.error(error));
 
     next(error);
   }
@@ -65,9 +63,13 @@ const remove = async (request, response, next) => {
 
     const result = await brandModel.findByIdAndDelete(id);
 
-    result && unlink(`public/brands/${result.logo}`, (error) => error && next(error));
+    if (result) {
+      unlink(`public/brands/${result.logo}`, (error) => error && console.error(error));
 
-    response.status(result ? 200 : 404).json({ message: result ? "The brand has been successfully removed." : "The brand was not found." });
+      response.json({ message: "The brand has been successfully removed." });
+    } else {
+      throw Object.assign(new Error("The brand was not found."), { status: 404 });
+    }
   } catch (error) {
     next(error);
   }
