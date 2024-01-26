7ead1a03bf10005151e48c85aa3f0aa1e20d7ed9
iFelani <iMeysamHazrati@gmail.com>
2024-01-26 19:25:51 +0330

    Update authentication routes

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import authentication from "../middlewares/authentication.js";
-import { register, login, me } from "../controllers/authentication.js";
+import { send, verify, register, login, logout, me } from "../controllers/authentication.js";
 
 const router = Router();
 
+router.post("/send", send);
+router.post("/verify", verify);
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", authentication, me);
+router.delete("/logout", authentication, logout);
 
 export default router;
