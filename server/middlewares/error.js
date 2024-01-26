e3072f8d849b18f4ec169a2c10ca074f210124ba
iFelani <iMeysamHazrati@gmail.com>
2024-01-26 21:11:07 +0330

    Update error handling middleware

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,6 +1,31 @@
+import jwt from "jsonwebtoken";
+import { MulterError } from "multer";
+import { ValidationError } from "yup";
+
 const middleware = (error, request, response, next) => {
-  const { status = error.code === "LIMIT_FILE_SIZE" ? 413 : error.name === "ValidationError" ? 400 : 500, message = "Unexpected error." } = error;
-  
+  let { status = 500, message = "Unexpected error." } = error;
+
+  if (error instanceof jwt.JsonWebTokenError) {
+    status = 401;
+
+    if (error instanceof jwt.TokenExpiredError) {
+      message = "The token has expired.";
+    } else {
+      message = "The token is invalid.";
+    }
+  } else if (error instanceof MulterError) {
+    status = 400;
+
+    if (error.code === "LIMIT_FILE_SIZE") {
+      status = 413;
+      message = "The size of uploaded files exceeds the allowed limit.";
+    } else if (error.code === "LIMIT_FILE_COUNT") {
+      message = "The number of uploaded files exceeds the allowed limit.";
+    }
+  } else if (error instanceof ValidationError) {
+    status = 400;
+  }
+
   response.status(status).json({ message });
 };
 
