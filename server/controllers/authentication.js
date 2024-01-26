bad15c9fab5b2536208055342a0665305af922a6
iFelani <iMeysamHazrati@gmail.com>
2024-01-26 19:17:49 +0330

    Update authentication controller

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -2,7 +2,120 @@ import jwt from "jsonwebtoken";
 import { hash, compare } from "bcrypt";
 
 import userModel from "../models/user.js";
-import bannedUserModel from "../models/bannedUser.js";
+import otpModel from "../models/otp.js";
+import mailer from "../utilities/mailer.js";
+
+const send = async (request, response, next) => {
+  try {
+    await otpModel.sendValidation(request.body);
+
+    const { email } = request.body;
+
+    const isExists = await userModel.findOne({ email });
+
+    if (isExists) {
+      throw Object.assign(new Error("This email already exists."), { status: 409 });
+    } else {
+      const isBlocked = await otpModel.findOne({ email, tries: 3 });
+
+      if (isBlocked) {
+        throw Object.assign(new Error("This email is blocked for a few hours."), { status: 403 });
+      } else {
+        const activeOTP = await otpModel.findOne({ email, expiresAt: { $gt: new Date() } });
+
+        let code, expiresAt = null;
+
+        if (activeOTP) {
+          ({ code, expiresAt } = activeOTP);
+        } else {
+          await otpModel.deleteOne({ email });
+
+          ({ code, expiresAt } = await otpModel.create({ email }));
+        }
+
+        const transporter = mailer();
+
+        transporter.sendMail({
+            to: email,
+            subject: "کد تایید شما برای ثبت نام در تکنوشاپ",
+            html: `
+              <table style="max-width: 40rem; color: #3c3f44; font-size: 1rem; font-family: Vazir, Segoe UI; padding: 0 1rem; margin: 0 auto; direction: rtl;">
+                <tbody>
+                  <tr>
+                    <td>
+                      <p>کد تایید شما:</p>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h1 style="text-align: center; margin: 1rem 0">
+                        <code style="background-color: #d6d8db; padding: 0.3rem 1rem; border-radius: 0.7rem;">${code}</code>
+                      </h1>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p>این کد تا تاریخ <strong>${expiresAt.toLocaleDateString("fa-IR")}</strong> ساعت <strong>${expiresAt.toLocaleTimeString("fa-IR")}</strong> اعتبار دارد.</p>
+                    </td>
+                  </tr>
+                  <tfoot>
+                    <tr>
+                      <td>
+                        <span style="text-align: center; padding-top: 1rem; margin-top: 1rem; border-top: 1px solid #d6d8db; display: block;">&copy; فروشگاه اینترنتی <a href="https://github.com/meysamhazrati/techno-shop" target="_blank" style="color: #0279d8;">تکنوشاپ</a></span>
+                      </td>
+                    </tr>
+                  </tfoot>
+                </tbody>
+              </table>
+          `},
+          (error, info) => {
+            if (error) {
+              throw error;
+            } else {
+              response.json({ message: "The code was sent successfully." });
+            }
+          }
+        );
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const verify = async (request, response, next) => {
+  try {
+    await otpModel.verifyValidation(request.body);
+
+    const { email, code } = request.body;
+
+    const otp = await otpModel.findOne({ email });
+
+    if (otp) {
+      if (otp.tries < 3) {
+        if (otp.expiresAt > new Date()) {
+          if (otp.code === code) {
+            await otpModel.deleteOne({ email, code });
+
+            response.json({ message: "Your email has been successfully verified." });
+          } else {
+            await otpModel.findByIdAndUpdate(otp._id, { $inc: { tries: 1 } });
+
+            throw Object.assign(new Error("The entered code is incorrect."), { status: 401 });
+          }
+        } else {
+          throw Object.assign(new Error("The entered code has expired."), { status: 410 });
+        }
+      } else {
+        throw Object.assign(new Error("Too many incorrect attempts have been made to verify this email."), { status: 429 });
+      }
+    } else {
+      throw Object.assign(new Error("No code has been sent for this email."), { status: 409 });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const register = async (request, response, next) => {
   try {
@@ -10,44 +123,33 @@ const register = async (request, response, next) => {
 
     const { firstName, lastName, phone, email, password } = request.body;
 
-    const isExists = await userModel.findOne({ $or: [{ phone }, { email }] }).lean();
+    const isExists = await userModel.findOne({ $or: [{ phone }, { email }] });
 
     if (isExists) {
-      response.status(409).json({ message: "This phone or email already exists." });
+      throw Object.assign(new Error("This phone or email already exists."), { status: 409 });
     } else {
-      const isBanned = await bannedUserModel.findOne({ $or: [{ phone }, { email }] }).lean();
+      const usersCount = await userModel.countDocuments();
 
-      if (isBanned) {
-        response.status(403).json({ message: "This phone number or email has been banned." });
-      } else {
-        const usersCount = await userModel.countDocuments();
-        const hashedPassword = await hash(password, 12);
+      const hashedPassword = await hash(password, 12);
 
-        const { _id: id } = await userModel.create({
-          firstName,
-          lastName,
-          phone,
-          email,
-          password: hashedPassword,
-          role: usersCount ? "USER" : "ADMIN",
-        });
+      const { _id: id } = await userModel.create({
+        firstName,
+        lastName,
+        phone,
+        email,
+        password: hashedPassword,
+        role: usersCount ? "USER" : "ADMIN",
+      });
 
-        const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+      const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-        response
-          .cookie("token", token, {
-            path: "/",
-            maxAge: 1000 * 60 * 60 * 24 * 30,
-            httpOnly: true,
-          })
-          .status(201)
-          .json({ message: "You have successfully registered." });
-      }
+      response.cookie("token", token, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+      }).status(201).json({ message: "You have successfully registered." });
     }
   } catch (error) {
-    error.status = 400;
-    error.message = "The entered information is not valid.";
-
     next(error);
   }
 };
@@ -58,42 +160,38 @@ const login = async (request, response, next) => {
 
     const { identifier, password } = request.body;
 
-    const user = await userModel.findOne({ $or: [{ phone: identifier }, { email: identifier }] }).lean();
+    const user = await userModel.findOne({ $or: [{ phone: identifier }, { email: identifier }] });
 
     if (user) {
       const { _id: id, password: userPassword, isBanned } = user;
 
       if (isBanned) {
-        response.status(403).json({ message: "This phone number or email has been banned." });
+        throw Object.assign(new Error("This phone number or email has been banned."), { status: 403 });
       } else {
         const isPasswordValid = await compare(password, userPassword);
 
         if (isPasswordValid) {
-          const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+          const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-          response
-            .cookie("token", token, {
-              path: "/",
-              maxAge: 1000 * 60 * 60 * 24 * 30,
-              httpOnly: true,
-            })
-            .status(201)
-            .json({ message: "You have successfully logged in." });
+          response.cookie("token", token, {
+            path: "/",
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            httpOnly: true,
+          }).json({ message: "You have successfully logged in." });
         } else {
-          response.status(401).json({ message: "The password is incorrect." });
+          throw Object.assign(new Error("The entered password is incorrect."), { status: 401 });
         }
       }
     } else {
-      response.status(401).json({ message: "The phone number or email is incorrect." });
+      throw Object.assign(new Error("The entered phone number or email is incorrect."), { status: 401 });
     }
   } catch (error) {
-    error.status = 400;
-    error.message = "The entered information is not valid.";
-
     next(error);
   }
 };
 
+const logout = async (request, response, next) => response.cookie("token", "", { path: "/", maxAge: 0 }).json({ message: "You have successfully logged out." });
+
 const me = async (request, response) => response.json(request.user);
 
-export { register, login, me };
+export { send, verify, register, login, logout, me };
