f148204e3010d803a77d522d000aedd6a3ece5e2
iFelani <iMeysamHazrati@gmail.com>
2024-01-28 17:12:49 +0330

    Create address model

---
+++ server/models/address.js
@@ -0,0 +1,37 @@
+import { Schema, Types, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    province: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    city: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+      minLength: 10,
+      maxLength: 10,
+    },
+    body: {
+      type: String,
+      required: true,
+      minLength: 10,
+      maxLength: 100,
+    },
+    recipient: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Address", schema);
