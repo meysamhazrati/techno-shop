b0bd48d8e30718297778b77fdaaa6b55d32b8380
iFelani <iMeysamHazrati@gmail.com>
2024-02-01 23:05:04 +0330

    Update order validations

--- server/validators/order.js
+++ server/validators/order.js
@@ -3,17 +3,14 @@ import { object, string, number, array } from "yup";
 const validator = object({
   shippingCost: number().required().min(0).max(10000000),
   totalAmount: number().required().min(0).max(10000000000),
-  products: array()
-    .of(
-      object({
-        quantity: number().required().min(1).max(100),
-        color: string().required(),
-        product: string().required(),
-      })
-    )
-    .required()
-    .min(1)
-    .max(100),
+  products: array().of(object({
+    quantity: number().required().min(1).max(100),
+    color: object({
+      name: string().required().min(3).max(15),
+      code: string().required(),
+    }).required(),
+    product: string().required(),
+  })).required().min(1).max(100),
   destination: string().required(),
 });
 
