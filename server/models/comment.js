7f270368ad31ebb25f04a242d1cae73e1085a78b
iFelani <iMeysamHazrati@gmail.com>
2024-02-01 15:33:08 +0330

    Create comment model

---
+++ server/models/comment.js
@@ -0,0 +1,38 @@
+import { Schema, Types, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    body: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 300,
+    },
+    score: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    isConfirmed: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+    product: {
+      type: Types.ObjectId,
+      ref: "Product",
+    },
+    article: {
+      type: Types.ObjectId,
+      ref: "Article",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Comment", schema);
