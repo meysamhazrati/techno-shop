35e44b18df97858e5891ed57d8c7ed45ec4705d1
iFelani <iMeysamHazrati@gmail.com>
2024-02-01 22:03:25 +0330

    Create ticket model

---
+++ server/models/ticket.js
@@ -0,0 +1,45 @@
+import { Schema, Types, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    department: {
+      type: String,
+      required: true,
+      enum: ["Management", "Finance", "Order Tracking", "Support", "Feedback", "Other"],
+    },
+    title: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    body: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 300,
+    },
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+    ticket: {
+      type: Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  { timestamps: true }
+);
+
+schema.virtual("replies", {
+  ref: "Ticket",
+  localField: "_id",
+  foreignField: "ticket",
+});
+
+export default model("Ticket", schema);
