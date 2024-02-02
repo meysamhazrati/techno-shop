2e7da279fd127677252db1d6f29c08d9e113a196
iFelani <iMeysamHazrati@gmail.com>
2024-02-02 16:29:31 +0330

    Create discountCode model

---
+++ server/models/discountCode.js
@@ -0,0 +1,54 @@
+import { Schema, Types, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    code: {
+      type: String,
+      required: false,
+      minLength: 7,
+      maxLength: 7,
+      match: /^[a-zA-Z\d]{7}$/,
+      default: () => Array(7).fill(0).map(() => "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[Math.floor(Math.random() * 62)]).join(""),
+    },
+    percent: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 100,
+    },
+    minimumAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10000000,
+    },
+    maximumUsage: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 1000000000,
+    },
+    usages: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+    creator: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+    categories: [
+      {
+        type: Types.ObjectId,
+        ref: "Category",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default model("DiscountCode", schema);
