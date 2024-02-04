e5448826de16e21ff7187f4f31f6b8baba1b3988
iFelani <iMeysamHazrati@gmail.com>
2024-02-04 18:09:11 +0330

    Create offer model

---
+++ server/models/offer.js
@@ -0,0 +1,41 @@
+import { Schema, Types, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 100,
+    },
+    description: {
+      type: String,
+      required: true,
+      minLength: 10,
+      maxLength: 200,
+    },
+    percent: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 100,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+    organizer: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+schema.virtual("categories", {
+  ref: "Category",
+  localField: "_id",
+  foreignField: "Offer",
+});
+
+export default model("Offer", schema);
