94500ee8fd78371ad2d917746931d5877f87b290
iFelani <iMeysamHazrati@gmail.com>
2024-02-08 12:39:59 +0330

    Update product validations

--- server/validators/product.js
+++ server/validators/product.js
@@ -2,11 +2,10 @@ import { object, string, number, array } from "yup";
 
 const create = object({
   title: string().required().min(5).max(100),
-  price: number().required().min(1000).max(1000000000),
-  inventory: number().required().min(0).max(1000000),
   warranty: number().required().min(0).max(100),
   colors: array().of(object({
-    price: number().required().min(0).max(1000000),
+    price: number().required().min(1000).max(1000000000),
+    inventory: number().required().min(1).max(100000),
     name: string().required().min(3).max(15),
     code: string().required(),
   })).required().min(1).max(10),
@@ -17,11 +16,10 @@ const create = object({
 
 const update = object({
   title: string().required().min(5).max(100),
-  price: number().required().min(1000).max(1000000000),
-  inventory: number().required().min(0).max(1000000),
   warranty: number().required().min(0).max(100),
   colors: array().of(object({
-    price: number().required().min(0).max(1000000),
+    price: number().required().min(1000).max(1000000000),
+    inventory: number().required().min(1).max(100000),
     name: string().required().min(3).max(15),
     code: string().required(),
   })).required().min(1).max(10),
