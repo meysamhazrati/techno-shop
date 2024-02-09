ca1a9f1d733fb1211853c1e3e5d17e66875f1a8c
iFelani <iMeysamHazrati@gmail.com>
2024-02-09 17:07:14 +0330

    Update product routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, getAll, get, getByCategory, update, remove } from "../controllers/product.js";
+import { create, getAll, get, getByCategory, search, update, remove } from "../controllers/product.js";
 
 const router = Router();
 const uploader = multer("products");
@@ -11,5 +11,6 @@ const uploader = multer("products");
 router.route("/").post(authentication, isAdmin, uploader.array("covers", 4), create).get(getAll);
 router.route("/:id").get(get).put(authentication, isAdmin, update).delete(authentication, isAdmin, remove);
 router.get("/categories/:id", getByCategory);
+router.get("/search/:title", search);
 
 export default router;
