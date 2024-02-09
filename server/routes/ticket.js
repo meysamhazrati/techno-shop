a6d871570da3521e8c41eb71dc6de149cfcea592
iFelani <iMeysamHazrati@gmail.com>
2024-02-09 22:43:10 +0330

    Update ticket routes

--- server/routes/ticket.js
+++ server/routes/ticket.js
@@ -2,14 +2,15 @@ import { Router } from "express";
 
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, reply, getAll, get, open, close } from "../controllers/ticket.js";
+import { create, reply, getAll, get, open, close, remove } from "../controllers/ticket.js";
 
 const router = Router();
 
 router.route("/").post(authentication, create).get(authentication, isAdmin, getAll);
 router.post("/reply/:id", authentication, reply);
 router.get("/:id", authentication, get);
-router.put("/open/:id").put(authentication, isAdmin, open);
-router.put("/close/:id").put(authentication, isAdmin, close);
+router.put("/open/:id", authentication, isAdmin, open);
+router.put("/close/:id", authentication, isAdmin, close);
+router.delete("/:id", authentication, isAdmin, remove);
 
 export default router;
