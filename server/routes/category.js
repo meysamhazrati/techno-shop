9a5329a12ffefe4a0912a0e819c975bf84389d29
iFelani <iMeysamHazrati@gmail.com>
2024-02-09 23:59:13 +0330

    Update category routes

--- server/routes/category.js
+++ server/routes/category.js
@@ -2,21 +2,12 @@ import { Router } from "express";
 
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import {
-  create,
-  getAll,
-  get,
-  update,
-  remove,
-} from "../controllers/category.js";
+import { create, getAll, get, update, remove } from "../controllers/category.js";
 
 const router = Router();
 
 router.route("/").post(authentication, isAdmin, create).get(getAll);
-router
-  .route("/:id")
-  .get(get)
-  .put(authentication, isAdmin, update)
-  .delete(authentication, isAdmin, remove);
+router.get("/:title", get);
+router.route("/:id").put(authentication, isAdmin, update).delete(authentication, isAdmin, remove);
 
 export default router;
