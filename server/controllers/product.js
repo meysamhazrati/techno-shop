1245b872f8fd974307f0cb7094028573fd16f95b
iFelani <iMeysamHazrati@gmail.com>
2024-02-15 16:10:13 +0330

    Update product controller

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,8 +2,8 @@ import { model as model_ } from "mongoose";
 import { unlink } from "fs";
 
 import model from "../models/product.js";
+import colorModel from "../models/color.js";
 import userModel from "../models/user.js";
-import orderModel from "../models/order.js";
 import commentModel from "../models/comment.js";
 import mobileModel from "../models/mobile.js";
 import tabletModel from "../models/tablet.js";
@@ -19,7 +19,7 @@ import consoleModel from "../models/console.js";
 const create = async (request, response, next) => {
   try {
     const covers = request.files;
-    await model.createValidation({ ...request.body, ...covers });
+    await model.createValidation({ ...request.body, covers });
 
     const { type } = request.query;
 
@@ -28,7 +28,9 @@ const create = async (request, response, next) => {
 
       await currentModel.validation(request.body);
 
-      await currentModel.create({ ...request.body, covers: covers.map((cover) => cover.filename) });
+      const { _id } = await currentModel.create({ ...request.body, covers: covers.map((cover) => cover.filename) });
+
+      await Promise.all(request.body.colors.map(async (color) => await colorModel.create({ ...color, product: _id })));
 
       response.status(201).json({ message: "The product has been successfully added." });
     } else {
@@ -45,10 +47,11 @@ const getAll = async (request, response, next) => {
   try {
     const { page = 1, length = 6 } = request.query;
 
-    const products = await model.find({}, "title covers colors").populate([
+    const products = await model.find({}, "title covers").populate([
+      { path: "colors", select: "price inventory name code" },
       { path: "brand", select: "-__v" },
-      { path: "category", select: "-offer -__v" },
-      { path: "offer", select: "percent expiresAt" },
+      { path: "category", select: "-__v" },
+      { path: "offer", select: "title percent expiresAt" },
       { path: "comments", select: "score" },
     ]).sort({ createdAt: -1 }).lean();
 
@@ -78,10 +81,11 @@ const get = async (request, response, next) => {
     const { id } = request.params;
 
     const product = await model.findById(id, "-__v").populate([
+      { path: "colors", select: "price inventory name code" },
       { path: "brand", select: "-__v" },
-      { path: "category", select: "-offer -__v" },
+      { path: "category", select: "-__v" },
       { path: "offer", select: "-organizer -__v" },
-      { path: "comments", select: "-__v", populate: { path: "sender", select: "firstName lastName avatar" } },
+      { path: "comments", select: "-__v", options: { sort: { createdAt: -1 } }, populate: { path: "sender", select: "firstName lastName avatar" } },
     ]).lean();
 
     if (product) {
@@ -99,9 +103,43 @@ const getByCategory = async (request, response, next) => {
     const { id } = request.params;
     const { page = 1, length = 6 } = request.query;
 
-    const products = await model.find({ category: id }, "title covers colors").populate([
+    const products = await model.find({ category: id }, "title covers").populate([
+      { path: "colors", select: "price inventory name code" },
       { path: "brand", select: "-__v" },
       { path: "offer", select: "percent expiresAt" },
+      { path: "comments", select: "score" },
+    ]).sort({ createdAt: -1 }).lean();
+
+    if (products.length) {
+      const currentPage = parseInt(page);
+      const lengthPerPage = parseInt(length);
+
+      const startIndex = (currentPage - 1) * lengthPerPage;
+      const endIndex = startIndex + lengthPerPage;
+
+      const currentPageProducts = products.slice(startIndex, endIndex).map(({ comments, ...product }) => ({ ...product, score: parseFloat((comments.reduce((previous, { score }) => previous + score, 5) / (comments.length + 1) || 5).toFixed(1)) }));
+      const hasNextPage = endIndex < products.length;
+
+      if (currentPageProducts.length) {
+        return response.json({ products: currentPageProducts, hasNextPage, nextPage: hasNextPage ? currentPage + 1 : null });
+      }
+    }
+
+    throw Object.assign(new Error("No product found."), { status: 404 });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getByOffer = async (request, response, next) => {
+  try {
+    const { id } = request.params;
+    const { page = 1, length = 6 } = request.query;
+
+    const products = await model.find({ offer: id }, "title covers").populate([
+      { path: "colors", select: "price inventory name code" },
+      { path: "brand", select: "-__v" },
+      { path: "category", select: "-__v" },
       { path: "comments", select: "score" },
     ]).sort({ createdAt: -1 }).lean();
 
@@ -131,9 +169,10 @@ const search = async (request, response, next) => {
     const { title } = request.params;
     const { page = 1, length = 6 } = request.query;
 
-    const products = await model.find({ title: { $regex: new RegExp(title, "i") } }, "title covers colors").populate([
+    const products = await model.find({ title: { $regex: new RegExp(title, "i") } }, "title covers").populate([
+      { path: "colors", select: "price inventory name code" },
       { path: "brand", select: "-__v" },
-      { path: "category", select: "-offer -__v" },
+      { path: "category", select: "-__v" },
       { path: "offer", select: "percent expiresAt" },
       { path: "comments", select: "score" },
     ]).sort({ createdAt: -1 }).lean();
@@ -174,6 +213,10 @@ const update = async (request, response, next) => {
       const result = await currentModel.findByIdAndUpdate(id, { ...request.body });
 
       if (result) {
+        const colors = await Promise.all(request.body.colors.map(async ({ price, inventory, name, code }) => await colorModel.findOneAndUpdate({ $or: [{ name }, { code }], product: id }, { price, inventory, name, code }, { upsert: true, new: true })));
+
+        await colorModel.deleteMany({ _id: { $nin: colors.map(({ _id }) => _id) }, product: id });
+
         response.json({ message: "The product has been successfully edited." });
       } else {
         throw Object.assign(new Error("The product was not found."), { status: 404 });
@@ -194,9 +237,9 @@ const remove = async (request, response, next) => {
 
     if (result) {
       result.covers.forEach((cover) => unlink(`public/products/${cover}`, (error) => console.error(error)));
-      
+
       await userModel.updateMany({ $or: [{ "cart.product": id }, { favorites: id }] }, { $pull: { cart: { product: id }, favorites: id } });
-      await orderModel.deleteMany({ "products.product": id });
+      await colorModel.deleteMany({ product: id });
       await commentModel.deleteMany({ product: id });
 
       response.json({ message: "The product has been successfully removed." });
@@ -208,4 +251,4 @@ const remove = async (request, response, next) => {
   }
 };
 
-export { create, getAll, get, getByCategory, search, update, remove };
+export { create, getAll, get, getByCategory, getByOffer, search, update, remove };
