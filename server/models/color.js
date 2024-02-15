decda77848bfe1938e092f57011ceee36cef71f2
iFelani <iMeysamHazrati@gmail.com>
2024-02-15 12:17:04 +0330

    Create color model

---
+++ server/models/color.js
@@ -0,0 +1,36 @@
+import { Schema, Types, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    price: {
+      type: Number,
+      required: true,
+      min: 1000,
+      max: 1000000000,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 100000,
+    },
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 15,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    product: {
+      type: Types.ObjectId,
+      ref: "Product",
+    },
+  },
+  { timestamps: true }
+);
+
+export { schema };
+export default model("Color", schema);
