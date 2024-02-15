986f7c09fce98490824cf22c38a2e6c1e48747a9
iFelani <iMeysamHazrati@gmail.com>
2024-02-15 19:35:53 +0330

    Update order controller

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,21 +1,40 @@
-import orderModel from "../models/order.js";
+import model from "../models/order.js";
+import productModel from "../models/product.js";
+import colorModel from "../models/color.js";
+import userModel from "../models/user.js";
+import addressModel from "../models/address.js";
+import discountCodeModel from "../models/discountCode.js";
 
 const create = async (request, response, next) => {
   try {
-    await orderModel.validation(request.body);
+    await model.validation(request.body);
 
-    const { shippingCost, totalAmount, products, destination, discountCode } = request.body;
+    const { shippingCost, totalAmount } = request.body;
 
-    await orderModel.create({
-      shippingCost,
-      totalAmount,
-      products,
-      buyer: request.user._id,
-      destination,
-      discountCode,
-    });
+    const products = await Promise.all(request.body.products.map(async ({ quantity, product, color }) => ({ quantity, product: await productModel.findById(product), color: await colorModel.findById(color) })));
 
-    response.status(201).json({ message: "The order has been successfully added." });
+    const hasInventory = products.every(({ quantity, color }) => color.inventory >= quantity);
+
+    if (hasInventory) {
+      const buyer = await userModel.findById( request.user._id, "-cart -favorites");
+      const destination = await addressModel.findById(request.body.destination);
+      const discountCode = await discountCodeModel.findById(request.body.discountCode);
+
+      await model.create({
+        shippingCost,
+        totalAmount,
+        products,
+        buyer,
+        destination,
+        discountCode: discountCode ?? undefined,
+      });
+
+      await Promise.all(request.body.products.map(async ({ quantity, color }) => await colorModel.findByIdAndUpdate(color, { $inc: { inventory: -quantity } })));
+
+      response.status(201).json({ message: "The order has been successfully added." });
+    } else {
+      throw Object.assign(new Error("The product(s) inventory is insufficient."), { status: 409 });
+    }
   } catch (error) {
     next(error);
   }
@@ -23,13 +42,26 @@ const create = async (request, response, next) => {
 
 const getAll = async (request, response, next) => {
   try {
-    const orders = await orderModel.find({}, "-__v").lean();
+    const { page = 1, length = 6 } = request.query;
+
+    const orders = await model.find({}, "shippingCost totalAmount status createdAt updatedAt buyer.firstName buyer.lastName").sort({ createdAt: -1 }).lean();
 
     if (orders.length) {
-      response.json(orders);
-    } else {
-      throw Object.assign(new Error("No order found."), { status: 404 });
+      const currentPage = parseInt(page);
+      const lengthPerPage = parseInt(length);
+
+      const startIndex = (currentPage - 1) * lengthPerPage;
+      const endIndex = startIndex + lengthPerPage;
+
+      const currentPageOrders = orders.slice(startIndex, endIndex);
+      const hasNextPage = endIndex < orders.length;
+
+      if (currentPageOrders.length) {
+        return response.json({ orders: currentPageOrders, hasNextPage, nextPage: hasNextPage ? currentPage + 1 : null });
+      }
     }
+
+    throw Object.assign(new Error("No order found."), { status: 404 });
   } catch (error) {
     next(error);
   }
@@ -39,12 +71,17 @@ const get = async (request, response, next) => {
   try {
     const { id } = request.params;
 
-    const { user } = request;
+    const { _id, role } = request.user;
 
-    const order = await orderModel.findById(id, "-__v").lean();
+    const order = await model.findById(id, "-__v -products.product.__v -products.color.product -products.color.__v -buyer.password -buyer.cart -buyer.favorites -buyer.__v -destination.recipient -destination.__v -discountCode.__v").populate([
+      { path: "products.product.brand", select: "name englishName" },
+      { path: "products.product.category discountCode.categories", select: "title englishTitle" },
+      { path: "products.product.offer", select: "-description -__v", populate: { path: "organizer", select: "firstName lastName" } },
+      { path: "discountCode.creator", select: "firstName lastName" },
+    ]).lean();
 
     if (order) {
-      if (user.role === "ADMIN" || user._id.equals(order.buyer)) {
+      if (role === "ADMIN" || _id.equals(order.buyer)) {
         response.json(order);
       } else {
         throw Object.assign(new Error("You don't have access to this order."), { status: 403 });
@@ -61,11 +98,11 @@ const deliver = async (request, response, next) => {
   try {
     const { id } = request.params;
 
-    const order = await orderModel.findById(id);
+    const order = await model.findById(id);
 
     if (order) {
       if (order.status === "In progress") {
-        await orderModel.findByIdAndUpdate(id, { status: "Delivered" });
+        await model.findByIdAndUpdate(id, { status: "Delivered" });
 
         response.json({ message: "The order has been successfully delivered." });
       } else if (order.status === "Delivered") {
@@ -85,14 +122,14 @@ const cancel = async (request, response, next) => {
   try {
     const { id } = request.params;
 
-    const { user } = request;
+    const { _id, role } = request.user;
 
-    const order = await orderModel.findById(id);
+    const order = await model.findById(id);
 
     if (order) {
-      if (user.role === "ADMIN" || user._id.equals(order.buyer)) {
+      if (role === "ADMIN" || _id.equals(order.buyer)) {
         if (order.status === "In progress") {
-          await orderModel.findByIdAndUpdate(id, { status: "Canceled" });
+          await model.findByIdAndUpdate(id, { status: "Canceled" });
 
           response.json({ message: "The order has been successfully canceled." });
         } else if (order.status === "Canceled") {
@@ -115,14 +152,14 @@ const return_ = async (request, response, next) => {
   try {
     const { id } = request.params;
 
-    const { user } = request;
+    const { _id, role } = request;
 
-    const order = await orderModel.findById(id);
+    const order = await model.findById(id);
 
     if (order) {
-      if (user.role === "ADMIN" || user._id.equals(order.buyer)) {
+      if (role === "ADMIN" || _id.equals(order.buyer)) {
         if (order.status === "Delivered") {
-          await orderModel.findByIdAndUpdate(id, { status: "Returned" });
+          await model.findByIdAndUpdate(id, { status: "Returned" });
 
           response.json({ message: "The order has been successfully returned." });
         } else if (order.status === "Returned") {
