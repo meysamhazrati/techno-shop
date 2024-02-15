2d6e726d745e5715ba85f312624762f1333ff42f
iFelani <iMeysamHazrati@gmail.com>
2024-02-15 23:33:37 +0330

    Update authentication middleware

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { parse } from "cookie";
 
-import userModel from "../models/user.js";
+import model from "../models/user.js";
 
 const middleware = async (request, response, next) => {
   try {
@@ -10,7 +10,27 @@ const middleware = async (request, response, next) => {
     if (token) {
       const { id } = jwt.verify(token, process.env.JWT_SECRET);
 
-      const user = await userModel.findById(id, "-password -__v");
+      const user = await model.findById(id, "-password -__v").populate([
+        { path: "cart", populate: [
+          { path: "product", select: "title warranty covers" },
+          { path: "color", select: "price inventory name code" },
+          { path: "offer", select: "percent expiresAt" },
+        ] },
+        { path: "favorites", select: "title covers", populate: [
+          { path: "colors", select: "price inventory name code" },
+          { path: "brand", select: "name englishName" },
+          { path: "category", select: "title englishTitle" },
+          { path: "offer", select: "percent expiresAt" },
+        ] },
+        { path: "addresses", select: "-__v", options: { sort: { createdAt: -1 } } },
+        { path: "orders", select: "shippingCost totalAmount status products.quantity products.product.title products.product.covers products.color.name products.color.code destination.province destination.city createdAt updatedAt", options: { sort: { createdAt: -1 } } },
+        { path: "comments", select: "-isBuyer -__v", options: { sort: { createdAt: -1 } }, populate: [
+          { path: "product", select: "title covers" },
+          { path: "article", select: "title cover" },
+        ] },
+        { path: "articles", select: "-__v", options: { sort: { createdAt: -1 } }, populate: { path: "category", select: "title englishTitle" } },
+        { path: "tickets", select: "-__v", match: { ticket: { $exists: false } }, options: { sort: { createdAt: -1 } } },
+      ]).lean();
 
       if (user) {
         request.user = user;
