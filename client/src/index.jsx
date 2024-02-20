ebb9e95aac904235a1d4a68b8b234cd73220ddc7
iFelani <iMeysamHazrati@gmail.com>
2024-02-20 22:30:40 +0330

    Install and config react-query

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import routes from "./routes";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={createBrowserRouter(routes)} />
+    <QueryClientProvider client={new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 1000 * 60 * 10,
+          gcTime: 1000 * 60 * 2,
+          retry: 2,
+        },
+      },
+    })}>
+      <RouterProvider router={createBrowserRouter(routes)} />
+    </QueryClientProvider>
   </StrictMode>,
 );
