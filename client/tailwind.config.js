3207cc3e0524967f09ea036539f3d6767d08c2d7
iFelani <iMeysamHazrati@gmail.com>
2024-02-27 18:35:22 +0330

    Add loader animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -20,6 +20,25 @@ export default {
         "vazirmatn-bold": "vazirmatn-bold",
         "vazirmatn-black": "vazirmatn-black",
       },
+      keyframes: {
+        loader: {
+          "33%": {
+            "background-size":
+              "calc(100% / 3) 0%, calc(100% / 3) 100%, calc(100% / 3) 100%",
+          },
+          "50%": {
+            "background-size":
+              "calc(100% / 3) 100%, calc(100% / 3) 0%, calc(100% / 3) 100%",
+          },
+          "66%": {
+            "background-size":
+              "calc(100% / 3) 100%, calc(100% / 3) 100%, calc(100% / 3) 0%",
+          },
+        },
+      },
+      animation: {
+        loader: "loader 0.7s linear infinite",
+      },
       container: {
         center: true,
         padding: {
