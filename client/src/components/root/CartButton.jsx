62a06608e1d22b094127ee2ed48fb24b640056eb
iFelani <iMeysamHazrati@gmail.com>
2024-03-01 17:32:24 +0330

    Create CartButton component

---
+++ client/src/components/root/CartButton.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import useMe from "../../hooks/authentication/me";
+import CartProduct from "../CartProduct";
+import CartIcon from "../../icons/Cart";
+import TomanIcon from "../../icons/Toman";
+
+const CartButton = () => {
+  const { isError, data } = useMe();
+
+  return (
+    <div className="group relative size-12 rounded-full transition-colors hover:bg-zinc-200">
+      <Link to={isError ? "/authentication/login" : "/me/cart"} className="flex size-full items-center justify-center rounded-full">
+        <CartIcon className="size-7 transition-colors group-hover:stroke-zinc-700" />
+      </Link>
+      {data?.cart.length > 0 && (
+        <>
+          <div className="pointer-events-none absolute right-1 top-1 flex size-5 items-center justify-center rounded-full border-[2px] border-white bg-primary-900 text-xs text-white transition-colors group-hover:border-zinc-200">{data.cart.length}</div>
+          {window.innerWidth > 1024 && (
+            <div className="invisible absolute left-0 top-auto rounded-2xl border-b-4 border-primary-900 bg-white p-3 opacity-0 shadow-lg transition-all group-hover:visible group-hover:opacity-100">
+              <span className="text-zinc-400">{data.cart.length} کالا</span>
+              <div className="max-h-80 w-[350px] divide-y divide-zinc-200 overflow-auto">
+                {data?.cart.map((product) => <CartProduct key={product._id} {...product} />)}
+              </div>
+              <div className="flex w-full items-center justify-between border-t border-zinc-200 pt-3">
+                <div>
+                  <span className="block text-xs text-zinc-500">مبلغ قابل پرداخت</span>
+                  <span className="flex items-center gap-x-[2px] font-vazirmatn-bold text-xl">
+                    {data.cart.reduce((previous, { quantity, product, color }) => previous + quantity * (Date.parse(product.offer?.expiresAt) > Date.now() ? color.price - color.price * (product.offer.percent / 100) : color.price), 0).toLocaleString()}
+                    <TomanIcon className="size-5" />
+                  </span>
+                </div>
+                <Link to="/me/cart" className="rounded-2xl bg-primary-900 px-5 py-3 font-vazirmatn-medium text-white transition-colors hover:bg-primary-800">ثبت سفارش</Link>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CartButton;
