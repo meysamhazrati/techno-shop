7202b8db2d0c659fd8186b6aaadbe708f38d3324
iFelani <iMeysamHazrati@gmail.com>
2024-03-01 15:31:16 +0330

    Add Minus icon

---
+++ client/src/icons/Minus.jsx
@@ -0,0 +1,9 @@
+const Minus = ({ className }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={className}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+    </svg>
+  );
+};
+
+export default Minus;
