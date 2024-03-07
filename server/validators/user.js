b86bc01bb3d8bb36d34e3c60874ea789edfc63e7
MeySaM <imeysamhazrati@gmail.com>
2024-03-07 22:14:55 +0330

    Update user validations

--- server/validators/user.js
+++ server/validators/user.js
@@ -10,7 +10,6 @@ const edit = object({
 const update = object({
   firstName: string().required().min(3).max(70),
   lastName: string().required().min(4).max(70),
-  phone: string().required().length(11).matches(/^09\d{9}$/),
   email: string().required().min(10).max(100).matches(/^\w+([.-]?\w)*@\w+([.-]?\w)*\.[a-zA-Z]{2,4}$/),
   password: string().min(8).max(20).matches(/^[\w?!$._-]{8,20}$/),
   role: string().required().oneOf(["USER", "ADMIN"]),
