58c9bec83e0dfc8bfb1827a3cc311c1732a7257b
MeySaM <imeysamhazrati@gmail.com>
2024-03-07 23:29:26 +0330

    Update user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { getAll, get, edit, update, addToCart, removeFromCart, addToFavorites, removeFromFavorites, ban, unBan, remove } from "../controllers/user.js";
+import { getAll, get, edit, update, addToCart, removeFromCart, emptyCart, addToFavorites, removeFromFavorites, emptyFavorites, ban, unBan, remove } from "../controllers/user.js";
 
 const router = Router();
 const uploader = multer("users");
@@ -13,8 +13,10 @@ router.get("/:id", authentication, isAdmin, get);
 router.route("/:id").put(authentication, isAdmin, uploader.single("avatar"), update).delete(authentication, isAdmin, remove);
 router.put("/cart/add/:id", authentication, addToCart);
 router.put("/cart/remove/:id", authentication, removeFromCart);
+router.put("/cart/empty", authentication, emptyCart);
 router.put("/favorites/add/:id", authentication, addToFavorites);
 router.put("/favorites/remove/:id", authentication, removeFromFavorites);
+router.put("/favorites/empty", authentication, emptyFavorites);
 router.put("/ban/:id", authentication, isAdmin, ban);
 router.put("/unBan/:id", authentication, isAdmin, unBan);
 
