620a9b7fd245872d7560d3c49625329845855f95
MeySaM <imeysamhazrati@gmail.com>
2024-03-08 20:58:29 +0330

    Update Root page

--- client/src/pages/root/Root.jsx
+++ client/src/pages/root/Root.jsx
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Header from "../../components/root/Header";
 
 const Root = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="container pt-20 lg:pt-32">
+        <Outlet />
+      </div>
+    </>
   );
 };
 
