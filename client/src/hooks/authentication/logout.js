2c50c773d99a04a971889021b462499e3bedbd9e
MeySaM <imeysamhazrati@gmail.com>
2024-03-08 22:24:21 +0330

    Create useLogout hook

---
+++ client/src/hooks/authentication/logout.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { Context as ToastContext } from "../../contexts/Toast";
+import { logout } from "../../axios/controllers/authentication";
+
+export default () => {
+  const client = useQueryClient();
+
+  const { openToast } = useContext(ToastContext);
+
+  const { mutate } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      client.invalidateQueries(["me"]);
+      openToast("success", "موفق!", "با موفقیت از حساب کاربری خود خارج شدید.");
+    },
+    onError: () => openToast("error", "خطا!", "مشکلی پیش آمد! لطفا بعدا دوباره تلاش کنید."),
+  });
+
+  return { mutate };
+};
