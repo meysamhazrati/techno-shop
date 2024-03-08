891ff51a2b640e0d9ccbadec21910ccf6d583ff0
MeySaM <imeysamhazrati@gmail.com>
2024-03-08 22:44:39 +0330

    Update axios authentication controller

--- client/src/axios/controllers/authentication.js
+++ client/src/axios/controllers/authentication.js
@@ -4,18 +4,19 @@ const send = async ({ email }, type) => await axios.post("/authentication/send",
 
 const verify = async ({ email, code }) => await axios.post("/authentication/verify", { email, code });
 
-const register = async ({ firstName, lastName, phone, email, password }) => await axios.post("/authentication/register", {
+const register = async ({ firstName, lastName, email, password }) => await axios.post("/authentication/register", {
   firstName,
   lastName,
-  phone,
   email,
   password,
 });
 
-const login = async ({ identifier, password }) => await axios.post("/authentication/login", { identifier, password });
+const login = async ({ email, password }) => await axios.post("/authentication/login", { email, password });
+
+const resetPassword = async ({ password }) => await axios.put("/authentication/resetPassword", { password });
 
 const me = async () => await axios.get("/authentication/me");
 
 const logout = async () => await axios.delete("/authentication/logout");
 
-export { send, verify, register, login, me, logout };
+export { send, verify, register, login, resetPassword, me, logout };
