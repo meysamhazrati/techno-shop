3618d33c12e31c419813b839e5d2a8d4a23a2e73
MeySaM <imeysamhazrati@gmail.com>
2024-03-08 22:39:22 +0330

    Update ProfileButton component

--- client/src/components/root/ProfileButton.jsx
+++ client/src/components/root/ProfileButton.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import useMe from "../../hooks/authentication/me";
+import useLogout from "../../hooks/authentication/logout";
 import UserIcon from "../../icons/User";
 import BagIcon from "../../icons/Bag";
 import HeartIcon from "../../icons/Heart";
@@ -9,6 +10,7 @@ import ExitIcon from "../../icons/Exit";
 
 const ProfileButton = () => {
   const { data } = useMe();
+  const { mutate } = useLogout();
 
   return (
     <div className="group relative size-12">
@@ -17,32 +19,32 @@ const ProfileButton = () => {
       </Link>
       {data && (
         <div className="invisible absolute left-0 top-auto w-64 overflow-hidden rounded-2xl bg-white opacity-0 shadow-lg transition-all group-hover:visible group-hover:opacity-100">
-          <button className="h-12 w-full px-6 hover:bg-zinc-200 hover:text-zinc-700">
-            <Link to="/me/profile" className="flex size-full items-center gap-x-2 border-b border-zinc-200">
+          <button className="h-12 w-full px-6 transition-colors hover:bg-zinc-200 hover:text-zinc-700">
+            <Link to="/me/profile" className="flex size-full items-center gap-x-2 border-b border-zinc-200 [&>*]:transition-colors">
               <UserIcon className="size-6" />
               <span>{`${data.firstName} ${data.lastName}`}</span>
             </Link>
           </button>
-          <div className="[&>*]:flex [&>*]:h-12 [&>*]:w-full [&>*]:items-center [&>*]:gap-x-2 [&>*]:px-6 hover:[&>*]:bg-zinc-200 hover:[&>*]:text-zinc-700">
-            <Link to="/me/orders">
+          <div className="[&>*]:flex [&>*]:h-12 [&>*]:w-full [&>*]:items-center [&>*]:gap-x-2 [&>*]:px-6 [&>*]:transition-colors hover:[&>*]:bg-zinc-200 hover:[&>*]:text-zinc-700">
+            <Link to="/me/orders" className="[&>*]:transition-colors">
               <BagIcon className="size-6" />
               <span>سفارش ها</span>
             </Link>
-            <Link to="/me/favorites">
+            <Link to="/me/favorites" className="[&>*]:transition-colors">
               <HeartIcon className="size-6" />
               <span>علاقه‌مندی ها</span>
             </Link>
-            <Link to="/me/comments">
+            <Link to="/me/comments" className="[&>*]:transition-colors">
               <CommentIcon className="size-6" />
               <span>دیدگاه ها</span>
             </Link>
-            <Link to="/me/tickets">
+            <Link to="/me/tickets" className="[&>*]:transition-colors">
               <TicketIcon className="size-6" />
               <span>تیکت ها</span>
             </Link>
           </div>
-          <button className="h-12 w-full px-6 hover:bg-zinc-200 hover:text-zinc-700">
-            <div className="flex size-full items-center gap-x-2 border-t border-zinc-200">
+          <button className="h-12 w-full px-6 transition-colors hover:bg-zinc-200 hover:text-zinc-700" onClick={mutate}>
+            <div className="flex size-full items-center gap-x-2 border-t border-zinc-200 [&>*]:transition-colors">
               <ExitIcon className="size-6" />
               <span>خروج</span>
             </div>
