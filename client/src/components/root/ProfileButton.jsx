9ff113995b752665679f75ae4950b0aa883d86bf
MeySaM <imeysamhazrati@gmail.com>
2024-03-15 23:44:38 +0330

    Update ProfileButton component

--- client/src/components/root/ProfileButton.jsx
+++ client/src/components/root/ProfileButton.jsx
@@ -9,20 +9,20 @@ import TicketIcon from "../../icons/Ticket";
 import ExitIcon from "../../icons/Exit";
 
 const ProfileButton = () => {
-  const { data } = useMe();
-  const { mutate } = useLogout();
+  const { me } = useMe();
+  const { logout } = useLogout();
 
   return (
     <div className="group relative size-12">
       <Link to="/me" className="block size-full overflow-hidden rounded-full">
-        <img src={`http://localhost:3000/users/${data?.avatar}`} alt="My Avatar" className="size-full object-cover" />
+        <img src={`http://localhost:3000/users/${me?.avatar}`} alt="My Avatar" className="size-full object-cover" />
       </Link>
-      {data && (
-        <div className="invisible absolute left-0 top-auto w-64 overflow-hidden rounded-2xl bg-white opacity-0 shadow-lg transition-all group-hover:visible group-hover:opacity-100">
+      {me && (
+        <div className="invisible absolute left-0 top-auto w-64 overflow-hidden rounded-3xl bg-white opacity-0 shadow transition-all group-hover:visible group-hover:opacity-100">
           <button className="h-12 w-full px-6 transition-colors hover:bg-zinc-200 hover:text-zinc-700">
             <Link to="/me/profile" className="flex size-full items-center gap-x-2 border-b border-zinc-200 [&>*]:transition-colors">
               <UserIcon className="size-6" />
-              <span>{`${data.firstName} ${data.lastName}`}</span>
+              <span>{me.firstName} {me.lastName}</span>
             </Link>
           </button>
           <div className="[&>*]:flex [&>*]:h-12 [&>*]:w-full [&>*]:items-center [&>*]:gap-x-2 [&>*]:px-6 [&>*]:transition-colors hover:[&>*]:bg-zinc-200 hover:[&>*]:text-zinc-700">
@@ -43,7 +43,7 @@ const ProfileButton = () => {
               <span>تیکت ها</span>
             </Link>
           </div>
-          <button className="h-12 w-full px-6 transition-colors hover:bg-zinc-200 hover:text-zinc-700" onClick={mutate}>
+          <button className="h-12 w-full px-6 transition-colors hover:bg-zinc-200 hover:text-zinc-700" onClick={logout}>
             <div className="flex size-full items-center gap-x-2 border-t border-zinc-200 [&>*]:transition-colors">
               <ExitIcon className="size-6" />
               <span>خروج</span>
