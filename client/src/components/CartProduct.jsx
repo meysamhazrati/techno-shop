854465267e188182070513517752f222837dc36f
MeySaM <imeysamhazrati@gmail.com>
2024-03-15 23:35:51 +0330

    Update CartProduct component

--- client/src/components/CartProduct.jsx
+++ client/src/components/CartProduct.jsx
@@ -1,28 +1,21 @@
 import { Link } from "react-router-dom";
-import useAddToCart from "../hooks/user/addToCart";
-import useRemoveFromCart from "../hooks/user/removeFromCart";
 import Timer from "./Timer";
-import Loader from "./Loader";
+import ProductButton from "./ProductButton";
 import VerifiedIcon from "../icons/Verified";
-import TrashIcon from "../icons/Trash";
-import MinusIcon from "../icons/Minus";
-import PlusIcon from "../icons/Plus";
 import TomanIcon from "../icons/Toman";
 import offer from "/images/offer.svg";
 
 const CartProduct = ({ quantity, product, color }) => {
-  const { isPending: isPendingAdd, mutate: add } = useAddToCart(product._id);
-  const { isPending: isPendingRemove, mutate: remove } = useRemoveFromCart(product._id,);
-
   return (
     <div className="grid grid-cols-[128px_1fr] items-center gap-x-4 gap-y-2 py-3">
       <Link to={`/products/${product._id}`} className="relative size-32 [&>img]:absolute [&>img]:size-full [&>img]:object-cover">
+        {Date.parse(product.offer?.expiresAt) > Date.now() && <div className="absolute right-0 top-4 z-10 flex h-5 w-9 items-center justify-center rounded-full bg-primary-900 font-vazirmatn-medium text-xs text-white">{product.offer.percent}%</div>}
         <img src={`http://localhost:3000/products/${product.covers[1]}`} alt="Product Cover" loading="lazy" />
         <img src={`http://localhost:3000/products/${product.covers[0]}`} alt="Product Cover" loading="lazy" className="transition-opacity duration-300 hover:opacity-0" />
       </Link>
       <div>
-        <h3 className="line-clamp-2 font-vazirmatn-medium text-lg">
-          <Link to={`/products/${product._id}`}>{product.title}</Link>
+        <h3 className="h-14 font-vazirmatn-medium text-lg">
+          <Link to={`/products/${product._id}`} className="line-clamp-2 transition-colors hover:text-primary-900">{product.title}</Link>
         </h3>
         <div className="mt-2 flex flex-col gap-y-1 text-sm text-zinc-500 [&>*]:flex [&>*]:items-center [&>*]:gap-x-2">
           <div>
@@ -43,22 +36,7 @@ const CartProduct = ({ quantity, product, color }) => {
           </div>
         </>
       )}
-      <div className="mx-auto flex h-11 w-[104px] items-center justify-between rounded-2xl border border-primary-900 px-2 text-primary-900">
-        {color.inventory > quantity ? (
-          <button onClick={() => !isPendingAdd && !isPendingRemove && add({ color: color._id })}>
-            <PlusIcon className="size-5 opacity-100" />
-          </button>
-        ) : (
-          <div>
-            <PlusIcon className="size-5 opacity-50" />
-          </div>
-        )}
-        <div className="flex flex-col items-center">
-          {isPendingAdd || isPendingRemove ? <Loader width={"28px"} height={"7px"} color={"#0279D9"} /> : <span className="font-vazirmatn-medium">{quantity}</span>}
-          {quantity === color.inventory && <span className="-mt-2 text-xs text-zinc-400">حداکثر</span>}
-        </div>
-        <button onClick={() => !isPendingAdd && !isPendingRemove && remove({ color: color._id })}>{quantity > 1 ? <MinusIcon className="size-5" /> : <TrashIcon className="size-5" />}</button>
-      </div>
+      <ProductButton id={product._id} quantity={quantity} color={color} />
       <div className="flex flex-col">
         {Date.parse(product.offer?.expiresAt) > Date.now() && <span className="text-sm text-zinc-400 line-through">{color.price.toLocaleString()}</span>}
         <span className="flex items-center gap-x-[2px] font-vazirmatn-bold text-lg">
