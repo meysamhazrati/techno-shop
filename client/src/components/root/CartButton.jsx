1256c0a5d3a4eb7cafd43dc41138fb8400bbb4ea
MeySaM <imeysamhazrati@gmail.com>
2024-03-15 23:41:04 +0330

    Update CartButton component

--- client/src/components/root/CartButton.jsx
+++ client/src/components/root/CartButton.jsx
@@ -5,31 +5,31 @@ import CartIcon from "../../icons/Cart";
 import TomanIcon from "../../icons/Toman";
 
 const CartButton = () => {
-  const { isError, data } = useMe();
+  const { isMeError, me } = useMe();
 
   return (
     <div className="group relative size-12 rounded-full transition-colors hover:bg-zinc-200">
-      <Link to={isError ? "/authentication/login" : "/me/cart"} className="flex size-full items-center justify-center rounded-full">
+      <Link to={isMeError ? "/authentication/login" : "/me/cart"} className="flex size-full items-center justify-center rounded-full">
         <CartIcon className="size-7 transition-colors group-hover:stroke-zinc-700" />
       </Link>
-      {data?.cart.length > 0 && (
+      {me?.cart.length > 0 && (
         <>
-          <div className="pointer-events-none absolute right-1 top-1 flex size-5 items-center justify-center rounded-full border-[2px] border-white bg-primary-900 text-xs text-white transition-colors group-hover:border-zinc-200">{data.cart.length}</div>
+          <div className="pointer-events-none absolute right-1 top-1 flex size-5 items-center justify-center rounded-full border-[2px] border-white bg-primary-900 text-xs text-white transition-colors group-hover:border-zinc-200">{me.cart.length}</div>
           {window.innerWidth > 1024 && (
-            <div className="invisible absolute left-0 top-auto rounded-2xl border-b-4 border-primary-900 bg-white p-3 opacity-0 shadow-lg transition-all group-hover:visible group-hover:opacity-100">
-              <span className="text-zinc-400">{data.cart.length} کالا</span>
+            <div className="invisible absolute left-0 top-auto rounded-3xl border-b-4 border-primary-900 bg-white p-3 opacity-0 shadow transition-all group-hover:visible group-hover:opacity-100">
+              <span className="text-zinc-400">{me.cart.length} کالا</span>
               <div className="max-h-80 w-[350px] divide-y divide-zinc-200 overflow-auto">
-                {data?.cart.map((product) => <CartProduct key={product._id} {...product} />)}
+                {me?.cart.map((product) => <CartProduct key={product._id} {...product} />)}
               </div>
               <div className="flex w-full items-center justify-between border-t border-zinc-200 pt-3">
                 <div>
                   <span className="block text-xs text-zinc-500">مبلغ قابل پرداخت</span>
                   <span className="flex items-center gap-x-[2px] font-vazirmatn-bold text-xl">
-                    {data.cart.reduce((previous, { quantity, product, color }) => previous + quantity * (Date.parse(product.offer?.expiresAt) > Date.now() ? color.price - color.price * (product.offer.percent / 100) : color.price), 0).toLocaleString()}
+                    {me.cart.reduce((previous, { quantity, product, color }) => previous + quantity * (Date.parse(product.offer?.expiresAt) > Date.now() ? color.price - color.price * (product.offer.percent / 100) : color.price), 0).toLocaleString()}
                     <TomanIcon className="size-5" />
                   </span>
                 </div>
-                <Link to="/me/cart" className="rounded-2xl bg-primary-900 px-5 py-3 font-vazirmatn-medium text-white transition-colors hover:bg-primary-800">ثبت سفارش</Link>
+                <Link to="/me/cart" className="rounded-full bg-primary-900 px-5 py-3 font-vazirmatn-medium text-white transition-colors hover:bg-primary-800">ثبت سفارش</Link>
               </div>
             </div>
           )}
