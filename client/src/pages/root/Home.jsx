aef4b76be166dd99bebf014af795fb380db6942b
MeySaM <imeysamhazrati@gmail.com>
2024-03-19 23:59:07 +0330

    Create AmazingOffer component

--- client/src/pages/root/Home.jsx
+++ client/src/pages/root/Home.jsx
@@ -1,11 +1,13 @@
 import Landing from "../../components/root/Landing";
 import BestSellers from "../../components/root/BestSellers";
+import AmazingOffer from "../../components/root/AmazingOffer";
 
 const Home = () => {
   return (
     <>
       <Landing />
       <BestSellers />
+      <AmazingOffer />
     </>
   );
 };
