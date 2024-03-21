427b303c2375deb572836a38d6220585971d0f0e
MeySaM <imeysamhazrati@gmail.com>
2024-03-21 14:07:30 +0330

    Update axios category controller

--- client/src/axios/controllers/category.js
+++ client/src/axios/controllers/category.js
@@ -1,12 +1,12 @@
 import axios from "../config";
 
-const create = async ({ title, englishTitle }) => await axios.post("/categories", { title, englishTitle });
+const create = async ({ title, englishTitle, logo }) => await axios.postForm("/categories", { title, englishTitle, logo });
 
 const getAll = async (page, length) => await axios.get("/categories", { params: { page, length } });
 
 const get = async (title) => await axios.get(`/categories/${title}`);
 
-const update = async (id, { title, englishTitle }) => await axios.put(`/categories/${id}`, { title, englishTitle });
+const update = async (id, { title, englishTitle, logo }) => await axios.putForm(`/categories/${id}`, { title, englishTitle, logo });
 
 const remove = async (id) => await axios.delete(`/categories/${id}`);
 
