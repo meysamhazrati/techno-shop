d91022b8111e3734d0d9813cbd4cacbf14c20812
MeySaM <imeysamhazrati@gmail.com>
2024-03-21 13:56:35 +0330

    Update category routes

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
+import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
 import { create, getAll, get, update, remove } from "../controllers/category.js";
 
 const router = Router();
+const uploader = multer("categories");
 
-router.route("/").post(authentication, isAdmin, create).get(getAll);
+router.route("/").post(authentication, isAdmin, uploader.single("logo"), create).get(getAll);
 router.get("/:title", get);
-router.route("/:id").put(authentication, isAdmin, update).delete(authentication, isAdmin, remove);
+router.route("/:id").put(authentication, isAdmin, uploader.single("logo"), update).delete(authentication, isAdmin, remove);
 
 export default router;
