d6387d9a680eef64a32f73c0671b1b9be2c86f73
MeySaM <imeysamhazrati@gmail.com>
2024-03-26 14:11:41 +0330

    Update article routes

--- server/routes/article.js
+++ server/routes/article.js
@@ -3,14 +3,13 @@ import { Router } from "express";
 import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, getAll, get, getByCategory, update, publish, draft, confirm, reject, remove } from "../controllers/article.js";
+import { create, getAll, get, update, publish, draft, confirm, reject, remove } from "../controllers/article.js";
 
 const router = Router();
 const uploader = multer("articles");
 
 router.route("/").post(authentication, uploader.single("cover"), create).get(getAll);
 router.route("/:id").get(get).put(authentication, uploader.single("cover"), update).delete(authentication, remove);
-router.get("/categories/:id", getByCategory);
 router.put("/publish/:id", authentication, publish);
 router.put("/draft/:id", authentication, draft);
 router.put("/confirm/:id", authentication, isAdmin, confirm);
