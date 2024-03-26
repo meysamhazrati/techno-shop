84dff3dc5226ff61775efc72649c2ff0383cd620
MeySaM <imeysamhazrati@gmail.com>
2024-03-26 14:23:03 +0330

    Add axios article controller

---
+++ client/src/axios/controllers/article.js
@@ -0,0 +1,21 @@
+import axios from "../config";
+
+const create = async ({ title, cover, body, isPublished, category }) => await axios.postForm("/articles", { title, cover, body, isPublished, category });
+
+const getAll = async (categories, sort, page, length) => await axios.get("/articles", { params: { categories, sort, page, length } });
+
+const get = async (id) => await axios.get(`/articles/${id}`);
+
+const update = async (id, { title, cover, body, isPublished }) => await axios.putForm(`/articles/${id}`, { title, cover, body, isPublished });
+
+const publish = async (id) => await axios.put(`/articles/publish/${id}`);
+
+const draft = async (id) => await axios.put(`/articles/draft/${id}`);
+
+const confirm = async (id) => await axios.put(`/articles/confirm/${id}`);
+
+const reject = async (id) => await axios.put(`/articles/reject/${id}`);
+
+const remove = async (id) => await axios.delete(`/articles/${id}`);
+
+export { create, getAll, get, update, publish, draft, confirm, reject, remove };
