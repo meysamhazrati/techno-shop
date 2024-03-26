017582f05a419774c4975fe46b674381692033d2
MeySaM <imeysamhazrati@gmail.com>
2024-03-26 23:08:47 +0330

    Add DoubleChevronUp icon

---
+++ client/src/icons/DoubleChevronUp.jsx
@@ -0,0 +1,10 @@
+const DoubleChevronUp = ({ className }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={className}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
+    </svg>
+  );
+};
+
+export default DoubleChevronUp;
