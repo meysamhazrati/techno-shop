5bdaf28a17fd5b79141b186fa7fdb9e24d99b34a
MeySaM <imeysamhazrati@gmail.com>
2024-03-26 16:20:28 +0330

    Create Article component

---
+++ client/src/components/root/Article.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import UserIcon from "../../icons/User";
+import StarIcon from "../../icons/Star";
+import ChevronLeftIcon from "../../icons/ChevronLeft";
+
+const Article = ({ _id, cover, title, author, score }) => {
+  return (
+    <div className="overflow-hidden rounded-3xl border border-zinc-200 bg-white">
+      <Link to={`/articles/${_id}`} className="block h-48 w-full">
+        <img src={`http://localhost:3000/articles/${cover}`} alt="Product Cover" loading="lazy" className="size-full object-cover" />
+      </Link>
+      <div className="p-4">
+        <h3 className="h-16 font-vazirmatn-medium text-xl/relaxed">
+          <Link to={`/articles/${_id}`} className="line-clamp-2 transition-colors hover:text-primary-900">{title}</Link>
+        </h3>
+        <div className="my-4 flex items-center justify-between gap-x-5">
+          <div className="flex items-center gap-x-1">
+            <UserIcon className="size-5" />
+            <span>{author.firstName} {author.lastName}</span>
+          </div>
+          <div className="flex items-center gap-x-1 text-yellow-400">
+            <span className="font-vazirmatn-bold text-lg">{score}</span>
+            <StarIcon className="size-5" />
+          </div>
+        </div>
+        <div className="flex justify-center border-t border-zinc-200 pt-4">
+          <Link to={`/articles/${_id}`} className="inline-flex items-center gap-x-1 transition-colors hover:text-primary-900">
+            <span className="text-lg">مطالعه</span>
+            <ChevronLeftIcon className="size-5" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
