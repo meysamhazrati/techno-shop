8b9bed63f8df3541c0d7779fa99b1a8f9006ec8d
MeySaM <imeysamhazrati@gmail.com>
2024-03-27 14:33:01 +0330

    Update Category component

--- client/src/components/root/Category.jsx
+++ client/src/components/root/Category.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Category = ({ title, englishTitle, logo }) => {
   return (
     <div className="flex flex-col items-center gap-y-2">
-      <Link to={`/categories/${englishTitle.toLowerCase().split(" ").join("-")}`} className="size-32">
-        <img src={`http://localhost:3000/categories/${logo}`} alt="Category Logo" className="size-full object-cover" />
+      <Link to={`/categories/${englishTitle.toLowerCase().split(" ").join("-")}`} className="size-32 overflow-hidden rounded-full">
+        <img src={`http://localhost:3000/categories/${logo}`} alt="Category Logo" loading="lazy" className="size-full object-cover" />
       </Link>
       <h1 className="font-vazirmatn-medium text-lg">
-        <Link to={`/categories/${englishTitle.toLowerCase().split(" ").join("-")}`}>{title}</Link>
+        <Link to={`/categories/${englishTitle.toLowerCase().split(" ").join("-")}`} className="line-clamp-1">{title}</Link>
       </h1>
     </div>
   );
