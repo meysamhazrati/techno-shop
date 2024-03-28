f29967d24a1c0e8c314792e7604792ab54703a70
MeySaM <imeysamhazrati@gmail.com>
2024-03-28 12:39:54 +0330

    Update and fix useLogout hook

--- client/src/hooks/authentication/logout.js
+++ client/src/hooks/authentication/logout.js
@@ -1,20 +1,23 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { Context as ToastContext } from "../../contexts/Toast";
 import { logout } from "../../axios/controllers/authentication";
 
 export default () => {
   const client = useQueryClient();
+  const navigate = useNavigate();
 
   const { openToast } = useContext(ToastContext);
 
   const { mutate } = useMutation({
     mutationFn: logout,
     onSuccess: () => {
-      client.invalidateQueries(["me"]);
-      openToast("success", "موفق!", "با موفقیت از حساب کاربری خود خارج شدید.");
+      client.invalidateQueries({ queryKey: ["me"] });
+      openToast("success", null, "با موفقیت از حساب کاربری خود خارج شدید.");
+      navigate("/");
     },
-    onError: () => openToast("error", "خطا!", "مشکلی پیش آمد! لطفا بعدا دوباره تلاش کنید."),
+    onError: () => openToast("error"),
   });
 
   return { logout: mutate };
