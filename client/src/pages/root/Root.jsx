095011ad8d0392447d8acf865111b50ab7f22bdb
MeySaM <imeysamhazrati@gmail.com>
2024-03-28 16:23:31 +0330

    Update Root page

--- client/src/pages/root/Root.jsx
+++ client/src/pages/root/Root.jsx
@@ -1,17 +1,7 @@
 import { Outlet } from "react-router-dom";
-import Header from "../../components/root/Header";
-import Footer from "../../components/root/Footer";
 
 const Root = () => {
-  return (
-    <>
-      <Header />
-      <div className="container pt-20 lg:pt-32">
-        <Outlet />
-      </div>
-      <Footer />
-    </>
-  );
+  return <Outlet />;
 };
 
 export default Root;
