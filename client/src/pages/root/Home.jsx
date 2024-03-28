c64c1c950f83a94d186ef8013eea82f8c33c983d
MeySaM <imeysamhazrati@gmail.com>
2024-03-28 16:21:46 +0330

    Update Home page

--- client/src/pages/root/Home.jsx
+++ client/src/pages/root/Home.jsx
@@ -1,23 +1,31 @@
+import Header from "../../components/root/Header";
 import Landing from "../../components/root/Landing";
 import Latests from "../../components/root/Latests";
-import BestSellers from "../../components/root/BestSellers";
 import AmazingOffer from "../../components/root/AmazingOffer";
 import Categories from "../../components/root/Categories";
 import Banners from "../../components/root/Banners";
+import BestSellers from "../../components/root/BestSellers";
 import Brands from "../../components/root/Brands";
 import Articles from "../../components/root/Articles";
+import MobileNavigation from "../../components/MobileNavigation";
+import Footer from "../../components/root/Footer";
 
 const Home = () => {
   return (
     <>
-      <Landing />
-      <Latests />
-      <AmazingOffer />
-      <Categories />
-      <Banners />
-      <BestSellers />
-      <Brands />
-      <Articles />
+      <Header />
+      <div className="container pb-20 pt-28 xs:pb-24 lg:pb-12 lg:pt-44">
+        <Landing />
+        <Latests />
+        <AmazingOffer />
+        <Categories />
+        <Banners />
+        <BestSellers />
+        <Brands />
+        <Articles />
+      </div>
+      <MobileNavigation />
+      <Footer />
     </>
   );
 };
