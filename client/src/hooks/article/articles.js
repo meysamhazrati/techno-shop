33631f037bd3b4cd4d7b86f93855f7a5e325e741
MeySaM <imeysamhazrati@gmail.com>
2024-03-31 17:06:48 +0330

    Update useArticles hook

--- client/src/hooks/article/articles.js
+++ client/src/hooks/article/articles.js
@@ -1,21 +1,21 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getAll } from "../../axios/controllers/article";
 
-const shouldRetry = (error) => error.response.status !== 404;
+const shouldRetry = ({ response }) => response.status !== 404;
 const shouldRefetch = ({ state }) => state.error?.response?.status !== 404;
 
 export default (categories, sort, length) => {
   const { isFetching, isError, data, hasNextPage, fetchNextPage } = useInfiniteQuery({
-    queryKey: ["articles", categories, sort],
+    queryKey: ["articles", { categories, sort }],
     queryFn: ({ pageParam }) => getAll(categories, sort, pageParam, length),
     initialPageParam: 1,
-    getNextPageParam: (lastPage) => lastPage.data.nextPage,
+    getNextPageParam: ({ data }) => data.nextPage,
     retry: (failureCount, error) => shouldRetry(error) && failureCount < 2,
     refetchOnMount: shouldRefetch,
     refetchOnReconnect: shouldRefetch,
     refetchOnWindowFocus: shouldRefetch,
-    select: (data) => data.pages.flatMap((page) => page.data.articles),
+    select: ({ pages }) => ({ articles: pages.flatMap(({ data }) => data.articles), total: pages[pages.length - 1].data.total }),
   });
 
-  return { isFetchingArticles: isFetching, isArticlesError: isError, articles: data, hasArticlesNextPage: hasNextPage, fetchArticlesNextPage: fetchNextPage };
+  return { isFetchingArticles: isFetching, isArticlesError: isError, articles: data?.articles, total: data?.total, hasArticlesNextPage: hasNextPage, fetchArticlesNextPage: fetchNextPage };
 };
