1731c54ee192f677e311071e753994c114d5be3e
MeySaM <imeysamhazrati@gmail.com>
2024-03-31 23:51:21 +0330

    Fix scroll problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, Outlet } from "react-router-dom";
 import ToastProvider from "./contexts/Toast";
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => window.scrollTo(0, 0), [location]);
+
   return (
     <ToastProvider>
       <Outlet />
