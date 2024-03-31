faeeb310db84f6952599338c1985736b829d9057
MeySaM <imeysamhazrati@gmail.com>
2024-03-31 22:57:49 +0330

    Update BestSellers component

--- client/src/components/root/BestSellers.jsx
+++ client/src/components/root/BestSellers.jsx
@@ -1,33 +1,21 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
 import useProducts from "../../hooks/product/products";
+import Section from "./Section";
+import SectionHeader from "./SectionHeader";
 import Product from "./Product";
 import ProductSkeleton from "./ProductSkeleton";
-import NoProductFound from "../NoProductFound";
-import ChevronRightIcon from "../../icons/ChevronRight";
-import ChevronLeftIcon from "../../icons/ChevronLeft";
+import NoResultFound from "../NoResultFound";
 import "swiper/css";
 
 const BestSellers = () => {
-  const { isFetchingProducts, isProductsError, products } = useProducts("best-sellers", 10);
+  const { isFetchingProducts, isProductsError, products } = useProducts(null, null, null, null, null, null, "best-seller", 10);
 
   return (
-    <section className="mt-12">
-      <div className="flex flex-wrap items-center justify-between gap-2">
-        <h2 className="font-vazirmatn-bold text-3xl">پرفروش‌ترین ها</h2>
-        {products?.length > 4 && (
-          <div className="mr-auto flex items-center gap-x-3 text-primary-900">
-            <button className="swiper-button-prev flex size-12 items-center justify-center rounded-full border border-primary-900 transition-colors hover:bg-primary-50">
-              <ChevronRightIcon className="size-6" />
-            </button>
-            <button className="swiper-button-next flex size-12 items-center justify-center rounded-full border border-primary-900 transition-colors hover:bg-primary-50">
-              <ChevronLeftIcon className="size-6" />
-            </button>
-          </div>
-        )}
-      </div>
+    <Section>
+      <SectionHeader title="پرفروش‌ترین ها" condition={products?.length > 4} navigation={true} />
       {isProductsError ? (
-        <NoProductFound />
+        <NoResultFound title="محصولی پیدا نشد!" />
       ) : (
         <Swiper
           slidesPerView={1}
@@ -41,20 +29,20 @@ const BestSellers = () => {
             1280: { slidesPerView: 4 },
           }}
           modules={[Autoplay, Navigation]}
-          className="mt-10"
+          className="mt-8"
         >
           {isFetchingProducts ? Array(5).fill(0).map((product, index) => (
             <SwiperSlide key={index}>
               <ProductSkeleton />
             </SwiperSlide>
-          )) : products?.map((product) => (
+          )) : products.map((product) => (
             <SwiperSlide key={product._id}>
               <Product {...product} />
             </SwiperSlide>
           ))}
         </Swiper>
       )}
-    </section>
+    </Section>
   );
 };
 
