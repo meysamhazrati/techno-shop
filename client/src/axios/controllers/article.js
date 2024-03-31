4b9b1af924c8173f0ef4bf6c447a3d93f6f1b457
MeySaM <imeysamhazrati@gmail.com>
2024-03-31 17:02:49 +0330

    Update axios article controller

--- client/src/axios/controllers/article.js
+++ client/src/axios/controllers/article.js
@@ -4,7 +4,7 @@ const create = async ({ title, cover, body, isPublished, category }) => await ax
 
 const getAll = async (categories, sort, page, length) => await axios.get("/articles", { params: { categories, sort, page, length } });
 
-const get = async (id) => await axios.get(`/articles/${id}`);
+const get = async (id, commentsPage, commentsLength) => await axios.get(`/articles/${id}`, { params: { "comments-page": commentsPage, "comments-length": commentsLength } });
 
 const update = async (id, { title, cover, body, isPublished }) => await axios.putForm(`/articles/${id}`, { title, cover, body, isPublished });
 
