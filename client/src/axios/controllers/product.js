477cdc3c1c8ee40e328efff93cb9efcce80f63b7
MeySaM <imeysamhazrati@gmail.com>
2024-03-31 16:50:06 +0330

    Update axios product controller

--- client/src/axios/controllers/product.js
+++ client/src/axios/controllers/product.js
@@ -2,18 +2,12 @@ import axios from "../config";
 
 const create = async (type, body) => await axios.postForm("/products", body, { params: { type } });
 
-const getAll = async (sort, page, length) => await axios.get("/products", { params: { sort, page, length } });
+const getAll = async (search, brands, categories, onlyAvailable, onlyAmazing, range, sort, page, length) => await axios.get("/products", { params: { search, brands, categories, "only-available": onlyAvailable, "only-amazing": onlyAmazing, range, sort, page, length } });
 
-const getByCategory = async (title, sort, page, length) => await axios.get(`/products/categories/${title}`, { params: { sort, page, length } });
-
-const getByOffer = async (id, sort, page, length) => await axios.get(`/products/offers/${id}`, { params: { sort, page, length } });
-
-const getBySearch = async (title, sort, page, length) => await axios.get(`/products/search/${title}`, { params: { sort, page, length } });
-
-const get = async (id) => await axios.get(`/products/${id}`);
+const get = async (id, commentsPage, commentsLength) => await axios.get(`/products/${id}`, { params: { "comments-page": commentsPage, "comments-length": commentsLength } });
 
 const update = async (id, type, body) => await axios.putForm(`/products/${id}`, body, { params: { type } });
 
 const remove = async (id) => await axios.delete(`/products/${id}`);
 
-export { create, getAll, getByCategory, getByOffer, getBySearch, get, update, remove };
+export { create, getAll, get, update, remove };
