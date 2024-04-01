c0ee7f44fb77c87a07c1c288b7e6aaf95d7b3236
MeySaM <imeysamhazrati@gmail.com>
2024-04-01 19:40:12 +0330

    Update brand routes

--- server/routes/brand.js
+++ server/routes/brand.js
@@ -3,12 +3,13 @@ import { Router } from "express";
 import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, getAll, update, remove } from "../controllers/brand.js";
+import { create, getAll, get, update, remove } from "../controllers/brand.js";
 
 const router = Router();
 const uploader = multer("brands");
 
 router.route("/").post(authentication, isAdmin, uploader.single("logo"), create).get(getAll);
+router.get("/:name", get);
 router.route("/:id").put(authentication, isAdmin, uploader.single("logo"), update).delete(authentication, isAdmin, remove);
 
 export default router;
