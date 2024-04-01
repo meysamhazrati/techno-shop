3bdfd7eff4b1a722cc97691e3126573783564d04
MeySaM <imeysamhazrati@gmail.com>
2024-04-01 19:44:32 +0330

    Update axios brand controller

--- client/src/axios/controllers/brand.js
+++ client/src/axios/controllers/brand.js
@@ -4,8 +4,10 @@ const create = async ({ name, englishName, logo }) => await axios.postForm("/bra
 
 const getAll = async (page, length) => await axios.get("/brands", { params: { page, length } });
 
+const get = async (name, productsCategories, onlyAvailableProducts, onlyAmazingProducts, productsRange, productsSort, productsPage, productsLength ) => await axios.get(`/brands/${name}`, { params: { "products-categories": productsCategories, "only-available-products": onlyAvailableProducts, "only-amazing-products": onlyAmazingProducts, "products-range": productsRange, "products-sort": productsSort, "products-page": productsPage, "products-length": productsLength } });
+
 const update = async (id, { name, englishName, logo }) => await axios.putForm(`/brands/${id}`, { name, englishName, logo });
 
 const remove = async (id) => await axios.delete(`/brands/${id}`);
 
-export { create, getAll, update, remove };
+export { create, getAll, get, update, remove };
