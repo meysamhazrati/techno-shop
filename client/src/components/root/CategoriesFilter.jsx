ac1efc461a1f3acda3632864f13ba5e94fd3c73d
MeySaM <imeysamhazrati@gmail.com>
2024-04-04 23:37:45 +0330

    Update CategoriesFilter component

--- client/src/components/root/CategoriesFilter.jsx
+++ client/src/components/root/CategoriesFilter.jsx
@@ -3,21 +3,21 @@ import { useSearchParams } from "react-router-dom";
 import useCategories from "../../hooks/category/categories";
 import ChevronIcon from "../../icons/Chevron";
 
-const CategoriesFilter = ({ setFilter }) => {
+const CategoriesFilter = ({ setCategories }) => {
   const [searchParams] = useSearchParams();
   const [isOpen, setIsOpen] = useState(false);
 
   const { isFetchingCategories, isCategoriesError, categories } = useCategories();
 
   return (
-    <div className="pt-3">
+    <div className="pt-4">
       <div className="flex w-full items-center justify-between">
         <span className="text-xl">دسته‌بندی‌ ها</span>
         <button className="flex size-8 items-center justify-center rounded-full transition-colors hover:bg-zinc-200 hover:text-zinc-700" onClick={() => setIsOpen((isOpen) => !isOpen)}>
           <ChevronIcon className={`size-5 ${isOpen ? "-rotate-90" : "rotate-90"} transition-transform`} />
         </button>
       </div>
-      <div className={`mt-3 ${isOpen ? "max-h-28 border-t border-zinc-200 py-2" : "max-h-0 border-none py-0"} flex flex-col gap-y-1 overflow-auto px-2 transition-all`}>
+      <div className={`mt-4 ${isOpen ? "max-h-28 pb-3" : "max-h-0"} flex flex-col gap-y-1 overflow-auto px-3 transition-all`}>
         {isCategoriesError ? (
           <span className="text-center text-lg">دسته‌بندی‌ای پیدا نشد!</span>
         ) :isFetchingCategories ? Array(4).fill(0).map((category, index) => (
@@ -33,8 +33,8 @@ const CategoriesFilter = ({ setFilter }) => {
             <input
               type="checkbox"
               checked={searchParams.get("categories")?.split(",").some((category) => category === englishTitle.toLowerCase().split(" ").join("-")) || false}
-              className="size-4 shrink-0 cursor-pointer appearance-none rounded bg-zinc-200 checked:bg-primary-900"
-              onChange={({ target }) => setFilter((categories) => target.checked ? [...categories, englishTitle.toLowerCase().split(" ").join("-")] : categories.filter((category) => category !== englishTitle.toLowerCase().split(" ").join("-")))}
+              className="size-4 shrink-0 cursor-pointer appearance-none rounded bg-zinc-200 transition-colors checked:bg-primary-900"
+              onChange={({ target }) => setCategories((categories) => target.checked ? [...categories, englishTitle.toLowerCase().split(" ").join("-")] : categories.filter((category) => category !== englishTitle.toLowerCase().split(" ").join("-")))}
             />
             <div className="flex w-full items-center justify-between gap-x-3">
               <span className="text-lg">{title}</span>
