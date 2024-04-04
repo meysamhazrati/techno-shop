8d9e23f078c64dac4215f03ae565c0b90abcaa56
MeySaM <imeysamhazrati@gmail.com>
2024-04-04 23:33:13 +0330

    Update BrandsFilter component

--- client/src/components/root/BrandsFilter.jsx
+++ client/src/components/root/BrandsFilter.jsx
@@ -3,21 +3,21 @@ import { useSearchParams } from "react-router-dom";
 import useBrands from "../../hooks/brand/brands";
 import ChevronIcon from "../../icons/Chevron";
 
-const BrandsFilter = ({ setFilter }) => {
+const BrandsFilter = ({ setBrands }) => {
   const [searchParams] = useSearchParams();
   const [isOpen, setIsOpen] = useState(false);
 
   const { isFetchingBrands, isBrandsError, brands } = useBrands();
 
   return (
-    <div className="pt-3">
+    <div className="pt-4">
       <div className="flex w-full items-center justify-between">
         <span className="text-xl">برند ها</span>
         <button className="flex size-8 items-center justify-center rounded-full transition-colors hover:bg-zinc-200 hover:text-zinc-700" onClick={() => setIsOpen((isOpen) => !isOpen)}>
           <ChevronIcon className={`size-5 ${isOpen ? "-rotate-90" : "rotate-90"} transition-transform`} />
         </button>
       </div>
-      <div className={`mt-3 ${isOpen ? "max-h-28 border-t border-zinc-200 py-2" : "max-h-0 border-none py-0"} flex flex-col gap-y-1 overflow-auto px-2 transition-all`}>
+      <div className={`mt-4 ${isOpen ? "max-h-28 pb-3" : "max-h-0"} flex flex-col gap-y-1 overflow-auto px-3 transition-all`}>
         {isBrandsError ? (
           <span className="text-center text-lg">برندی پیدا نشد!</span>
         ) : isFetchingBrands ? Array(4).fill(0).map((brand, index) => (
@@ -33,8 +33,8 @@ const BrandsFilter = ({ setFilter }) => {
             <input
               type="checkbox"
               checked={searchParams.get("brands")?.split(",").some((brand) => brand === englishName.toLowerCase().split(" ").join("-")) || false}
-              className="size-4 shrink-0 cursor-pointer appearance-none rounded bg-zinc-200 checked:bg-primary-900"
-              onChange={({ target }) => setFilter((brands) => target.checked ? [...brands, englishName.toLowerCase().split(" ").join("-")] : brands.filter((brand) => brand !== englishName.toLowerCase().split(" ").join("-")))}
+              className="size-4 shrink-0 cursor-pointer appearance-none rounded bg-zinc-200 transition-colors checked:bg-primary-900"
+              onChange={({ target }) => setBrands((brands) => target.checked ? [...brands, englishName.toLowerCase().split(" ").join("-")] : brands.filter((brand) => brand !== englishName.toLowerCase().split(" ").join("-")))}
             />
             <div className="flex w-full items-center justify-between gap-x-3">
               <span className="text-lg">{name}</span>
