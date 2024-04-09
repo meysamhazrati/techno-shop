c54abf603804482b2eccfaea33dbc9e478498ad7
MeySaM <imeysamhazrati@gmail.com>
2024-04-09 23:17:41 +0330

    Update Root page

--- client/src/pages/root/Root.jsx
+++ client/src/pages/root/Root.jsx
@@ -1,7 +1,19 @@
 import { Outlet } from "react-router-dom";
+import Header from "../../components/root/Header";
+import MobileNavigation from "../../components/MobileNavigation";
+import Footer from "../../components/root/Footer";
 
 const Root = () => {
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <main className="container flex min-h-svh flex-col justify-center pb-[72px] pt-24 xs:pb-[88px] lg:min-h-max lg:pb-12 lg:pt-44">
+        <Outlet />
+      </main>
+      <MobileNavigation />
+      <Footer />
+    </>
+  );
 };
 
 export default Root;
