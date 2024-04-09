184c6687665fa17b1bdbcd2c0c013030f4f54fe1
MeySaM <imeysamhazrati@gmail.com>
2024-04-09 23:42:34 +0330

    Update other pages

--- client/src/pages/root/Categories.jsx
+++ client/src/pages/root/Categories.jsx
@@ -2,14 +2,11 @@ import { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import useCategories from "../../hooks/category/categories";
-import Header from "../../components/root/Header";
 import Section from "../../components/root/Section";
 import SectionHeader from "../../components/root/SectionHeader";
 import CategorySection from "../../components/root/CategorySection";
 import ProductSkeleton from "../../components/root/ProductSkeleton";
 import NoResultFound from "../../components/NoResultFound";
-import MobileNavigation from "../../components/MobileNavigation";
-import Footer from "../../components/root/Footer";
 
 const Categories = () => {
   const { isFetchingCategories, isCategoriesError, categories } = useCategories(7);
@@ -18,44 +15,35 @@ const Categories = () => {
     document.title = "تکنوشاپ - دسته‌بندی‌ ها";
   }, []);
 
-  return (
-    <>
-      <Header />
-      <main className="container flex min-h-screen flex-col justify-center pb-20 pt-24 xs:pb-24 lg:min-h-max lg:pb-12 lg:pt-44">
-        {isCategoriesError ? (
-          <NoResultFound title="دسته‌بندی‌ای پیدا نشد!" />
-        ) : isFetchingCategories ? (
-          Array(3).fill(0).map((category, index) => (
-            <Section key={index}>
-              <SectionHeader title="در حال بارگذاری" condition={false} />
-                {<Swiper
-                  slidesPerView={1}
-                  spaceBetween={16}
-                  loop={true}
-                  autoplay={{ delay: 2000 }}
-                  breakpoints={{
-                    640: { slidesPerView: 2 },
-                    1024: { slidesPerView: 3 },
-                    1280: { slidesPerView: 4 },
-                  }}
-                  modules={[Autoplay]}
-                  className="mt-8"
-                >
-                  {Array(5).fill(0).map((product, index) => (
-                    <SwiperSlide key={index}>
-                      <ProductSkeleton />
-                    </SwiperSlide>
-                  ))}
-                </Swiper>}
-              </Section>
-            ))
-        ) : (
-          categories.map((category) => <CategorySection key={category._id} {...category} />)
-        )}
-      </main>
-      <MobileNavigation />
-      <Footer />
-    </>
+  return isCategoriesError ? (
+    <NoResultFound title="دسته‌بندی‌ای پیدا نشد!" />
+  ) : isFetchingCategories ? (
+    Array(3).fill(0).map((category, index) => (
+      <Section key={index}>
+        <SectionHeader title="در حال بارگذاری" condition={false} />
+        {<Swiper
+          slidesPerView={1}
+          spaceBetween={16}
+          loop={true}
+          autoplay={{ delay: 2000 }}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+            1280: { slidesPerView: 4 },
+          }}
+          modules={[Autoplay]}
+          className="mt-8"
+        >
+          {Array(5).fill(0).map((product, index) => (
+            <SwiperSlide key={index}>
+              <ProductSkeleton />
+            </SwiperSlide>
+          ))}
+        </Swiper>}
+      </Section>
+      ))
+  ) : (
+    categories.map((category) => <CategorySection key={category._id} {...category} />)
   );
 };
 
--- client/src/pages/root/Home.jsx
+++ client/src/pages/root/Home.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import Header from "../../components/root/Header";
 import Landing from "../../components/root/Landing";
 import Latests from "../../components/root/Latests";
 import AmazingOffer from "../../components/root/AmazingOffer";
@@ -8,8 +7,6 @@ import Banners from "../../components/root/Banners";
 import BestSellers from "../../components/root/BestSellers";
 import Brands from "../../components/root/Brands";
 import Articles from "../../components/root/Articles";
-import MobileNavigation from "../../components/MobileNavigation";
-import Footer from "../../components/root/Footer";
 
 const Home = () => {
   useEffect(() => {
@@ -18,19 +15,14 @@ const Home = () => {
 
   return (
     <>
-      <Header />
-      <main className="container pb-20 pt-28 xs:pb-24 lg:pb-12 lg:pt-44">
-        <Landing />
-        <Latests />
-        <AmazingOffer />
-        <Categories />
-        <Banners />
-        <BestSellers />
-        <Brands />
-        <Articles />
-      </main>
-      <MobileNavigation />
-      <Footer />
+      <Landing />
+      <Latests />
+      <AmazingOffer />
+      <Categories />
+      <Banners />
+      <BestSellers />
+      <Brands />
+      <Articles />
     </>
   );
 };
