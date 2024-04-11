294d028a114154167a9a477e55d9d752dbeb9a9a
MeySaM <imeysamhazrati@gmail.com>
2024-04-11 16:21:33 +0330

    Create Brands page

---
+++ client/src/pages/root/Brands.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import useBrands from "../../hooks/brand/brands";
+import Section from "../../components/root/Section";
+import SectionHeader from "../../components/root/SectionHeader";
+import BrandSection from "../../components/root/BrandSection";
+import ProductSkeleton from "../../components/root/ProductSkeleton";
+import NoResultFound from "../../components/NoResultFound";
+
+const Brands = () => {
+  const { isFetchingBrands, isBrandsError, brands } = useBrands();
+
+  useEffect(() => {
+    document.title = "تکنوشاپ - برند ها";
+  }, []);
+
+  return isBrandsError ? (
+    <NoResultFound title="برندی پیدا نشد!" />
+  ) : isFetchingBrands ? (
+    Array(3).fill(0).map((brand, index) => (
+      <Section key={index}>
+        <SectionHeader title="در حال بارگذاری" condition={false} />
+        {<Swiper
+          slidesPerView={1}
+          spaceBetween={16}
+          loop={true}
+          autoplay={{ delay: 2000 }}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+            1280: { slidesPerView: 4 },
+          }}
+          modules={[Autoplay]}
+          className="mt-8"
+        >
+          {Array(5).fill(0).map((product, index) => (
+            <SwiperSlide key={index}>
+              <ProductSkeleton />
+            </SwiperSlide>
+          ))}
+        </Swiper>}
+      </Section>
+      ))
+  ) : (
+    brands.map((brand) => <BrandSection key={brand._id} {...brand} />)
+  );
+};
+
+export default Brands;
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -3,6 +3,7 @@ import Root from "./pages/root/Root";
 import Home from "./pages/root/Home";
 import Categories from "./pages/root/Categories";
 import Category from "./pages/root/Category";
+import Brands from "./pages/root/Brands";
 import Error from "./pages/Error";
 import NotFound from "./pages/NotFound";
 
@@ -19,6 +20,7 @@ export default [
           { index: true, element: <Home /> },
           { path: "categories", element: <Categories /> },
           { path: "categories/:title", element: <Category /> },
+          { path: "brands", element: <Brands /> },
         ],
       },
       {
