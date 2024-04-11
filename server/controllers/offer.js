d0ab252b42d3810f325e9dad742ce5bb41d31473
MeySaM <imeysamhazrati@gmail.com>
2024-04-11 17:01:34 +0330

    Update offer controller

--- server/controllers/offer.js
+++ server/controllers/offer.js
@@ -1,14 +1,17 @@
 import model from "../models/offer.js";
 import productModel from "../models/product.js";
+import brandModel from "../models/brand.js";
+import categoryModel from "../models/category.js";
 
 const create = async (request, response, next) => {
   try {
     await model.createValidation(request.body);
 
-    const { title, description, percent, expiresAt, categories } = request.body;
+    const { title, englishTitle, description, percent, expiresAt, categories } = request.body;
 
     const { _id } = await model.create({
       title,
+      englishTitle,
       description,
       percent,
       expiresAt: Date.now() + 1000 * 60 * 60 * expiresAt,
@@ -49,16 +52,84 @@ const getAll = async (request, response, next) => {
   }
 };
 
+const get = async (request, response, next) => {
+  try {
+    const { title } = request.params;
+    const { "products-brands": productsBrands, "products-categories": productsCategories, "products-price": productsPrice = "0-1000000000", "only-available-products": onlyAvailableProducts = false, "products-sort": productsSort, "products-page": productsPage = 1, "products-length": productsLength } = request.query;
+
+    const productsFilteredBrands = productsBrands?.trim() ? await Promise.all(productsBrands.trim().split(",").map(async (name) => (await brandModel.findOne({ englishName: { $regex: new RegExp(`^${name.split("-").join(" ")}$`, "i") } }))?._id)) : undefined;
+    const productsFilteredCategories = productsCategories?.trim() ? await Promise.all(productsCategories.trim().split(",").map(async (title) => (await categoryModel.findOne({ englishTitle: { $regex: new RegExp(`^${title.split("-").join(" ")}$`, "i") } }))?._id)) : undefined;
+
+    const offer = await model.findOne({ englishTitle: { $regex: new RegExp(`^${title.split("-").join(" ")}$`, "i") } }, "-__v").populate({ path: "products", select: "title covers", match: { brand: productsFilteredBrands, category: productsFilteredCategories }, populate: [
+      { path: "colors", select: "price sales inventory name code", options: { sort: { price: 1 } } },
+      { path: "brand", select: "-__v" },
+      { path: "category", select: "-__v" },
+      { path: "comments", select: "score" },
+    ] }).lean();
+
+    if (offer && offer.expiresAt > new Date()) {
+      const filteredProducts = offer.products.filter(({ colors }) => colors[0].price >= (productsPrice.split("-")[0] || 0) && colors[0].price <= (productsPrice.split("-")[1] || 1000000000) && (JSON.parse(onlyAvailableProducts) ? colors[0].inventory !== 0 : true));
+      let products = [];
+      let totalProducts = 0;
+      let nextProductsPage = null;
+
+      if (filteredProducts.length) {
+        const currentProductsPage = parseInt(productsPage);
+        const productsLengthPerPage = parseInt(productsLength) || filteredProducts.length;
+
+        const productsStartIndex = (currentProductsPage - 1) * productsLengthPerPage;
+        const productsEndIndex = productsStartIndex + productsLengthPerPage;
+
+        let sortedProducts = [];
+
+        switch (productsSort) {
+          case "best-seller": {
+            sortedProducts = filteredProducts.toSorted((firstProduct, secondProduct) => secondProduct.colors.reduce((previous, { sales }) => previous + sales, 0) - firstProduct.colors.reduce((previous, { sales }) => previous + sales, 0));
+            break;
+          }
+          case "popular": {
+            sortedProducts = filteredProducts.toSorted((firstProduct, secondProduct) => parseFloat((secondProduct.comments.reduce((previous, { score }) => previous + score, 5) / (secondProduct.comments.length + 1) || 5).toFixed(1)) - parseFloat((firstProduct.comments.reduce((previous, { score }) => previous + score, 5) / (firstProduct.comments.length + 1) || 5).toFixed(1)));
+            break;
+          }
+          case "cheap": {
+            sortedProducts = filteredProducts.toSorted((firstProduct, secondProduct) => firstProduct.colors[0].price - secondProduct.colors[0].price);
+            break;
+          }
+          case "expensive": {
+            sortedProducts = filteredProducts.toSorted((firstProduct, secondProduct) => secondProduct.colors[0].price - firstProduct.colors[0].price);
+            break;
+          }
+          default: {
+            sortedProducts = filteredProducts.toSorted((firstProduct, secondProduct) => secondProduct.createdAt - firstProduct.createdAt);
+            break;
+          }
+        }
+
+        products = sortedProducts.slice(productsStartIndex, productsEndIndex).map(({ comments, ...product }) => ({ ...product, score: parseFloat((comments.reduce((previous, { score }) => previous + score, 5) / (comments.length + 1) || 5).toFixed(1)) }));
+        totalProducts = sortedProducts.length;
+        nextProductsPage = productsEndIndex < sortedProducts.length ? currentProductsPage + 1 : null;
+      }
+
+      response.json({ ...offer, products, totalProducts, nextProductsPage });
+    } else {
+      throw Object.assign(new Error("The offer was not found."), { status: 404 });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 const update = async (request, response, next) => {
   try {
     await model.updateValidation(request.body);
 
     const { id } = request.params;
 
-    const { title, description, percent, expiresAt } = request.body;
+    const { title, englishTitle, description, percent, expiresAt } = request.body;
 
     const result = await model.findByIdAndUpdate(id, {
       title,
+      englishTitle,
       description,
       percent,
       expiresAt: Date.now() + 1000 * 60 * 60 * expiresAt,
@@ -81,6 +152,8 @@ const remove = async (request, response, next) => {
     const result = await model.findByIdAndDelete(id);
 
     if (result) {
+      await productModel.findOneAndUpdate({ offer: id }, { $unset: { offer: true } });
+
       response.json({ message: "The offer has been successfully removed." });
     } else {
       throw Object.assign(new Error("The offer was not found."), { status: 404 });
@@ -90,4 +163,4 @@ const remove = async (request, response, next) => {
   }
 };
 
-export { create, getAll, update, remove };
+export { create, getAll, get, update, remove };
