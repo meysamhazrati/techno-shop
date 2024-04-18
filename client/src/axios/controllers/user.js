ecdd85968e4082269e9a8125ffde832a21ee4947
MeySaM <imeysamhazrati@gmail.com>
2024-04-18 16:12:47 +0330

    Update axios user controller

--- client/src/axios/controllers/user.js
+++ client/src/axios/controllers/user.js
@@ -4,23 +4,9 @@ const getAll = async (page, length) => await axios.get("/users", { params: { pag
 
 const get = async (id) => await axios.get(`/users/${id}`);
 
-const edit = async ({ firstName, lastName, currentPassword, newPassword, avatar }) => await axios.putForm("/users", {
-  firstName,
-  lastName,
-  currentPassword,
-  newPassword,
-  avatar,
-});
+const edit = async ({ firstName, lastName, currentPassword, newPassword, avatar }) => await axios.putForm("/users", { firstName, lastName, currentPassword, newPassword, avatar });
 
-const update = async (id, { firstName, lastName, phone, email, password, role, avatar }) => await axios.putForm(`/users/${id}`, {
-  firstName,
-  lastName,
-  phone,
-  email,
-  password,
-  role,
-  avatar,
-});
+const update = async (id, { firstName, lastName, phone, email, password, role, avatar }) => await axios.putForm(`/users/${id}`, { firstName, lastName, phone, email, password, role, avatar });
 
 const addToCart = async (id, { color }) => await axios.put(`/users/cart/add/${id}`, { color });
 
@@ -36,7 +22,7 @@ const emptyFavorites = async () => await axios.put("/users/favorites/empty");
 
 const ban = async (id) => await axios.put(`/users/ban/${id}`);
 
-const unBan = async (id) => await axios.put(`/users/unBan/${id}`);
+const unBan = async (id) => await axios.put(`/users/un-ban/${id}`);
 
 const remove = async (id) => await axios.delete(`/users/${id}`);
 
