46c02c93a596ab56c4346c5b97c67cb67fac07d2
MeySaM <imeysamhazrati@gmail.com>
2024-04-25 23:02:50 +0330

    Update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  define: {
+    "process.env.SERVER_URI": JSON.stringify(loadEnv(mode, process.cwd(), "").SERVER_URI),
+  },
+  server: {
+    port: 8080,
+  },
   plugins: [react()],
-});
+}));
