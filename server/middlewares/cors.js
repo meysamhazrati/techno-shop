9d214616afe3e20d26bc890d59c67f29ebc3173a
MeySaM <imeysamhazrati@gmail.com>
2024-04-25 22:36:34 +0330

    Update cors Access-Control-Allow-Origin

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,5 +1,5 @@
 const middleware = (request, response, next) => {
-  response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  response.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URI);
   response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   response.setHeader("Access-Control-Allow-Credentials", true);
