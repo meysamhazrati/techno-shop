7760c5e713c3a583e47ff534e50b2b3f36cd07f9
MeySaM <imeysamhazrati@gmail.com>
2024-04-26 19:57:57 +0330

    Update authentication routes

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 
 import authentication from "../middlewares/authentication.js";
-import { send, verify, register, login, resetPassword, me, logout } from "../controllers/authentication.js";
+import { sendOTP, verifyOTP, register, login, resetPassword, me, logout } from "../controllers/authentication.js";
 
 const router = Router();
 
-router.post("/send", send);
-router.post("/verify", verify);
+router.post("/otp/send", sendOTP);
+router.post("/otp/verify", verifyOTP);
 router.post("/register", register);
 router.post("/login", login);
-router.put("/resetPassword", authentication, resetPassword);
+router.put("/reset-password", resetPassword);
 router.get("/me", authentication, me);
 router.delete("/logout", authentication, logout);
 
