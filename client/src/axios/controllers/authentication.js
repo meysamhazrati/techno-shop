d944e3065a1fdde1fa0bca16d50b20318a16af10
MeySaM <imeysamhazrati@gmail.com>
2024-04-26 20:06:07 +0330

    Update axios authentication controller

--- client/src/axios/controllers/authentication.js
+++ client/src/axios/controllers/authentication.js
@@ -1,22 +1,17 @@
 import axios from "../config";
 
-const send = async ({ email }, type) => await axios.post("/authentication/send", { email }, { params: { type } });
+const sendOTP = async ({ email }, type) => await axios.post("/authentication/otp/send", { email }, { params: { type } });
 
-const verify = async ({ email, code }) => await axios.post("/authentication/verify", { email, code });
+const verifyOTP = async ({ email, code }) => await axios.post("/authentication/otp/verify", { email, code });
 
-const register = async ({ firstName, lastName, email, password }) => await axios.post("/authentication/register", {
-  firstName,
-  lastName,
-  email,
-  password,
-});
+const register = async ({ firstName, lastName, email, password }) => await axios.post("/authentication/register", { firstName, lastName, email, password });
 
 const login = async ({ email, password }) => await axios.post("/authentication/login", { email, password });
 
-const resetPassword = async ({ password }) => await axios.put("/authentication/resetPassword", { password });
+const resetPassword = async ({ email, password }) => await axios.put("/authentication/reset-password", { email, password });
 
 const me = async () => await axios.get("/authentication/me");
 
 const logout = async () => await axios.delete("/authentication/logout");
 
-export { send, verify, register, login, resetPassword, me, logout };
+export { sendOTP, verifyOTP, register, login, resetPassword, me, logout };
