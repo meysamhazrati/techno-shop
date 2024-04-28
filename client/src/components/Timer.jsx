6180e59ef74cb0d802266da4fb280208c4bbee28
MeySaM <imeysamhazrati@gmail.com>
2024-04-28 21:11:50 +0330

    Update Timer component

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const Timer = ({ expiresAt }) => {
+const Timer = ({ day = true, hour = true, minute = true, expiresAt }) => {
   const [days, setDays] = useState("00");
   const [hours, setHours] = useState("00");
   const [minutes, setMinutes] = useState("00");
@@ -13,9 +13,9 @@ const Timer = ({ expiresAt }) => {
       difference = Date.parse(expiresAt) - Date.now();
 
       if (difference > 0) {
-        setDays(Math.floor(difference / (1000 * 60 * 60 * 24)).toString().padStart(2, "0"));
-        setHours(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, "0"));
-        setMinutes(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, "0"));
+        day && setDays(Math.floor(difference / (1000 * 60 * 60 * 24)).toString().padStart(2, "0"));
+        hour && setHours(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, "0"));
+        minute && setMinutes(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, "0"));
         setSeconds(Math.floor((difference % (1000 * 60)) / 1000).toString().padStart(2, "0"));
       } else {
         clearInterval(timer);
@@ -23,9 +23,9 @@ const Timer = ({ expiresAt }) => {
     }, 1000);
 
     return () => clearInterval(timer);
-  }, [expiresAt]);
+  }, [day, hour, minute, expiresAt]);
 
-  return <span>{seconds} : {minutes} : {hours} {days > 0 && `: ${days}`}</span>;
+  return <span>{seconds} {minute && `: ${minutes}`} {hour && `: ${hours}`} {day && days > 0 && `: ${days}`}</span>;
 };
 
 export default Timer;
