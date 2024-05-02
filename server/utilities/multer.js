06a9c2e52c6e67cff6fb7a7e095605d5393fb5e5
MeySaM <imeysamhazrati@gmail.com>
2024-05-02 20:39:29 +0330

    Update multer utility

--- server/utilities/multer.js
+++ server/utilities/multer.js
@@ -1,19 +1,17 @@
 import multer, { diskStorage } from "multer";
+import { mkdirSync } from "fs";
 import { extname } from "path";
 
 const utility = (directory) => multer({
-    storage: diskStorage({
-      destination: (request, file, callback) => callback(null, `public/${directory}/`),
-      filename: (request, file, callback) => callback(null, `${Date.now() + Math.floor(Math.random() * 1000000000)}${extname(file.originalname)}`),
-    }),
-    limits: { fileSize: 20 * 1024 * 1024 },
-    fileFilter: (request, file, callback) => {
-      if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
-        callback(null, true);
-      } else {
-        callback(Object.assign(new Error("The file extension must be png, jpg or jpeg."), { status: 400 }), false);
-      }
+  storage: diskStorage({
+    destination: (request, file, callback) => {
+      mkdirSync(`public/${directory}`, { recursive: true });
+      return callback(null, `public/${directory}`);
     },
-  });
+    filename: (request, file, callback) => callback(null, `${new Date().toISOString().slice(0, 10)}_${Array(7).fill().map(() => Math.floor(Math.random() * 10)).join("")}${extname(file.originalname)}`),
+  }),
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: (request, file, callback) => file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" ? callback(null, true) : callback(Object.assign(new Error("The file extension must be png, jpg or jpeg."), { status: 400 }), false),
+});
 
 export default utility;
