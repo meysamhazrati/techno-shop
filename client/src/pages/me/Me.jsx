0f3da5540f1dc825103e0c5675a85109455ac4f2
MeySaM <imeysamhazrati@gmail.com>
2024-05-03 19:47:53 +0330

    Update Me page

--- client/src/pages/me/Me.jsx
+++ client/src/pages/me/Me.jsx
@@ -1,19 +1,33 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import useMe from "../../hooks/authentication/me";
+import SideBar from "../../components/me/SideBar";
+import Header from "../../components/me/Header";
+import MobileNavigation from "../../components/MobileNavigation";
 
 const Me = () => {
   const navigate = useNavigate();
 
-  const { isFetchingMe, isMeError } = useMe();
+  const { isPendingMe, isMeError } = useMe();
 
   useEffect(() => {
-    if (isMeError) {
-      navigate("/");
-    }
+    isMeError && navigate("/");
   }, [isMeError, navigate]);
 
-  return !isFetchingMe && !isMeError && <></>;
+  return !isPendingMe && !isMeError && (
+    <>
+      <main className="container relative flex min-h-svh flex-col items-start gap-4 pb-[72px] pt-4 xs:gap-8 xs:pb-[104px] xs:pt-8 lg:flex-row lg:pb-8">
+        <SideBar />
+        <section className="flex w-full flex-col gap-4 xs:gap-y-8">
+          <Header />
+          <section className="rounded-3xl bg-white">
+            <Outlet />
+          </section>
+        </section>
+      </main>
+      <MobileNavigation />
+    </>
+  );
 };
 
 export default Me;
