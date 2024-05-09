053ee857e6545492a1c4bf2ffc43035a9934428b
MeySaM <imeysamhazrati@gmail.com>
2024-05-09 20:08:24 +0330

    Create useEditUser hook

---
+++ client/src/hooks/user/edit.js
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { Context as ToastContext } from "../../contexts/Toast";
+import { edit } from "../../axios/controllers/user";
+
+const shouldRetry = ({ response }) => !/400|401/.test(response.status);
+
+export default () => {
+  const { openToast } = useContext(ToastContext);
+
+  const { isPending, mutate } = useMutation({
+    mutationFn: ({ firstName, lastName, currentPassword, newPassword, avatar }) => edit({ firstName, lastName, currentPassword, newPassword, avatar }),
+    retry: (failureCount, error) => shouldRetry(error) && failureCount < 2,
+    onSuccess: () => openToast("success", null, "اطلاعات شما با موفقیت ویرایش شد."),
+    onError: ({ response }) => openToast("error", null, response.status === 400 ? "اطلاعات وارد شده معتبر نمی‌باشد." : response.status === 401 ? "رمز عبور فعلی وارد شده معتبر نمی‌باشد." : null),
+  });
+
+  return { isPendingEditUser: isPending, editUser: mutate };
+};
