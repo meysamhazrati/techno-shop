25bbbd6b3bf7f4239be56b14394f0cf6e2e51757
MeySaM <imeysamhazrati@gmail.com>
2024-05-13 21:52:54 +0330

    Update CartProduct component

--- client/src/components/CartProduct.jsx
+++ client/src/components/CartProduct.jsx
@@ -7,29 +7,29 @@ import VerifiedIcon from "../icons/Verified";
 
 const CartProduct = ({ product, color }) => {
   return (
-    <div className="grid grid-cols-[128px_1fr] items-center gap-x-4 gap-y-2 py-3">
-      <div className="relative size-32">
+    <div className={`grid grid-cols-[150px_1fr] items-center gap-x-4 gap-y-2 overflow-auto py-4 first:pt-0 last:pb-0 ${color.inventory > 0 ? "opacity-100" : "opacity-50"}`}>
+      <div className="relative h-32 w-full">
         <ProductCover id={product._id} covers={product.covers} />
-        {Date.parse(product.offer?.expiresAt) > Date.now() && <div className="absolute right-0 top-4 z-10 flex h-5 w-9 items-center justify-center rounded-full bg-primary-900 font-vazirmatn-medium text-xs text-white">{product.offer.percent}%</div>}
+        {color.inventory > 0 && Date.parse(product.offer?.expiresAt) > Date.now() && <div className="absolute right-0 top-4 z-10 flex h-6 w-11 items-center justify-center rounded-full bg-primary-900 font-vazirmatn-medium text-sm text-white">{product.offer.percent}%</div>}
       </div>
       <div>
         <h3 className="h-14 font-vazirmatn-medium text-lg">
           <Link to={`/products/${product._id}`} className="line-clamp-2 transition-colors hover:text-primary-900">{product.title}</Link>
         </h3>
-        <div className="mt-2 flex flex-col gap-y-1 text-sm text-zinc-500 [&>*]:flex [&>*]:items-center [&>*]:gap-x-2">
+        <div className="mt-2 flex flex-col gap-y-1 text-nowrap text-zinc-500 [&>*]:flex [&>*]:items-center [&>*]:gap-x-2">
           <div>
-            <div className="mr-[1.7px] size-4 rounded-full border border-current" style={{ backgroundColor: color.code }}></div>
+            <div className="mx-[1.7px] size-5 rounded-full border border-zinc-500" style={{ backgroundColor: color.code }}></div>
             <span>{color.name}</span>
           </div>
           <div>
-            <VerifiedIcon className="size-5" />
+            <VerifiedIcon className="size-6 shrink-0" />
             <span>گارانتی {product.warranty} ماهه</span>
           </div>
         </div>
       </div>
-      {Date.parse(product.offer?.expiresAt) > Date.now() && <AmazingOfferTimer width="full" fontSize="xs" expiresAt={product.offer.expiresAt} />}
-      <ProductButton id={product._id} color={color} disabled={!color.inventory} />
-      <ProductPrice price={color.price} offer={product.offer} priceFontSize="base" discountedPriceFontSize="xs" gapX="px" iconSize="4" hasInventory={color.inventory} />
+      {color.inventory > 0 && Date.parse(product.offer?.expiresAt) > Date.now() && <AmazingOfferTimer width="full" fontSize="sm" expiresAt={product.offer.expiresAt} />}
+      {color.inventory > 0 ? <ProductButton id={product._id} color={color} disabled={!color.inventory} /> : <button></button>}
+      <ProductPrice price={color.price} offer={product.offer} priceFontSize="lg" discountedPriceFontSize="sm" gapX="[2px]" iconSize="5" hasInventory={color.inventory} />
     </div>
   );
 };
