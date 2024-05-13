01dfad9b96f3eaefc70a00214c16248ad2cb6b43
MeySaM <imeysamhazrati@gmail.com>
2024-05-13 23:09:55 +0330

    Update CartButton component

--- client/src/components/root/CartButton.jsx
+++ client/src/components/root/CartButton.jsx
@@ -12,27 +12,25 @@ const CartButton = () => {
       <Link to={isMeError ? "/authentication/login" : "/me/cart"} className="flex size-full items-center justify-center rounded-full">
         <CartIcon className="size-7 transition-colors group-hover:stroke-zinc-700" />
       </Link>
-      {me?.cart.length > 0 && (
+      {me?.cart.filter(({ color }) => color.inventory !== 0).reduce((previous, { quantity }) => previous + quantity, 0) > 0 && (
         <>
-          <div className="pointer-events-none absolute right-1 top-1 flex size-5 items-center justify-center rounded-full border-[2px] border-white bg-primary-900 text-xs text-white transition-colors group-hover:border-zinc-200">{me.cart.length}</div>
-          {window.innerWidth > 1024 && (
-            <div className="invisible absolute left-0 top-auto rounded-3xl border-b-4 border-primary-900 bg-white p-3 opacity-0 shadow transition-all group-hover:visible group-hover:opacity-100">
-              <span className="text-zinc-400">{me.cart.length} کالا</span>
-              <div className="max-h-80 w-[350px] divide-y divide-zinc-200 overflow-auto">
-                {me?.cart.map((product) => <CartProduct key={product._id} {...product} />)}
-              </div>
-              <div className="flex w-full items-center justify-between border-t border-zinc-200 pt-3">
-                <div>
-                  <span className="block text-xs text-zinc-500">مبلغ قابل پرداخت</span>
-                  <span className="flex items-center gap-x-[2px] font-vazirmatn-bold text-xl">
-                    {me.cart.reduce((previous, { quantity, product, color }) => previous + quantity * (Date.parse(product.offer?.expiresAt) > Date.now() ? color.price - color.price * (product.offer.percent / 100) : color.price), 0).toLocaleString()}
-                    <TomanIcon className="size-5" />
-                  </span>
-                </div>
-                <Link to="/me/cart" className="rounded-full bg-primary-900 px-5 py-3 font-vazirmatn-medium text-white transition-colors hover:bg-primary-800">ثبت سفارش</Link>
-              </div>
+          <div className="pointer-events-none absolute right-1 top-1 flex size-5 items-center justify-center rounded-full border-[2px] border-white bg-primary-900 text-xs text-white transition-colors group-hover:border-zinc-200">{me.cart.filter(({ color }) => color.inventory !== 0).reduce((previous, { quantity }) => previous + quantity, 0).toLocaleString()}</div>
+          <div className="invisible absolute left-0 top-auto hidden rounded-3xl border-b-4 border-primary-900 bg-white p-4 opacity-0 shadow transition-all group-hover:visible group-hover:opacity-100 lg:block">
+            <span className="text-zinc-400">{me.cart.filter(({ color }) => color.inventory !== 0).reduce((previous, { quantity }) => previous + quantity, 0).toLocaleString()} محصول</span>
+            <div className="mt-4 max-h-80 w-[400px] divide-y divide-zinc-200 overflow-auto pb-4">
+              {me?.cart.map((product) => <CartProduct key={product._id} {...product} />)}
             </div>
-          )}
+            <div className="flex w-full items-center justify-between border-t border-zinc-200 pt-4">
+              <div>
+                <span className="block text-sm text-zinc-500">مبلغ قابل پرداخت</span>
+                <span className="flex items-center gap-x-[2px] font-vazirmatn-bold text-lg">
+                  {me.cart.filter(({ color }) => color.inventory !== 0).reduce((previous, { quantity, product, color }) => previous + quantity * (Date.parse(product.offer?.expiresAt) > Date.now() ? color.price - color.price * (product.offer.percent / 100) : color.price), 0).toLocaleString()}
+                  <TomanIcon className="size-5" />
+                </span>
+              </div>
+              <Link to="/me/cart" className="rounded-full bg-primary-900 px-7 py-3 text-white transition-colors hover:bg-primary-800">ثبت سفارش</Link>
+            </div>
+          </div>
         </>
       )}
     </div>
