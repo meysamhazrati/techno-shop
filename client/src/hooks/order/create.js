0b3e701cf177a3b8bd8ea7812b73a07a700fba7c
MeySaM <imeysamhazrati@gmail.com>
2024-05-13 21:38:43 +0330

    Create useCreateOrder hook

---
+++ client/src/hooks/order/create.js
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { Context as ToastContext } from "../../contexts/Toast";
+import { create } from "../../axios/controllers/order";
+
+const shouldRetry = ({ response }) => response.status !== 400;
+
+export default () => {
+  const client = useQueryClient();
+
+  const { openToast } = useContext(ToastContext);
+
+  const { isPending, mutate } = useMutation({
+    mutationFn: ({ totalPrice, products, destination, discountCode }) => create({ totalPrice, products, destination, discountCode }),
+    retry: (failureCount, error) => shouldRetry(error) && failureCount < 2,
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ["me"] });
+      openToast("success", null, "سفارش شما با موفقیت ثبت شد.");
+    },
+    onError: ({ response }) => openToast("error", null, response.status === 400 ? "اطلاعات وارد شده معتبر نمی‌باشد." : null),
+  });
+
+  return { isPendingCreateOrder: isPending, createOrder: mutate };
+};
