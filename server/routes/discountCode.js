e2ce5cb373dcdb38272fc999546ba5756b383a73
MeySaM <imeysamhazrati@gmail.com>
2024-05-13 14:40:25 +0330

    Update discountCode routes

--- server/routes/discountCode.js
+++ server/routes/discountCode.js
@@ -3,13 +3,12 @@ import { Router } from "express";
 import authentication from "../middlewares/authentication.js";
 import isBanned from "../middlewares/isBanned.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, getAll, check, use, remove } from "../controllers/discountCode.js";
+import { create, getAll, use, remove } from "../controllers/discountCode.js";
 
 const router = Router();
 
 router.route("/").post(authentication, isBanned, isAdmin, create).get(authentication, isBanned, isAdmin, getAll);
-router.post("/check", authentication, isBanned, check);
-router.post("/use/:id", authentication, isBanned, use);
+router.put("/use/:code", authentication, isBanned, use);
 router.delete("/:id", authentication, isBanned, isAdmin, remove);
 
 export default router;
