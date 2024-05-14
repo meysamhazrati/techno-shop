f410139d8e29d4b5d3c27a8a440e3fed17b3baf4
MeySaM <imeysamhazrati@gmail.com>
2024-05-14 17:05:33 +0330

    Create useRemoveAddress hook

---
+++ client/src/hooks/address/remove.js
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { Context as ToastContext } from "../../contexts/Toast";
+import { remove } from "../../axios/controllers/address";
+
+const shouldRetry = ({ response }) => !/403|404/.test(response.status);
+
+export default (id) => {
+  const { openToast } = useContext(ToastContext);
+
+  const { isPending, mutate } = useMutation({
+    mutationFn: () => remove(id),
+    retry: (failureCount, error) => shouldRetry(error) && failureCount < 2,
+    onSuccess: () => openToast("success", null, "آدرس مورد نظر با موفقیت حذف شد."),
+    onError: ({ response }) => openToast("error", null, response.status === 403 ? "شما دسترسی لازم برای حذف این آدرس را ندارید." : response.status === 404 ? "آدرس مورد نظر پیدا نشد." : null),
+  });
+
+  return { isPendingRemoveAddress: isPending, removeAddress: mutate };
+};
