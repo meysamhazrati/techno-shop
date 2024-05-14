aa58f901a79027309e503dcb7f9ecae08996e6d4
MeySaM <imeysamhazrati@gmail.com>
2024-05-14 16:22:53 +0330

    Update address routes

--- server/routes/address.js
+++ server/routes/address.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import authentication from "../middlewares/authentication.js";
 import isBanned from "../middlewares/isBanned.js";
-import { create, update, remove } from "../controllers/address.js";
+import isAdmin from "../middlewares/isAdmin.js";
+import { create, getAll, update, remove } from "../controllers/address.js";
 
 const router = Router();
 
-router.route("/").post(authentication, isBanned, create);
+router.route("/").post(authentication, isBanned, create).get(authentication, isBanned, isAdmin, getAll);
 router.route("/:id").put(authentication, isBanned, update).delete(authentication, isBanned, remove);
 
 export default router;
