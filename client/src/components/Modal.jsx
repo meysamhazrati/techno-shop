b2632df700c2df41f8a8aa72fab971dfddb50281
MeySaM <imeysamhazrati@gmail.com>
2024-05-22 22:00:37 +0330

    Update Modal component

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,33 +1,52 @@
-import { forwardRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = forwardRef(({ onClose, children }, ref) => {
+const Modal = ({ isOpen, onClose, children }) => {
+  const [shouldRender, setShouldRender] = useState(false);
+
+  const modal = useRef();
+
   useEffect(() => {
-    const animationFrame = requestAnimationFrame(() => {
-      ref.current.style.visibility = "visible";
-      ref.current.style.opacity = "100%";
-    });
+    let animationFrame = null;
+    let removeTimeout = null;
 
     const onKeyUp = ({ key }) => key === "Escape" && onClose();
 
-    document.body.classList.add("overflow-hidden");
-
     window.addEventListener("keyup", onKeyUp);
 
-    return () => {
-      cancelAnimationFrame(animationFrame);
-      window.removeEventListener("keyup", onKeyUp);
-    };
-  }, [ref, onClose]);
+    if (isOpen) {
+      setShouldRender(true);
 
-  return createPortal(
-    <div ref={ref} className="invisible fixed inset-0 z-50 flex items-center justify-center overflow-hidden bg-black/50 px-4 opacity-0 transition-all duration-300 lg:px-8" onClick={({ target }) => target === ref.current && onClose()}>
+      if (modal.current) {
+        animationFrame = requestAnimationFrame(() => {
+          modal.current.style.visibility = "visible";
+          modal.current.style.opacity = "100%";
+        });
+      }
+
+      document.body.classList.add("overflow-hidden");
+    } else if (!isOpen && shouldRender) {
+      modal.current.style.visibility = "hidden";
+      modal.current.style.opacity = "0%";
+
+      document.body.classList.remove("overflow-hidden");
+
+      removeTimeout = setTimeout(() => setShouldRender(false), getComputedStyle(modal.current).getPropertyValue("transition-duration").slice(0, -1) * 1000);
+    }
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      cancelAnimationFrame(animationFrame);
+      clearTimeout(removeTimeout);
+    };
+  }, [isOpen, shouldRender, onClose]);
+
+  return shouldRender && createPortal(
+    <div ref={modal} className="invisible fixed inset-0 z-50 flex items-center justify-center overflow-hidden bg-black/50 px-4 opacity-0 transition-all duration-300 lg:px-8" onClick={({ target }) => target === modal.current && onClose()}>
       <div className="rounded-3xl bg-white p-6">{children}</div>
     </div>,
     document.getElementById("modal")
   );
-});
-
-Modal.displayName = "Modal";
+};
 
 export default Modal;
