b9fd968477b3bdd569f1b812523b37e1fd229638
MeySaM <imeysamhazrati@gmail.com>
2024-05-24 15:32:05 +0330

    Update article routes

--- server/routes/article.js
+++ server/routes/article.js
@@ -4,16 +4,14 @@ import multer from "../utilities/multer.js";
 import authentication from "../middlewares/authentication.js";
 import isBanned from "../middlewares/isBanned.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, getAll, get, update, publish, draft, confirm, reject, remove } from "../controllers/article.js";
+import { create, getAll, get, update, publish, draft, remove } from "../controllers/article.js";
 
 const router = Router();
 const uploader = multer("articles");
 
-router.route("/").post(authentication, isBanned, uploader.single("cover"), create).get(getAll);
-router.route("/:id").get(get).put(authentication, isBanned, uploader.single("cover"), update).delete(authentication, isBanned, remove);
-router.put("/publish/:id", authentication, isBanned, publish);
-router.put("/draft/:id", authentication, isBanned, draft);
-router.put("/confirm/:id", authentication, isBanned, isAdmin, confirm);
-router.put("/reject/:id", authentication, isBanned, isAdmin, reject);
+router.route("/").post(authentication, isBanned, isAdmin, uploader.single("cover"), create).get(getAll);
+router.route("/:id").get(get).put(authentication, isBanned, isAdmin, uploader.single("cover"), update).delete(authentication, isBanned, isAdmin, remove);
+router.put("/publish/:id", authentication, isBanned, isAdmin, publish);
+router.put("/draft/:id", authentication, isBanned, isAdmin, draft);
 
 export default router;
