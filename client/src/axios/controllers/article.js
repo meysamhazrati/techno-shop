228702f1955b6742567e4dae9ae6e7f668af3f10
MeySaM <imeysamhazrati@gmail.com>
2024-05-24 15:42:24 +0330

    Update axios article controller

--- client/src/axios/controllers/article.js
+++ client/src/axios/controllers/article.js
@@ -2,20 +2,16 @@ import axios from "../config";
 
 const create = async ({ title, cover, body, isPublished, category }) => await axios.postForm("/articles", { title, cover, body, isPublished, category });
 
-const getAll = async (categories, onlyPublished, onlyConfirmed, sort, page, length) => await axios.get("/articles", { params: { categories, "only-published": onlyPublished, "only-confirmed": onlyConfirmed, sort, page, length } });
+const getAll = async (categories, onlyPublished, sort, page, length) => await axios.get("/articles", { params: { categories, "only-published": onlyPublished, sort, page, length } });
 
 const get = async (id, commentsPage, commentsLength) => await axios.get(`/articles/${id}`, { params: { "comments-page": commentsPage, "comments-length": commentsLength } });
 
-const update = async (id, { title, cover, body, isPublished }) => await axios.putForm(`/articles/${id}`, { title, cover, body, isPublished });
+const update = async (id, { title, cover, body }) => await axios.putForm(`/articles/${id}`, { title, cover, body });
 
 const publish = async (id) => await axios.put(`/articles/publish/${id}`);
 
 const draft = async (id) => await axios.put(`/articles/draft/${id}`);
 
-const confirm = async (id) => await axios.put(`/articles/confirm/${id}`);
-
-const reject = async (id) => await axios.put(`/articles/reject/${id}`);
-
 const remove = async (id) => await axios.delete(`/articles/${id}`);
 
-export { create, getAll, get, update, publish, draft, confirm, reject, remove };
+export { create, getAll, get, update, publish, draft, remove };
