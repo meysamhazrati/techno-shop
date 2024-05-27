72f8f4d8229481c3cfaeaed753fd351024b3edd8
MeySaM <imeysamhazrati@gmail.com>
2024-05-27 17:14:15 +0330

    Update Favorites page

--- client/src/pages/me/Favorites.jsx
+++ client/src/pages/me/Favorites.jsx
@@ -1,36 +1,29 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import useMe from "../../hooks/authentication/me";
-import useEmptyFavorites from "../../hooks/user/emptyFavorites";
 import Favorite from "../../components/me/Favorite";
 import NoResultFound from "../../components/NoResultFound";
-import Modal from "../../components/Modal";
-import Confirm from "../../components/Confirm";
 
 const Favorites = () => {
-  const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
-
   const { me } = useMe();
-  const { isPendingEmptyFavorites, emptyFavorites } = useEmptyFavorites();
 
   useEffect(() => {
     document.title = "تکنوشاپ - من - علاقه‌مندی ها";
   }, []);
 
-  return me.favorites.length !== 0 ? (
+  return (
     <>
-      <div className="flex items-center justify-between gap-x-2">
-        <span className="text-lg text-zinc-400">{me.favorites.length.toLocaleString()} علاقه‌مندی</span>
-        <button disabled={isPendingEmptyFavorites} className="flex items-center gap-x-2 text-red-500" onClick={() => setIsRemoveModalOpen(true)}>حذف همه</button>
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="font-vazirmatn-bold text-xl">علاقه‌مندی ها</h2>
+        <span className="mr-auto text-zinc-500">{me.favorites.length.toLocaleString()} علاقه‌مندی</span>
       </div>
-      <div className="mt-4 divide-y divide-zinc-200 overflow-hidden">
-        {me.favorites.map((favorite) => <Favorite key={favorite._id} {...favorite} colors={favorite.colors.toSorted((firstColor, secondColor) => firstColor.price - secondColor.price)} />)}
-      </div>
-      <Modal isOpen={isRemoveModalOpen} onClose={() => setIsRemoveModalOpen(false)}>
-        <Confirm title="علاقه‌مندی ها را خالی می‌کنید؟" isPending={isPendingEmptyFavorites} onCancel={() => setIsRemoveModalOpen(false)} onConfirm={() => emptyFavorites(null, { onSuccess: () => setIsRemoveModalOpen(false) })} />
-      </Modal>
+      {me.favorites.length !== 0 ? (
+        <div className="mt-4 divide-y divide-zinc-200 overflow-hidden">
+          {me.favorites.map((favorite) => <Favorite key={favorite._id} {...favorite} />)}
+        </div>
+      ) : (
+        <NoResultFound title="علاقه‌مندی‌ای پیدا نشد!" className="mt-4" />
+      )}
     </>
-  ) : (
-    <NoResultFound title="علاقه‌مندی‌ای پیدا نشد!" />
   );
 };
 
