cf49846bb6e84da7516fcb03087c9d8cd13d2488
MeySaM <imeysamhazrati@gmail.com>
2024-05-27 15:42:04 +0330

    Create favorite model

---
+++ server/models/favorite.js
@@ -0,0 +1,17 @@
+import { Schema, Types, model } from "mongoose";
+
+const schema = new Schema(
+  {
+    user: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+    product: {
+      type: Types.ObjectId,
+      ref: "Product",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Favorite", schema);
