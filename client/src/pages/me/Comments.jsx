1a313020daf147cb7ee763bfe3f52ab15f4c942d
MeySaM <imeysamhazrati@gmail.com>
2024-05-27 17:20:30 +0330

    Update Comments page

--- client/src/pages/me/Comments.jsx
+++ client/src/pages/me/Comments.jsx
@@ -10,15 +10,20 @@ const Comments = () => {
     document.title = "تکنوشاپ - من - دیدگاه ها";
   }, []);
 
-  return me.comments.length !== 0 ? (
+  return (
     <>
-      <span className="text-lg text-zinc-400">{me.comments.length.toLocaleString()} دیدگاه</span>
-      <div className="mt-4 divide-y divide-zinc-200 overflow-hidden">
-        {me.comments.map((comment) => <Comment key={comment._id} {...comment} />)}
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="font-vazirmatn-bold text-xl">دیدگاه ها</h2>
+        <span className="mr-auto text-zinc-500">{me.comments.length.toLocaleString()} دیدگاه</span>
       </div>
+      {me.comments.length !== 0 ? (
+        <div className="mt-4 divide-y divide-zinc-200 overflow-hidden">
+          {me.comments.map((comment) => <Comment key={comment._id} {...comment} />)}
+        </div>
+      ) : (
+        <NoResultFound title="دیدگاهی پیدا نشد!" className="mt-4" />
+      )}
     </>
-  ) : (
-    <NoResultFound title="دیدگاهی پیدا نشد!" />
   );
 };
 
