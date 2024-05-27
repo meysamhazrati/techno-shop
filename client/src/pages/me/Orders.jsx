a84c574aba8f25ae2818c05c731766b17f0457a7
MeySaM <imeysamhazrati@gmail.com>
2024-05-27 11:50:15 +0330

    Update Orders page

--- client/src/pages/me/Orders.jsx
+++ client/src/pages/me/Orders.jsx
@@ -10,12 +10,20 @@ const Orders = () => {
     document.title = "تکنوشاپ - من - سفارش ها";
   }, []);
 
-  return me.orders.length !== 0 ? (
-    <div className="divide-y divide-zinc-200 overflow-hidden">
-      {me.orders.map((order) => <Order key={order._id} {...order} />)}
-    </div>
-  ) : (
-    <NoResultFound title="سفارشی پیدا نشد!" />
+  return (
+    <>
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="font-vazirmatn-bold text-xl">سفارش ها</h2>
+        <span className="mr-auto text-zinc-500">{me.orders.length.toLocaleString()} سفارش</span>
+      </div>
+      {me.orders.length !== 0 ? (
+        <div className="mt-4 divide-y divide-zinc-200 overflow-hidden">
+          {me.orders.map((order) => <Order key={order._id} {...order} />)}
+        </div>
+      ) :(
+        <NoResultFound title="سفارشی پیدا نشد!" className="mt-4" />
+      )}
+    </>
   );
 };
 
