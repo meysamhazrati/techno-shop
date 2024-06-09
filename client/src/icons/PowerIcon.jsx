731326a9805f4d7802abf25db33e63d6400fd9b8
MeySaM <imeysamhazrati@gmail.com>
2024-06-09 20:22:29 +0330

    Add Power icon

---
+++ client/src/icons/PowerIcon.jsx
@@ -0,0 +1,9 @@
+const PowerIcon = ({ className }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={className}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
+    </svg>
+  );
+};
+
+export default PowerIcon;
