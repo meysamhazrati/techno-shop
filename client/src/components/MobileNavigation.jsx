dd80a615da8ed8fa7548552b3569df59665f8009
MeySaM <imeysamhazrati@gmail.com>
2024-06-09 20:38:58 +0330

    Update MobileNavigation component

--- client/src/components/MobileNavigation.jsx
+++ client/src/components/MobileNavigation.jsx
@@ -1,30 +1,30 @@
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, NavLink } from "react-router-dom";
 import HomeIcon from "../icons/HomeIcon";
 import SquaresIcon from "../icons/SquaresIcon";
 import CartIcon from "../icons/CartIcon";
 import UserIcon from "../icons/UserIcon";
 
 const MobileNavigation = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
   return (
     <nav className="fixed bottom-0 left-0 right-0 z-50 grid h-14 grid-cols-4 items-center gap-x-3 bg-white px-3 text-center text-sm shadow xs:h-[72px] lg:hidden">
-      <Link to="/">
-        <HomeIcon solid={location.pathname === "/"} className="mx-auto size-7" />
+      <NavLink to="/" className={({ isActive }) => isActive ? "font-vazirmatn-medium" : "font-vazirmatn-regular"}>
+        <HomeIcon solid={pathname === "/"} className="mx-auto size-7" />
         <span className="mt-1 hidden xs:block">خانه</span>
-      </Link>
-      <Link to="/categories">
-        <SquaresIcon solid={location.pathname.startsWith("/categories")} className="mx-auto size-7" />
+      </NavLink>
+      <NavLink to="/categories" className={({ isActive }) => isActive ? "font-vazirmatn-medium" : "font-vazirmatn-regular"}>
+        <SquaresIcon solid={pathname.startsWith("/categories")} className="mx-auto size-7" />
         <span className="mt-1 hidden xs:block">دسته‌بندی ها</span>
-      </Link>
-      <Link to="/cart">
-        <CartIcon solid={location.pathname === "/cart"} className="mx-auto size-7" />
+      </NavLink>
+      <NavLink to="/cart" className={({ isActive }) => isActive ? "font-vazirmatn-medium" : "font-vazirmatn-regular"}>
+        <CartIcon solid={pathname === "/cart"} className="mx-auto size-7" />
         <span className="mt-1 hidden xs:block">سبد خرید</span>
-      </Link>
-      <Link to="/me">
-        <UserIcon solid={location.pathname.startsWith("/me")} className="mx-auto size-7" />
+      </NavLink>
+      <NavLink to="/me" className={({ isActive }) => isActive ? "font-vazirmatn-medium" : "font-vazirmatn-regular"}>
+        <UserIcon solid={pathname.startsWith("/me")} className="mx-auto size-7" />
         <span className="mt-1 hidden xs:block">حساب کاربری</span>
-      </Link>
+      </NavLink>
     </nav>
   );
 };
