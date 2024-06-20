685940f2fd8dfe7ff17fff98780e5e39a48e91c2
MeySaM <imeysamhazrati@gmail.com>
2024-06-20 19:52:49 +0330

    Create AddressSkeleton component

---
+++ client/src/components/admin/AddressSkeleton.jsx
@@ -0,0 +1,29 @@
+const AddressSkeleton = ({ recipientField }) => {
+  return (
+    <tr className="border-t border-zinc-200 [&>*]:h-[72px] [&>*]:px-5">
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      {recipientField && (
+        <td>
+          <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+        </td>
+      )}
+      <td>
+        <div className="flex items-center justify-center gap-x-2">
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default AddressSkeleton;
