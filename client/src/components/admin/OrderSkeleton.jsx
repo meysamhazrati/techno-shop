444c9071c64222073ae3ba03622de52767d9f305
MeySaM <imeysamhazrati@gmail.com>
2024-06-20 20:37:09 +0330

    Create OrderSkeleton component

---
+++ client/src/components/admin/OrderSkeleton.jsx
@@ -0,0 +1,32 @@
+const OrderSkeleton = ({ buyerField }) => {
+  return (
+    <tr className="border-t border-zinc-200 [&>*]:h-[72px] [&>*]:px-5">
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      {buyerField && (
+        <td>
+          <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+        </td>
+      )}
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="flex items-center justify-center gap-x-2">
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default OrderSkeleton;
