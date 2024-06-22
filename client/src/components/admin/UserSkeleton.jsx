d7ed5676d8aa838bdf96288671ac190175c7115e
MeySaM <imeysamhazrati@gmail.com>
2024-06-22 22:14:55 +0330

    Update UserSkeleton component

--- client/src/components/admin/UserSkeleton.jsx
+++ client/src/components/admin/UserSkeleton.jsx
@@ -20,7 +20,7 @@ const UserSkeleton = () => {
         <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
       </td>
       <td>
-        <div className="flex items-center justify-between gap-x-2">
+        <div className="flex items-center justify-center gap-x-2">
           <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
           <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
           <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
