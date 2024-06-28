e7351ca73fd20ca46c054a7995a5849dfb192dea
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:32:08 +0330

    Update article model

--- server/models/article.js
+++ server/models/article.js
@@ -1,48 +1,40 @@
 import { Schema, Types, model } from "mongoose";
 
-import { create, update } from "../validators/article.js";
-
-const schema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 100,
-    },
-    cover: {
-      type: String,
-      required: true,
-    },
-    body: {
-      type: String,
-      required: true,
-      minLength: 100,
-      maxLength: 10000,
-    },
-    isPublished: {
-      type: Boolean,
-      required: true,
-    },
-    author: {
-      type: Types.ObjectId,
-      ref: "User",
-    },
-    category: {
-      type: Types.ObjectId,
-      ref: "Category",
-    },
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 100,
   },
-  { timestamps: true }
-);
+  cover: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+    minLength: 100,
+    maxLength: 10000,
+  },
+  isPublished: {
+    type: Boolean,
+    required: true,
+  },
+  author: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+  category: {
+    type: Types.ObjectId,
+    ref: "Category",
+  },
+}, { timestamps: true });
 
 schema.virtual("comments", {
   ref: "Comment",
   localField: "_id",
   foreignField: "article",
 });
-
-schema.statics.createValidation = (body) => create.validate(body);
-schema.statics.updateValidation = (body) => update.validate(body);
 
 export default model("Article", schema);
