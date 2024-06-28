8f5219ae8c488105e4752db6c9d42e4b816a14f6
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 22:57:04 +0330

    Update product model

--- server/models/product.js
+++ server/models/product.js
@@ -1,41 +1,36 @@
 import { Schema, Types, model } from "mongoose";
 
-import { create, update } from "../validators/product.js";
-
-const schema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 100,
-    },
-    warranty: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 100,
-    },
-    covers: [
-      {
-        type: String,
-      },
-    ],
-    brand: {
-      type: Types.ObjectId,
-      ref: "Brand",
-    },
-    category: {
-      type: Types.ObjectId,
-      ref: "Category",
-    },
-    offer: {
-      type: Types.ObjectId,
-      ref: "Offer",
-    },
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 100,
   },
-  { timestamps: true }
-);
+  warranty: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100,
+  },
+  covers: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: Types.ObjectId,
+    ref: "Brand",
+  },
+  category: {
+    type: Types.ObjectId,
+    ref: "Category",
+  },
+  offer: {
+    type: Types.ObjectId,
+    ref: "Offer",
+  },
+}, { timestamps: true });
 
 schema.virtual("colors", {
   ref: "Color",
@@ -48,9 +43,6 @@ schema.virtual("comments", {
   localField: "_id",
   foreignField: "product",
 });
-
-schema.statics.createValidation = (body) => create.validate(body);
-schema.statics.updateValidation = (body) => update.validate(body);
 
 export { schema };
 export default model("Product", schema);
