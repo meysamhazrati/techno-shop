b0807a149a457dc66582896f9620224acbde2d17
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:30:44 +0330

    Update favorite validations

--- server/validators/favorite.js
+++ server/validators/favorite.js
@@ -1,5 +1,5 @@
 import { object, string } from "yup";
 
-const validator = object({ product: string().required() });
+const validator = object({ product: string().required("محصول الزامی است.").matches(/^[a-fA-F\d]{24}$/, "محصول نامعتبر است.") });
 
 export default validator;
