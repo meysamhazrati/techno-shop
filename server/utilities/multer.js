a47277258dfdd5e3d00c2a1c5c6a80aa59838411
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 21:07:58 +0330

    Update multer utility

--- server/utilities/multer.js
+++ server/utilities/multer.js
@@ -12,7 +12,7 @@ const utility = (directory) => multer({
     filename: (request, file, callback) => callback(null, `${new Date().toISOString().slice(0, 10)}_${Array(7).fill().map(() => Math.floor(Math.random() * 10)).join("")}${extname(file.originalname)}`),
   }),
   limits: { fileSize: 2 * 1024 * 1024 },
-  fileFilter: (request, file, callback) => file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" ? callback(null, true) : callback(Object.assign(new Error("The file extension must be png, jpg or jpeg."), { status: 400 }), false),
+  fileFilter: (request, file, callback) => file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" ? callback(null, true) : callback(Object.assign(new Error("فرمت عکس باید PNG، JPG یا JPEG باشد."), { status: 400 }), false),
 });
 
 export default utility;
