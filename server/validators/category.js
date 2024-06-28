5e4bb6114b9c3988e5fa59aa0b6ae29cc62da284
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 22:45:14 +0330

    Update category validations

--- server/validators/category.js
+++ server/validators/category.js
@@ -1,14 +1,14 @@
 import { object, string } from "yup";
 
 const create = object({
-  title: string().required().min(3).max(50),
-  englishTitle: string().required().min(3).max(50),
-  logo: object().required(),
+  title: string().required("عنوان الزامی است.").min(3, "عنوان باید حداقل 3 کاراکتر باشد.").max(50, "عنوان باید حداکثر 50 کاراکتر باشد."),
+  englishTitle: string().required("عنوان انگلیسی الزامی است.").min(3, "عنوان انگلیسی باید حداقل 3 کاراکتر باشد.").max(50, "عنوان انگلیسی باید حداکثر 50 کاراکتر باشد."),
+  logo: object().required("لوگو الزامی است."),
 });
 
 const update = object({
-  title: string().required().min(3).max(50),
-  englishTitle: string().required().min(3).max(50),
+  title: string().required("عنوان الزامی است.").min(3, "عنوان باید حداقل 3 کاراکتر باشد.").max(50, "عنوان باید حداکثر 50 کاراکتر باشد."),
+  englishTitle: string().required("عنوان انگلیسی الزامی است.").min(3, "عنوان انگلیسی باید حداقل 3 کاراکتر باشد.").max(50, "عنوان انگلیسی باید حداکثر 50 کاراکتر باشد."),
 });
 
-export { create, update };
+export default { create, update };
