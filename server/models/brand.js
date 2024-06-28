6f359969aa9203b8243c6dc4cd906f51fc1ac7bb
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 22:36:32 +0330

    Update brand model

--- server/models/brand.js
+++ server/models/brand.js
@@ -1,36 +1,28 @@
 import { Schema, model } from "mongoose";
 
-import { create, update } from "../validators/brand.js";
-
-const schema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    englishName: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    logo: {
-      type: String,
-      required: true,
-    },
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
   },
-  { timestamps: true }
-);
+  englishName: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  logo: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
 schema.virtual("products", {
   ref: "Product",
   localField: "_id",
   foreignField: "brand",
 });
-
-schema.statics.createValidation = (body) => create.validate(body);
-schema.statics.updateValidation = (body) => update.validate(body);
 
 export default model("Brand", schema);
