9ce7459ecab9098c2f5b5cdafbef1820a6fd580b
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:40:00 +0330

    Update offer model

--- server/models/offer.js
+++ server/models/offer.js
@@ -1,52 +1,44 @@
 import { Schema, Types, model } from "mongoose";
 
-import { create, update } from "../validators/offer.js";
-
-const schema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 100,
-    },
-    englishTitle: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 100,
-    },
-    description: {
-      type: String,
-      required: true,
-      minLength: 10,
-      maxLength: 200,
-    },
-    percent: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 100,
-    },
-    expiresAt: {
-      type: Date,
-      required: true,
-    },
-    organizer: {
-      type: Types.ObjectId,
-      ref: "User",
-    },
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 100,
   },
-  { timestamps: true }
-);
+  englishTitle: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 100,
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 200,
+  },
+  percent: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 100,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+  organizer: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+}, { timestamps: true });
 
 schema.virtual("products", {
   ref: "Product",
   localField: "_id",
   foreignField: "offer",
 });
-
-schema.statics.createValidation = (body) => create.validate(body);
-schema.statics.updateValidation = (body) => update.validate(body);
 
 export default model("Offer", schema);
