cdb1a47d7daf135b3269f5c008c1905a09fd1df4
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:40:47 +0330

    Update offer validations

--- server/validators/offer.js
+++ server/validators/offer.js
@@ -1,20 +1,20 @@
 import { object, string, number, array } from "yup";
 
 const create = object({
-  title: string().required().min(5).max(100),
-  englishTitle: string().required().min(5).max(100),
-  description: string().required().min(10).max(200),
-  percent: number().required().min(1).max(100),
-  expiresAt: number().required().min(1).max(10000),
-  categories: array().of(string()).required().min(1).max(7),
+  title: string().required("عنوان الزامی است.").min(5, "عنوان باید حداقل 5 کاراکتر باشد.").max(100, "عنوان باید حداکثر 100 کاراکتر باشد."),
+  englishTitle: string().required("عنوان انگلیسی الزامی است.").min(5, "عنوان انگلیسی باید حداقل 5 کاراکتر باشد.").max(100, "عنوان انگلیسی باید حداکثر 100 کاراکتر باشد."),
+  description: string().required("توضیحات الزامی است.").min(10, "توضیحات باید حداقل 10 کاراکتر باشد.").max(200, "توضیحات باید حداکثر 200 کاراکتر باشد."),
+  percent: number().required("درصد الزامی است.").min(1, "درصد باید حداقل 1 باشد.").max(100, "درصد باید حداکثر 100 باشد."),
+  expiresAt: number().required("انقضا الزامی است.").min(1, "انقضا باید حداقل 1 ساعت باشد.").max(10000, "انقضا باید حداکثر 10000 ساعت باشد."),
+  categories: array().required("دسته‌بندی‌ ها الزامی هستند.").min(1, "دسته‌بندی‌ ها باید حداقل 1 عدد باشند.").max(7, "دسته‌بندی‌ ها باید حداکثر 7 عدد باشند.").of(string().matches(/^[a-fA-F\d]{24}$/, "دسته‌بندی‌ نامعتبر است.")),
 });
 
 const update = object({
-  title: string().required().min(5).max(100),
-  englishTitle: string().required().min(5).max(100),
-  description: string().required().min(10).max(200),
-  percent: number().required().min(1).max(100),
-  expiresAt: number().required().min(1).max(10000),
+  title: string().required("عنوان الزامی است.").min(5, "عنوان باید حداقل 5 کاراکتر باشد.").max(100, "عنوان باید حداکثر 100 کاراکتر باشد."),
+  englishTitle: string().required("عنوان انگلیسی الزامی است.").min(5, "عنوان انگلیسی باید حداقل 5 کاراکتر باشد.").max(100, "عنوان انگلیسی باید حداکثر 100 کاراکتر باشد."),
+  description: string().required("توضیحات الزامی است.").min(10, "توضیحات باید حداقل 10 کاراکتر باشد.").max(200, "توضیحات باید حداکثر 200 کاراکتر باشد."),
+  percent: number().required("درصد الزامی است.").min(1, "درصد باید حداقل 1 باشد.").max(100, "درصد باید حداکثر 100 باشد."),
+  expiresAt: number().required("انقضا الزامی است.").min(1, "انقضا باید حداقل 1 ساعت باشد.").max(10000, "انقضا باید حداکثر 10000 ساعت باشد."),
 });
 
-export { create, update };
+export default { create, update };
