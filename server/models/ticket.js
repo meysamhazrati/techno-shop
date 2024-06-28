fc809701fe83c05f51894e0dbf0c403726309523
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:37:10 +0330

    Update ticket model

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -1,49 +1,41 @@
 import { Schema, Types, model } from "mongoose";
 
-import { create, reply } from "../validators/ticket.js";
-
-const schema = new Schema(
-  {
-    department: {
-      type: String,
-      required: false,
-      enum: ["Management", "Finance", "Order Tracking", "Support", "Feedback", "Other"],
-    },
-    title: {
-      type: String,
-      required: false,
-      minLength: 2,
-      maxLength: 20,
-    },
-    body: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 300,
-    },
-    isOpen: {
-      type: Boolean,
-      required: false,
-    },
-    sender: {
-      type: Types.ObjectId,
-      ref: "User",
-    },
-    ticket: {
-      type: Types.ObjectId,
-      ref: "Ticket",
-    },
+const schema = new Schema({
+  department: {
+    type: String,
+    required: false,
+    enum: ["مدیریت", "مالی", "پیگیری سفارش", "پشتیبانی", "بازخورد", "سایر"],
   },
-  { timestamps: true }
-);
+  title: {
+    type: String,
+    required: false,
+    minLength: 2,
+    maxLength: 20,
+  },
+  body: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 300,
+  },
+  isOpen: {
+    type: Boolean,
+    required: false,
+  },
+  sender: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+  ticket: {
+    type: Types.ObjectId,
+    ref: "Ticket",
+  },
+}, { timestamps: true });
 
 schema.virtual("replies", {
   ref: "Ticket",
   localField: "_id",
   foreignField: "ticket",
 });
-
-schema.statics.createValidation = (body) => create.validate(body);
-schema.statics.replyValidation = (body) => reply.validate(body);
 
 export default model("Ticket", schema);
