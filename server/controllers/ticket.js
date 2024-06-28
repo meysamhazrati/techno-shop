7de49cafe106e732b0dce016267e42c20dfd444f
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:39:22 +0330

    Update ticket controller

--- server/controllers/ticket.js
+++ server/controllers/ticket.js
@@ -1,20 +1,15 @@
 import model from "../models/ticket.js";
+import validator from "../validators/ticket.js";
 
 const create = async (request, response, next) => {
   try {
-    await model.createValidation(request.body);
+    const body = request.body;
+    
+    await validator.create.validate(body);
 
-    const { department, title, body } = request.body;
+    await model.create({ ...body, isOpen: true, sender: request.user._id });
 
-    await model.create({
-      department,
-      title,
-      body,
-      isOpen: true,
-      sender: request.user._id,
-    });
-
-    response.status(201).json({ message: "The ticket has been successfully added." });
+    response.status(201).json({ message: "تیکت شما با موفقیت ثبت شد." });
   } catch (error) {
     next(error);
   }
@@ -26,22 +21,22 @@ const reply = async (request, response, next) => {
 
     const { _id, role } = request.user;
 
-    const ticket = await model.findOne({ _id: id, ticket: { $exists: false } });
+    const ticket = await model.findById(id);
 
     if (ticket) {
       if (role === "ADMIN" || _id.equals(ticket.sender)) {
-        await model.replyValidation(request.body);
+        const body = request.body;
+        
+        await validator.reply.validate(body);
 
-        const { body } = request.body;
+        await model.create({ ...body, sender: _id, ticket: id });
 
-        await model.create({ body, sender: _id, ticket: id });
-
-        response.status(201).json({ message: "Your reply has been successfully sent." });
+        response.status(201).json({ message: "پاسخ شما با موفقیت ثبت شد." });
       } else {
-        throw Object.assign(new Error("You don't have access to reply to this ticket."), { status: 403 });
+        throw Object.assign(new Error("شما دسترسی لازم برای پاسخ به تیکت مورد نظر را ندارید."), { status: 403 });
       }
     } else {
-      throw Object.assign(new Error("The ticket was not found."), { status: 404 });
+      throw Object.assign(new Error("تیکت مورد نظر پیدا نشد."), { status: 404 });
     }
   } catch (error) {
     next(error);
@@ -68,7 +63,7 @@ const getAll = async (request, response, next) => {
       }
     }
 
-    throw Object.assign(new Error("No ticket found."), { status: 404 });
+    throw Object.assign(new Error("تیکتی پیدا نشد."), { status: 404 });
   } catch (error) {
     next(error);
   }
@@ -89,10 +84,10 @@ const get = async (request, response, next) => {
       if (role === "ADMIN" || _id.equals(ticket.sender)) {
         response.json(ticket);
       } else {
-        throw Object.assign(new Error("You don't have access to this ticket."), { status: 403 });
+        throw Object.assign(new Error("شما دسترسی لازم به تیکت مورد نظر را ندارید."), { status: 403 });
       }
     } else {
-      throw Object.assign(new Error("The ticket was not found."), { status: 404 });
+      throw Object.assign(new Error("تیکت مورد نظر پیدا نشد."), { status: 404 });
     }
   } catch (error) {
     next(error);
@@ -107,14 +102,14 @@ const open = async (request, response, next) => {
 
     if (ticket) {
       if (ticket.isOpen) {
-        throw Object.assign(new Error("This ticket is already open."), { status: 409 });
+        throw Object.assign(new Error("تیکت مورد نظر از قبل باز شده است."), { status: 409 });
       } else {
         await model.findByIdAndUpdate(id, { isOpen: true });
 
-        response.json({ message: "The ticket has been successfully opened." });
+        response.json({ message: "تیکت مورد نظر با موفقیت باز شد." });
       }
     } else {
-      throw Object.assign(new Error("The ticket was not found."), { status: 404 });
+      throw Object.assign(new Error("تیکت مورد نظر پیدا نشد."), { status: 404 });
     }
   } catch (error) {
     next(error);
@@ -131,12 +126,12 @@ const close = async (request, response, next) => {
       if (ticket.isOpen) {
         await model.findByIdAndUpdate(id, { isOpen: false });
 
-        response.json({ message: "The ticket has been successfully closed." });
+        response.json({ message: "تیکت مورد نظر با موفقیت بسته شد." });
       } else {
-        throw Object.assign(new Error("This ticket has already been closed."), { status: 409 });
+        throw Object.assign(new Error("تیکت مورد نظر از قبل بسته شده است."), { status: 409 });
       }
     } else {
-      throw Object.assign(new Error("The ticket was not found."), { status: 404 });
+      throw Object.assign(new Error("تیکت مورد نظر پیدا نشد."), { status: 404 });
     }
   } catch (error) {
     next(error);
@@ -147,14 +142,14 @@ const remove = async (request, response, next) => {
   try {
     const { id } = request.params;
 
-    const result = await model.findOneAndDelete({ _id: id, ticket: { $exists: false } });
+    const result = await model.findByIdAndDelete(id);
 
     if (result) {
       await model.deleteMany({ ticket: id });
 
-      response.json({ message: "The ticket has been successfully removed." });
+      response.json({ message: "تیکت مورد نظر با موفقیت حذف شد." });
     } else {
-      throw Object.assign(new Error("The ticket was not found."), { status: 404 });
+      throw Object.assign(new Error("تیکت مورد نظر پیدا نشد."), { status: 404 });
     }
   } catch (error) {
     next(error);
