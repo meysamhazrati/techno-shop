3d0c070b373c5f5924c78c3fa321fc4ba569e844
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 22:38:13 +0330

    Update brand validations

--- server/validators/brand.js
+++ server/validators/brand.js
@@ -1,14 +1,14 @@
 import { object, string } from "yup";
 
 const create = object({
-  name: string().required().min(3).max(50),
-  englishName: string().required().min(3).max(50),
-  logo: object().required(),
+  name: string().required("نام الزامی است.").min(3, "نام باید حداقل 3 کاراکتر باشد.").max(50, "نام باید حداکثر 50 کاراکتر باشد."),
+  englishName: string().required("نام انگلیسی الزامی است.").min(3, "نام انگلیسی باید حداقل 3 کاراکتر باشد.").max(50, "نام انگلیسی باید حداکثر 50 کاراکتر باشد."),
+  logo: object().required("لوگو الزامی است."),
 });
 
 const update = object({
-  name: string().required().min(3).max(50),
-  englishName: string().required().min(3).max(50),
+  name: string().required("نام الزامی است.").min(3, "نام باید حداقل 3 کاراکتر باشد.").max(50, "نام باید حداکثر 50 کاراکتر باشد."),
+  englishName: string().required("نام انگلیسی الزامی است.").min(3, "نام انگلیسی باید حداقل 3 کاراکتر باشد.").max(50, "نام انگلیسی باید حداکثر 50 کاراکتر باشد."),
 });
 
-export { create, update };
+export default { create, update };
