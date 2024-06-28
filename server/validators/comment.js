804a7ae96dd6efd6ee5c7cfd4056f417d5e64c5e
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:35:16 +0330

    Update comment validations

--- server/validators/comment.js
+++ server/validators/comment.js
@@ -1,18 +1,23 @@
 import { object, string, number, ValidationError } from "yup";
 
-const validator = object({
-  body: string().required().min(5).max(300),
-  score: number().required().min(1).max(5),
-  product: string(),
-  article: string(),
+const create = object({
+  body: string().required("متن الزامی است.").min(5, "متن باید حداقل 5 کاراکتر باشد.").max(300, "متن باید حداکثر 300 کاراکتر باشد."),
+  score: number().required("امتیاز الزامی است.").min(1, "امتیاز باید حداقل 1 باشد.").max(5, "امتیاز باید حداکثر 5 باشد."),
+  product: string().matches(/^[a-fA-F\d]{24}$/, "محصول نامعتبر است."),
+  article: string().matches(/^[a-fA-F\d]{24}$/, "مقاله نامعتبر است."),
 }).test(({ product, article }) => {
   if (!product && !article) {
-    throw new ValidationError("one of the product or article fields is required.");
+    throw new ValidationError("یکی از مقادیر محصول یا مقاله الزامی است.");
   } else if (product && article) {
-    throw new ValidationError("only one of the product or article fields is allowed.");
+    throw new ValidationError("فقط یکی از مقادیر محصول یا مقاله مجاز است.");
   } else {
     return true;
   }
 });
 
-export default validator;
+const update = object({
+  body: string().required("متن الزامی است.").min(5, "متن باید حداقل 5 کاراکتر باشد.").max(300, "متن باید حداکثر 300 کاراکتر باشد."),
+  score: number().required("امتیاز الزامی است.").min(1, "امتیاز باید حداقل 1 باشد.").max(5, "امتیاز باید حداکثر 5 باشد."),
+});
+
+export default { create, update };
