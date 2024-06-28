af461eb80dcedf71e04b225837e5a653ba5cd1fa
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 22:44:09 +0330

    Update category model

--- server/models/category.js
+++ server/models/category.js
@@ -1,36 +1,28 @@
 import { Schema, model } from "mongoose";
 
-import { create, update } from "../validators/category.js";
-
-const schema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    englishTitle: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    logo: {
-      type: String,
-      required: true,
-    },
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
   },
-  { timestamps: true }
-);
+  englishTitle: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  logo: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
 schema.virtual("products", {
   ref: "Product",
   localField: "_id",
   foreignField: "category",
 });
-
-schema.statics.createValidation = (body) => create.validate(body);
-schema.statics.updateValidation = (body) => update.validate(body);
 
 export default model("Category", schema);
