0f19c7a489d42ce7c37ce6b8bbe82e83a8de5c26
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:34:35 +0330

    Update comment model

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,46 +1,39 @@
 import { Schema, Types, model } from "mongoose";
 
-import validator from "../validators/comment.js";
-
-const schema = new Schema(
-  {
-    body: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 300,
-    },
-    score: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 5,
-    },
-    isBuyer: {
-      type: Boolean,
-      required: false,
-    },
-    isConfirmed: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    sender: {
-      type: Types.ObjectId,
-      ref: "User",
-    },
-    product: {
-      type: Types.ObjectId,
-      ref: "Product",
-    },
-    article: {
-      type: Types.ObjectId,
-      ref: "Article",
-    },
+const schema = new Schema({
+  body: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 300,
   },
-  { timestamps: true }
-);
-
-schema.statics.validation = (body) => validator.validate(body);
+  score: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  isBuyer: {
+    type: Boolean,
+    required: false,
+  },
+  isConfirmed: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  sender: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+  product: {
+    type: Types.ObjectId,
+    ref: "Product",
+  },
+  article: {
+    type: Types.ObjectId,
+    ref: "Article",
+  },
+}, { timestamps: true });
 
 export default model("Comment", schema);
