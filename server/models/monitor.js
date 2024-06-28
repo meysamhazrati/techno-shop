e27d025f76530f2371a25b295c364abf0b99eaae
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:08:59 +0330

    Update monitor model

--- server/models/monitor.js
+++ server/models/monitor.js
@@ -1,74 +1,68 @@
 import { Schema } from "mongoose";
 
 import productModel from "./product.js";
-import validator from "../validators/monitor.js";
 
-const schema = new Schema(
-  {
-    weight: {
-      type: Number,
-      required: true,
-      min: 1000,
-      max: 50000,
-    },
-    screen: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    screenSize: {
-      type: Number,
-      required: true,
-      min: 10,
-      max: 60,
-    },
-    resolutionWidth: {
-      type: Number,
-      required: true,
-      min: 500,
-      max: 10000,
-    },
-    resolutionHeight: {
-      type: Number,
-      required: true,
-      min: 500,
-      max: 10000,
-    },
-    minimumResponseTime: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 10,
-    },
-    maximumResponseTime: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 10,
-    },
-    panel: {
-      type: String,
-      required: true,
-      minLength: 2,
-      maxLength: 20,
-    },
-    backlight: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 20,
-    },
-    color: {
-      type: Number,
-      required: true,
-      min: 1000000,
-      max: 1000000000,
-    },
+const schema = new Schema({
+  weight: {
+    type: Number,
+    required: true,
+    min: 1000,
+    max: 50000,
   },
-  { timestamps: true }
-);
-
-schema.statics.validation = (body) => validator.validate(body);
+  screen: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  screenSize: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 60,
+  },
+  resolutionWidth: {
+    type: Number,
+    required: true,
+    min: 500,
+    max: 10000,
+  },
+  resolutionHeight: {
+    type: Number,
+    required: true,
+    min: 500,
+    max: 10000,
+  },
+  minimumResponseTime: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10,
+  },
+  maximumResponseTime: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10,
+  },
+  panel: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 20,
+  },
+  backlight: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+  },
+  color: {
+    type: Number,
+    required: true,
+    min: 1000000,
+    max: 1000000000,
+  },
+}, { timestamps: true });
 
 export default productModel.discriminator("Monitor", schema);
