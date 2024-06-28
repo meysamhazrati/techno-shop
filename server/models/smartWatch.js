b61cfd8b47e2871fe00e1b926dcea0837fdf02a6
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:20:58 +0330

    Update smartWatch model

--- server/models/smartWatch.js
+++ server/models/smartWatch.js
@@ -1,94 +1,88 @@
 import { Schema } from "mongoose";
 
 import productModel from "./product.js";
-import validator from "../validators/smartWatch.js";
 
-const schema = new Schema(
-  {
-    weight: {
-      type: Number,
-      required: true,
-      min: 30,
-      max: 500,
-    },
-    strapMaterial: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    chip: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 100,
-    },
-    operatingSystem: {
-      type: String,
-      required: true,
-      enum: ["WearOS", "WatchOS", "Tizen"],
-    },
-    operatingSystemVersion: {
-      type: Number,
-      required: true,
-      min: 3,
-      max: 10,
-    },
-    internalMemory: {
-      type: Number,
-      required: true,
-      enum: [16, 32, 64, 128, 256],
-    },
-    RAM: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 10,
-    },
-    screen: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    screenSize: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 4,
-    },
-    resolutionWidth: {
-      type: Number,
-      required: true,
-      min: 200,
-      max: 1000,
-    },
-    resolutionHeight: {
-      type: Number,
-      required: true,
-      min: 200,
-      max: 1000,
-    },
-    battery: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 50,
-    },
-    batteryCapacity: {
-      type: Number,
-      required: true,
-      min: 200,
-      max: 1000,
-    },
-    isWaterproof: {
-      type: Boolean,
-      required: true,
-    },
+const schema = new Schema({
+  weight: {
+    type: Number,
+    required: true,
+    min: 30,
+    max: 500,
   },
-  { timestamps: true }
-);
-
-schema.statics.validation = (body) => validator.validate(body);
+  strapMaterial: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  chip: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 100,
+  },
+  operatingSystem: {
+    type: String,
+    required: true,
+    enum: ["WearOS", "WatchOS", "Tizen"],
+  },
+  operatingSystemVersion: {
+    type: Number,
+    required: true,
+    min: 3,
+    max: 10,
+  },
+  internalMemory: {
+    type: Number,
+    required: true,
+    enum: [16, 32, 64, 128, 256],
+  },
+  RAM: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10,
+  },
+  screen: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  screenSize: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 4,
+  },
+  resolutionWidth: {
+    type: Number,
+    required: true,
+    min: 200,
+    max: 1000,
+  },
+  resolutionHeight: {
+    type: Number,
+    required: true,
+    min: 200,
+    max: 1000,
+  },
+  battery: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  batteryCapacity: {
+    type: Number,
+    required: true,
+    min: 200,
+    max: 1000,
+  },
+  isWaterproof: {
+    type: Boolean,
+    required: true,
+  },
+}, { timestamps: true });
 
 export default productModel.discriminator("SmartWatch", schema);
