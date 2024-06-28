e7a67510b4c9a6c21745a5cae423ac574f752a82
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 20:53:59 +0330

    Update error handling middleware

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -3,24 +3,24 @@ import { MulterError } from "multer";
 import { ValidationError } from "yup";
 
 const middleware = (error, request, response, next) => {
-  let { status = 500, message = "Unexpected error." } = error;
+  let { status = 500, message = "مشکلی پیش آمد! لطفا بعدا دوباره تلاش کنید." } = error;
 
   if (error instanceof jwt.JsonWebTokenError) {
     status = 401;
 
     if (error instanceof jwt.TokenExpiredError) {
-      message = "The token has expired.";
+      message = "توکن شما منقضی شده است.";
     } else {
-      message = "The token is invalid.";
+      message = "توکن شما نامعتبر شده است.";
     }
   } else if (error instanceof MulterError) {
     status = 400;
 
     if (error.code === "LIMIT_FILE_SIZE") {
       status = 413;
-      message = "The size of uploaded files exceeds the allowed limit.";
+      message = "حجم عکس‌های آپلود شده بیشتر از حد مجاز است.";
     } else if (error.code === "LIMIT_FILE_COUNT") {
-      message = "The number of uploaded files exceeds the allowed limit.";
+      message = "تعداد عکس‌های آپلود شده بیشتر از حد مجاز است.";
     }
   } else if (error instanceof ValidationError) {
     status = 400;
