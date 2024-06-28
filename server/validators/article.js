e6af970d723717f7c1863b04da056add75103c83
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 23:32:49 +0330

    Update article validations

--- server/validators/article.js
+++ server/validators/article.js
@@ -1,16 +1,17 @@
 import { object, string, boolean } from "yup";
 
 const create = object({
-  title: string().required().min(5).max(100),
-  cover: object().required(),
-  body: string().required().min(100).max(10000),
-  isPublished: boolean().required(),
-  category: string().required(),
+  title: string().required("عنوان الزامی است.").min(5, "عنوان باید حداقل 5 کاراکتر باشد.").max(100, "عنوان باید حداکثر 100 کاراکتر باشد."),
+  cover: object().required("کاور الزامی است."),
+  body: string().required("متن الزامی است.").min(100, "متن باید حداقل 100 کاراکتر باشد.").max(10000, "متن باید حداکثر 10000 کاراکتر باشد."),
+  isPublished: boolean().required("وضعیت انتشار الزامی است."),
+  category: string().required("دسته‌بندی‌ الزامی است.").matches(/^[a-fA-F\d]{24}$/, "دسته‌بندی‌ نامعتبر است."),
 });
 
 const update = object({
-  title: string().required().min(5).max(100),
-  body: string().required().min(100).max(10000),
+  title: string().required("عنوان الزامی است.").min(5, "عنوان باید حداقل 5 کاراکتر باشد.").max(100, "عنوان باید حداکثر 100 کاراکتر باشد."),
+  body: string().required("متن الزامی است.").min(100, "متن باید حداقل 100 کاراکتر باشد.").max(10000, "متن باید حداکثر 10000 کاراکتر باشد."),
+  category: string().required("دسته‌بندی‌ الزامی است.").matches(/^[a-fA-F\d]{24}$/, "دسته‌بندی‌ نامعتبر است."),
 });
 
-export { create, update };
+export default { create, update };
