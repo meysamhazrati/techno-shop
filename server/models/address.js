4d755e18338d0f197b0cca679d716ee37436c540
MeySaM <imeysamhazrati@gmail.com>
2024-06-28 22:29:57 +0330

    Update address model

--- server/models/address.js
+++ server/models/address.js
@@ -1,42 +1,35 @@
 import { Schema, Types, model } from "mongoose";
 
-import validator from "../validators/address.js";
-
-const schema = new Schema(
-  {
-    province: {
-      type: String,
-      required: true,
-      minLength: 2,
-      maxLength: 20,
-    },
-    city: {
-      type: String,
-      required: true,
-      minLength: 2,
-      maxLength: 20,
-    },
-    postalCode: {
-      type: String,
-      required: true,
-      minLength: 10,
-      maxLength: 10,
-    },
-    body: {
-      type: String,
-      required: true,
-      minLength: 10,
-      maxLength: 100,
-    },
-    recipient: {
-      type: Types.ObjectId,
-      ref: "User",
-    },
+const schema = new Schema({
+  province: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 20,
   },
-  { timestamps: true }
-);
-
-schema.statics.validation = (body) => validator.validate(body);
+  city: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 20,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 10,
+  },
+  body: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 100,
+  },
+  recipient: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+}, { timestamps: true });
 
 export { schema };
 export default model("Address", schema);
