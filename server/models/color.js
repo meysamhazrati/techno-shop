9a6303c5b280b74ea99178415d38be559f4c01f2
MeySaM <imeysamhazrati@gmail.com>
2024-06-30 14:06:27 +0330

    Update color model

--- server/models/color.js
+++ server/models/color.js
@@ -1,40 +1,37 @@
 import { Schema, Types, model } from "mongoose";
 
-const schema = new Schema(
-  {
-    price: {
-      type: Number,
-      required: true,
-      min: 1000,
-      max: 1000000000,
-    },
-    sales: {
-      type: Number,
-      default: 0,
-    },
-    inventory: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 100000,
-    },
-    name: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 15,
-    },
-    code: {
-      type: String,
-      required: true,
-    },
-    product: {
-      type: Types.ObjectId,
-      ref: "Product",
-    },
+const schema = new Schema({
+  price: {
+    type: Number,
+    required: true,
+    min: 1000,
+    max: 1000000000,
   },
-  { timestamps: true }
-);
+  sales: {
+    type: Number,
+    default: 0,
+  },
+  inventory: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 100000,
+  },
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 15,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  product: {
+    type: Types.ObjectId,
+    ref: "Product",
+  },
+}, { timestamps: true });
 
 export { schema };
 export default model("Color", schema);
