c433d4475601dd409c2493b73ff85a52fa1a5d3a
MeySaM <imeysamhazrati@gmail.com>
2024-06-30 14:07:12 +0330

    Update comment routes

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -3,13 +3,13 @@ import { Router } from "express";
 import authentication from "../middlewares/authentication.js";
 import isBanned from "../middlewares/isBanned.js";
 import isAdmin from "../middlewares/isAdmin.js";
-import { create, getAll, confirm, reject, remove } from "../controllers/comment.js";
+import { create, getAll, update, confirm, reject, remove } from "../controllers/comment.js";
 
 const router = Router();
 
 router.route("/").post(authentication, isBanned, create).get(authentication, isBanned, isAdmin, getAll);
+router.route("/:id").put(authentication, isBanned, update).delete(authentication, isBanned, remove);
 router.put("/confirm/:id", authentication, isBanned, isAdmin, confirm);
 router.put("/reject/:id", authentication, isBanned, isAdmin, reject);
-router.delete("/:id", authentication, isBanned, remove);
 
 export default router;
