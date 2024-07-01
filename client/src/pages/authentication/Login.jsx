c99bb6f8692fdfefcacb2a7de7f8271fb0b1ab03
MeySaM <imeysamhazrati@gmail.com>
2024-07-01 18:02:56 +0330

    Update Login page

--- client/src/pages/authentication/Login.jsx
+++ client/src/pages/authentication/Login.jsx
@@ -1,6 +1,5 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { ToastContext } from "../../contexts/Toast";
 import useLogin from "../../hooks/authentication/useLogin";
 import Loader from "../../components/Loader";
 
@@ -8,23 +7,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { openToast } = useContext(ToastContext);
-
   const { isPendingLogin, login } = useLogin();
-
-  const submit = (event) => {
-    event.preventDefault();
-
-    if (email.trim().length >= 10 && email.trim().length <= 100 && /^\w+([.-]?\w)*@\w+([.-]?\w)*\.[a-zA-Z]{2,4}$/.test(email.trim())) {
-      if (/^[\w?!$._-]{8,20}$/.test(password.trim())) {
-        login({ email: email.trim(), password: password.trim() });
-      } else {
-        openToast("error", null, "رمز عبور باید بین 8 تا 20 کاراکتر باشد.");
-      }
-    } else {
-      openToast("error", null, "ایمیل وارد شده نامعتبر است.");
-    }
-  };
 
   useEffect(() => {
     document.title = "تکنوشاپ - ورود";
@@ -36,7 +19,11 @@ const Login = () => {
         <h2 className="font-vazirmatn-bold text-2xl">ورود</h2>
         <span className="mt-3 block text-lg text-zinc-500">حساب کاربری ندارید؟ <Link to="/authentication/register" className="text-primary-900">ثبت نام کنید</Link></span>
       </div>
-      <form className="mt-6 flex flex-col gap-y-4 [&>*]:h-14" onSubmit={submit}>
+      <form className="mt-6 flex flex-col gap-y-4 [&>*]:h-14" onSubmit={(event) => {
+        event.preventDefault();
+
+        login({ email: email.trim(), password: password.trim() });
+      }}>
         <input
           type="text"
           inputMode="email"
