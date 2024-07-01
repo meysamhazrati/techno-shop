22b4f16eead4f69b48fd182245cb381c2e0281fc
MeySaM <imeysamhazrati@gmail.com>
2024-07-01 17:50:31 +0330

    Update Comments component

--- client/src/components/root/Comments.jsx
+++ client/src/components/root/Comments.jsx
@@ -1,5 +1,4 @@
-import { useState, useContext } from "react";
-import { ToastContext } from "../../contexts/Toast";
+import { useState } from "react";
 import useMe from "../../hooks/authentication/useMe";
 import useCreateComment from "../../hooks/comment/useCreateComment";
 import Section from "./Section";
@@ -14,29 +13,8 @@ const Comments = ({ isFetching, isError, hasNextPage, fetchNextPage, id, comment
   const [body, setBody] = useState("");
   const [score, setScore] = useState(5);
 
-  const { openToast } = useContext(ToastContext);
-
   const { isFetchingMe, isMeError } = useMe();
   const { isPendingCreateComment, createComment } = useCreateComment();
-
-  const submit = (event) => {
-    event.preventDefault();
-
-    if (isFetchingMe || isMeError) {
-      openToast("error", null, "لطفا ابتدا وارد حساب کاربری خود شوید.");
-    } else {
-      if (body.trim().length >= 5 && body.trim().length <= 300) {
-        createComment({ body: body.trim(), score, [submitFor]: id }, {
-          onSuccess: () => {
-            setBody("");
-            setScore(5);
-          },
-        });
-      } else {
-        openToast("error", null, "دیدگاه باید بین 5 تا 300 حروف باشد.");
-      }
-    }
-  };
 
   return !isError && (
     <Section>
@@ -51,14 +29,26 @@ const Comments = ({ isFetching, isError, hasNextPage, fetchNextPage, id, comment
             </div>
           )}
           <div className="flex items-center justify-center gap-x-2">
-            {Array(5).fill(0).map((star, index) => (
+            {Array(5).fill().map((star, index) => (
               <button key={index} className={score >= index + 1 ? "text-yellow-400" : "text-zinc-200"} onClick={() => setScore(index + 1)}>
                 <StarIcon className="size-8 transition-all" />
               </button>
             ))}
           </div>
-          <form onSubmit={submit}>
-            <textarea value={body} className="mt-4 max-h-96 min-h-52 w-full rounded-3xl border border-zinc-200 p-4 text-lg outline-none placeholder:text-zinc-400" placeholder="دیدگاه شما..." onInput={({ target }) => setBody(target.value)} />
+          <form onSubmit={(event) => {
+            event.preventDefault();
+
+            createComment({ body: body.trim(), score, [submitFor]: id }, { onSuccess: () => {
+              setBody("");
+              setScore(5);
+            } });
+          }}>
+            <textarea
+              value={body}
+              placeholder="دیدگاه شما..."
+              className="mt-4 max-h-96 min-h-52 w-full rounded-3xl border border-zinc-200 p-4 text-lg outline-none placeholder:text-zinc-400"
+              onInput={({ target }) => setBody(target.value)}
+            />
             <button type="submit" disabled={isFetchingMe || isMeError} className="mt-3 flex h-12 w-full items-center justify-center rounded-full bg-primary-900 text-lg text-white transition-colors enabled:hover:bg-primary-800 disabled:bg-primary-100">
               {isPendingCreateComment ? <Loader width={"40px"} height={"10px"} color={"#ffffff"} /> : "ثبت"}
             </button>
@@ -81,8 +71,8 @@ const Comments = ({ isFetching, isError, hasNextPage, fetchNextPage, id, comment
                             {comment.isBuyer && <span className="rounded-full bg-primary-50 px-3 py-0.5 text-sm text-primary-900">خریدار</span>}
                           </div>
                           <div className="mt-1 flex [&>*]:size-5">
-                            {Array(comment.score).fill(0).map((star, index) => <StarIcon key={index} className="text-yellow-400" />)}
-                            {Array(5 - comment.score).fill(0).map((star, index) => <StarIcon key={index} className="text-zinc-200" />)}
+                            {Array(comment.score).fill().map((star, index) => <StarIcon key={index} className="text-yellow-400" />)}
+                            {Array(5 - comment.score).fill().map((star, index) => <StarIcon key={index} className="text-zinc-200" />)}
                           </div>
                         </div>
                       </div>
@@ -91,7 +81,7 @@ const Comments = ({ isFetching, isError, hasNextPage, fetchNextPage, id, comment
                     <p className="mt-3 text-lg" dangerouslySetInnerHTML={{ __html: comment.body.replaceAll("\n", "<br />") }}></p>
                   </div>
                 ))}
-                {isFetching && Array(5).fill(0).map((comment, index) => (
+                {isFetching && Array(5).fill().map((comment, index) => (
                   <div key={index} className="animate-pulse py-4 first:pt-0 last:pb-0">
                     <div className="flex items-center justify-between gap-x-3">
                       <div className="flex items-center gap-x-3">
@@ -99,7 +89,7 @@ const Comments = ({ isFetching, isError, hasNextPage, fetchNextPage, id, comment
                         <div>
                           <div className="h-5 w-32 rounded-full bg-skeleton"></div>
                           <div className="mt-2 flex">
-                            {Array(5).fill(0).map((star, index) => <StarIcon key={index} className="size-5 text-skeleton" />)}
+                            {Array(5).fill().map((star, index) => <StarIcon key={index} className="size-5 text-skeleton" />)}
                           </div>
                         </div>
                       </div>
