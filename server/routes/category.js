a3495ee7238ecafd77c387b9eefc860839aa0381
MeySaM <imeysamhazrati@gmail.com>
2024-07-03 23:22:48 +0330

    Update category routes

--- server/routes/category.js
+++ server/routes/category.js
@@ -7,7 +7,7 @@ import isAdmin from "../middlewares/isAdmin.js";
 import { create, getAll, get, update, remove } from "../controllers/category.js";
 
 const router = Router();
-const uploader = multer("categories");
+const uploader = multer("categories", 2 * 1024 * 1024, 1, ["image/png", "image/jpg", "image/jpeg"]);
 
 router.route("/").post(authentication, isBanned, isAdmin, uploader.single("logo"), create).get(getAll);
 router.get("/:title", get);
