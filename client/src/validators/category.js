4f8d176ea1a0a3ace7c05feb397c1e9ccc1b9cc9
MeySaM <imeysamhazrati@gmail.com>
2024-07-03 23:26:14 +0330

    Update category validator

--- client/src/validators/category.js
+++ client/src/validators/category.js
@@ -1,14 +1,39 @@
-import { object, string } from "yup";
+import { object, string, mixed, ValidationError } from "yup";
 
 const create = object({
   title: string().required("عنوان الزامی است.").min(3, "عنوان باید حداقل 3 کاراکتر باشد.").max(50, "عنوان باید حداکثر 50 کاراکتر باشد."),
   englishTitle: string().required("عنوان انگلیسی الزامی است.").min(3, "عنوان انگلیسی باید حداقل 3 کاراکتر باشد.").max(50, "عنوان انگلیسی باید حداکثر 50 کاراکتر باشد."),
-  logo: object().required("لوگو الزامی است."),
+  logo: mixed().required("لوگو الزامی است.").test(({ size, type }) => {
+    if (size <= 2 * 1024 * 1024) {
+      if (["image/png", "image/jpg", "image/jpeg"].includes(type)) {
+        return true;
+      } else {
+        throw new ValidationError("فرمت لوگو باید PNG، JPG یا JPEG باشد.");
+      }
+    } else {
+      throw new ValidationError("حجم لوگو باید حداکثر 2 مگابایت باشد.");
+    }
+  }),
 });
 
 const update = object({
   title: string().required("عنوان الزامی است.").min(3, "عنوان باید حداقل 3 کاراکتر باشد.").max(50, "عنوان باید حداکثر 50 کاراکتر باشد."),
   englishTitle: string().required("عنوان انگلیسی الزامی است.").min(3, "عنوان انگلیسی باید حداقل 3 کاراکتر باشد.").max(50, "عنوان انگلیسی باید حداکثر 50 کاراکتر باشد."),
+  logo: mixed().nullable().test((value) => {
+    if (!value) {
+      return true;
+    } else {
+      if (value.size <= 2 * 1024 * 1024) {
+        if (["image/png", "image/jpg", "image/jpeg"].includes(value.type)) {
+          return true;
+        } else {
+          throw new ValidationError("فرمت لوگو باید PNG، JPG یا JPEG باشد.");
+        }
+      } else {
+        throw new ValidationError("حجم لوگو باید حداکثر 2 مگابایت باشد.");
+      }
+    }
+  }),
 });
 
 export default { create, update };
