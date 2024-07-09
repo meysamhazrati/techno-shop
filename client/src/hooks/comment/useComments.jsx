c49bf9dad8eb41911d952eb547437af7f7ef05ab
MeySaM <imeysamhazrati@gmail.com>
2024-07-09 18:31:46 +0330

    Create useComments hook

---
+++ client/src/hooks/comment/useComments.jsx
@@ -0,0 +1,16 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getAll } from "../../axios/controllers/comment";
+
+const useComments = (length) => {
+  const { isFetching, isError, data, hasNextPage, fetchNextPage } = useInfiniteQuery({
+    queryKey: ["comments"],
+    queryFn: ({ pageParam }) => getAll(pageParam, length),
+    initialPageParam: 1,
+    getNextPageParam: ({ data }) => data.nextPage,
+    select: ({ pages }) => ({ comments: pages.flatMap(({ data }) => data.comments), total: pages[pages.length - 1].data.total }),
+  });
+
+  return { isFetchingComments: isFetching, isCommentsError: isError, comments: data?.comments, total: data?.total, hasCommentsNextPage: hasNextPage, fetchCommentsNextPage: fetchNextPage };
+};
+
+export default useComments;
