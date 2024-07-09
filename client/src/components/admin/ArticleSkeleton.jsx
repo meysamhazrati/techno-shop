cdddc36b142c660c0464c7f1bbe7ac867938a3ff
MeySaM <imeysamhazrati@gmail.com>
2024-07-09 16:07:34 +0330

    Create admin/ArticleSkeleton component

---
+++ client/src/components/admin/ArticleSkeleton.jsx
@@ -0,0 +1,33 @@
+const ArticleSkeleton = () => {
+  return (
+    <tr className="border-t border-zinc-200 [&>*]:h-[120px] [&>*]:px-5">
+      <td>
+        <div className="h-24 w-40 animate-pulse rounded-3xl bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="flex items-center justify-center gap-x-2">
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+          <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default ArticleSkeleton;
