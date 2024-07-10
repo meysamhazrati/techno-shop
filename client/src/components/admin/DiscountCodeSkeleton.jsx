d75066fd9821c3124596b3ffc62f9be7a1e325b3
MeySaM <imeysamhazrati@gmail.com>
2024-07-10 23:08:02 +0330

    Create DiscountCodeSkeleton component

---
+++ client/src/components/admin/DiscountCodeSkeleton.jsx
@@ -0,0 +1,35 @@
+const DiscountCodeSkeleton = () => {
+  return (
+    <tr className="border-t border-zinc-200 [&>*]:h-[72px] [&>*]:px-5">
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+      <td>
+        <div className="h-9 w-24 animate-pulse rounded-full bg-skeleton"></div>
+      </td>
+    </tr>
+  );
+};
+
+export default DiscountCodeSkeleton;
