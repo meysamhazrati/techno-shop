0cf624516d37e2709858b6aea3e470a36536498a
MeySaM <imeysamhazrati@gmail.com>
2024-07-11 23:37:09 +0330

    Update admin/ProductSkeleton component

--- client/src/components/admin/ProductSkeleton.jsx
+++ client/src/components/admin/ProductSkeleton.jsx
@@ -2,7 +2,7 @@ const ProductSkeleton = () => {
   return (
     <tr className="border-t border-zinc-200 [&>*]:h-[120px] [&>*]:px-5">
       <td>
-        <div className="h-24 w-40 animate-pulse rounded-3xl bg-skeleton"></div>
+        <div className="mx-auto h-24 w-40 animate-pulse rounded-3xl bg-skeleton"></div>
       </td>
       <td>
         <div className="h-6 w-full animate-pulse rounded-full bg-skeleton"></div>
