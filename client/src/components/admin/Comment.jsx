6fb17ed71211a84e03a73f1c42666036af83a829
MeySaM <imeysamhazrati@gmail.com>
2024-07-11 23:49:54 +0330

    Update admin/Comment component

--- client/src/components/admin/Comment.jsx
+++ client/src/components/admin/Comment.jsx
@@ -13,7 +13,7 @@ import StarIcon from "../../icons/StarIcon";
 const Comment = ({ _id, body, score, isConfirmed, sender, product, article, createdAt }) => {
   const [newBody, setNewBody] = useState(body);
   const [newScore, setNewScore] = useState(score);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
   const [isRejectModalOpen, setIsRejectModalOpen] = useState(false);
   const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
@@ -31,9 +31,11 @@ const Comment = ({ _id, body, score, isConfirmed, sender, product, article, crea
       <tr className="border-t border-zinc-200 [&>*]:h-[72px] [&>*]:px-5">
         <td>{score} امتیاز</td>
         <td>{isConfirmed ? "تایید شده" : "رد شده"}</td>
-        <td>
-          <Link to={product ? `/admin/products/${product._id}` : `/admin/articles/${article._id}`} className="text-primary-900">{product ? product.title : article.title}</Link>
-        </td>
+        {(product?.title || article?.title) && (
+          <td>
+            <Link to={product ? `/admin/products/${product._id}` : `/admin/articles/${article._id}`} className="text-primary-900">{product ? product.title : article.title}</Link>
+          </td>
+        )}
         {sender.firstName && sender.lastName && (
           <td>
             <Link to={`/admin/users/${sender._id}`} className="text-primary-900">{sender.firstName} {sender.lastName}</Link>
@@ -42,21 +44,21 @@ const Comment = ({ _id, body, score, isConfirmed, sender, product, article, crea
         <td>{new Intl.DateTimeFormat("fa", { dateStyle: "medium" }).format(Date.parse(createdAt))}</td>
         <td>
           <div className="flex items-center justify-center gap-x-2 text-base">
-            <button className="flex h-9 w-24 items-center justify-center rounded-full bg-zinc-500 text-white transition-colors hover:bg-zinc-400" onClick={() => setIsEditModalOpen(true)}>ویرایش</button>
+            <button className="flex h-9 w-24 items-center justify-center rounded-full bg-zinc-500 text-white transition-colors hover:bg-zinc-400" onClick={() => setIsUpdateModalOpen(true)}>ویرایش</button>
             {isConfirmed ? <button className="flex h-9 w-24 items-center justify-center rounded-full bg-yellow-400 text-white transition-colors hover:bg-yellow-300" onClick={() => setIsRejectModalOpen(true)}>رد</button> : <button className="flex h-9 w-24 items-center justify-center rounded-full bg-yellow-400 text-white transition-colors hover:bg-yellow-300" onClick={() => setIsConfirmModalOpen(true)}>تایید</button>}
             <button className="flex h-9 w-24 items-center justify-center rounded-full bg-red-500 text-white transition-colors hover:bg-red-400" onClick={() => setIsRemoveModalOpen(true)}>حذف</button>
             <button className="flex h-9 w-24 items-center justify-center rounded-full bg-primary-900 text-white transition-colors hover:bg-primary-800" onClick={() => setIsCommentModalOpen(true)}>مشاهده</button>
           </div>
         </td>
       </tr>
-      <Modal isOpen={isEditModalOpen} onClose={() => setIsEditModalOpen(false)}>
+      <Modal isOpen={isUpdateModalOpen} onClose={() => setIsUpdateModalOpen(false)}>
         <h6 className="text-center font-vazirmatn-medium text-2xl">ویرایش دیدگاه</h6>
         <form className="mt-6 flex flex-col gap-y-3 text-lg xs:w-80 [&>*]:w-full" onSubmit={(event) => {
           event.preventDefault();
 
-          updateComment({ body: newBody.trim(), score: newScore }, { onSuccess: () => {
-            client.invalidateQueries([{ queryKey: ["comments"] }, { queryKey: ["users", { id: sender._id || sender }] }]);
-            setIsEditModalOpen(false);
+          updateComment({ body: newBody, score: newScore }, { onSuccess: () => {
+            client.invalidateQueries([{ queryKey: ["comments"] }, { queryKey: ["users", { id: sender._id || sender }] }, { queryKey: [product ? "products" : "articles", { id: product ? (product._id || product) : (article._id || article) }] }]);
+            setIsUpdateModalOpen(false);
           } });
         }}>
           <div className="flex items-center justify-center gap-x-2">
@@ -81,7 +83,7 @@ const Comment = ({ _id, body, score, isConfirmed, sender, product, article, crea
         <Modal isOpen={isRejectModalOpen} onClose={() => setIsRejectModalOpen(false)}>
           <Confirm title="این دیدگاه را رد می‌کنید؟" isPending={isPendingRejectComment} onCancel={() => setIsRejectModalOpen(false)} onConfirm={() =>
             rejectComment(null, { onSuccess: () => {
-              client.invalidateQueries({ queryKey: ["users", { id: sender._id || sender }] });
+              client.invalidateQueries([{ queryKey: ["users", { id: sender._id || sender }] }, { queryKey: [product ? "products" : "articles", { id: product ? (product._id || product) : (article._id || article) }] }]);
               setIsRejectModalOpen(false);
           } })} />
         </Modal>
@@ -89,7 +91,7 @@ const Comment = ({ _id, body, score, isConfirmed, sender, product, article, crea
         <Modal isOpen={isConfirmModalOpen} onClose={() => setIsConfirmModalOpen(false)}>
           <Confirm title="این دیدگاه را تایید می‌کنید؟" isPending={isPendingConfirmComment} onCancel={() => setIsConfirmModalOpen(false)} onConfirm={() =>
             confirmComment(null, { onSuccess: () => {
-              client.invalidateQueries({ queryKey: ["users", { id: sender._id || sender }] });
+              client.invalidateQueries([{ queryKey: ["users", { id: sender._id || sender }] }, { queryKey: [product ? "products" : "articles", { id: product ? (product._id || product) : (article._id || article) }] }]);
               setIsConfirmModalOpen(false);
           } })} />
         </Modal>
@@ -97,7 +99,7 @@ const Comment = ({ _id, body, score, isConfirmed, sender, product, article, crea
       <Modal isOpen={isRemoveModalOpen} onClose={() => setIsRemoveModalOpen(false)}>
         <Confirm title="این دیدگاه را حذف می‌کنید؟" isPending={isPendingRemoveComment} onCancel={() => setIsRemoveModalOpen(false)} onConfirm={() =>
           removeComment(null, { onSuccess: () => {
-            client.invalidateQueries([{ queryKey: ["comments"] }, { queryKey: ["users", { id: sender._id || sender }] }]);
+            client.invalidateQueries([{ queryKey: ["comments"] }, { queryKey: ["users", { id: sender._id || sender }] }, { queryKey: [product ? "products" : "articles", { id: product ? (product._id || product) : (article._id || article) }] }]);
             setIsRemoveModalOpen(false);
         } })} />
       </Modal>
