8311bfe40e91814d0c5cb016f6b2a70d9b2abf77
MeySaM <imeysamhazrati@gmail.com>
2024-07-11 23:19:14 +0330

    Update axios product controller

--- client/src/axios/controllers/product.js
+++ client/src/axios/controllers/product.js
@@ -4,7 +4,7 @@ const create = async (type, body) => await axios.postForm("/products", body, { p
 
 const getAll = async (search, brands, categories, price, onlyAvailable, onlyAmazing, sort, page, length) => await axios.get("/products", { params: { search, brands, categories, price, "only-available": onlyAvailable, "only-amazing": onlyAmazing, sort, page, length } });
 
-const get = async (id, commentsPage, commentsLength) => await axios.get(`/products/${id}`, { params: { "comments-page": commentsPage, "comments-length": commentsLength } });
+const get = async (id, onlyConfirmedComments, commentsPage, commentsLength) => await axios.get(`/products/${id}`, { params: { "only-confirmed-comments": onlyConfirmedComments, "comments-page": commentsPage, "comments-length": commentsLength } });
 
 const update = async (id, type, body) => await axios.put(`/products/${id}`, body, { params: { type } });
 
