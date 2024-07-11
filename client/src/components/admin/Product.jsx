7e04a9cbbda345fee36a8732404df81d727b8ff2
MeySaM <imeysamhazrati@gmail.com>
2024-07-11 23:38:15 +0330

    Update admin/Product component

--- client/src/components/admin/Product.jsx
+++ client/src/components/admin/Product.jsx
@@ -4,7 +4,7 @@ import useRemoveProduct from "../../hooks/product/useRemoveProduct";
 import Modal from "../Modal";
 import Confirm from "../Confirm";
 
-const Product = ({ _id, title, covers, score, brand, category }) => {
+const Product = ({ _id, title, covers, colors, score, brand, category, offer }) => {
   const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
 
   const { isPendingRemoveProduct, removeProduct } = useRemoveProduct(_id);
@@ -13,12 +13,14 @@ const Product = ({ _id, title, covers, score, brand, category }) => {
     <>
       <tr className="border-t border-zinc-200 [&>*]:h-[120px] [&>*]:px-5">
         <td>
-          <img src={`${process.env.SERVER_URI}/images/products/${covers[0]}`} alt="Product Cover" loading="lazy" className="h-24 w-40 rounded-3xl object-cover" />
+          <img src={`${process.env.SERVER_URI}/images/products/${covers[0]}`} alt={title} loading="lazy" className="mx-auto h-24 w-40 rounded-3xl object-cover" />
         </td>
         <td>{title}</td>
-        <td>{score}</td>
-        {brand && <td>{brand.name}</td>}
-        {category && <td>{category.title}</td>}
+        <td>{colors.reduce((previous, { sales }) => previous + sales, 0)} عدد</td>
+        <td>{score} ستاره</td>
+        <td>{brand.name}</td>
+        <td>{category.title}</td>
+        <td>{Date.parse(offer?.expiresAt) > Date.now() ? offer.title : "ندارد"}</td>
         <td>
           <div className="flex items-center justify-center gap-x-2 text-base">
             <button className="flex h-9 w-24 items-center justify-center rounded-full bg-red-500 text-white transition-colors hover:bg-red-400" onClick={() => setIsRemoveModalOpen(true)}>حذف</button>
