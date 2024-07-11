12ea27cfe8a2776704e581e720418006cd0c6546
MeySaM <imeysamhazrati@gmail.com>
2024-07-11 23:29:24 +0330

    Update User component

--- client/src/components/admin/User.jsx
+++ client/src/components/admin/User.jsx
@@ -18,7 +18,7 @@ const User = ({ _id, firstName, lastName, email, role, avatar, isBanned }) => {
   const [newPassword, setNewPassword] = useState("");
   const [newRole, setNewRole] = useState(role);
   const [newAvatar, setNewAvatar] = useState(null);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
   const [isBanModalOpen, setIsBanModalOpen] = useState(false);
   const [isUnBanModalOpen, setIsUnBanModalOpen] = useState(false);
   const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
@@ -35,7 +35,7 @@ const User = ({ _id, firstName, lastName, email, role, avatar, isBanned }) => {
     <>
       <tr className="border-t border-zinc-200 [&>*]:h-[72px] [&>*]:px-5">
         <td>
-          <UserAvatar user={{ firstName, lastName, avatar }} className="size-12 text-base" />
+          <UserAvatar user={{ firstName, lastName, avatar }} className="mx-auto size-12 text-base" />
         </td>
         <td>{firstName}</td>
         <td>{lastName}</td>
@@ -44,25 +44,25 @@ const User = ({ _id, firstName, lastName, email, role, avatar, isBanned }) => {
         <td>{isBanned ? "ممنوع" : "آزاد"}</td>
         <td>
           <div className="flex items-center justify-center gap-x-2 text-base">
-            <button className="flex h-9 w-24 items-center justify-center rounded-full bg-zinc-500 text-white transition-colors hover:bg-zinc-400" onClick={() => setIsEditModalOpen(true)}>ویرایش</button>
+            <button className="flex h-9 w-24 items-center justify-center rounded-full bg-zinc-500 text-white transition-colors hover:bg-zinc-400" onClick={() => setIsUpdateModalOpen(true)}>ویرایش</button>
             {isBanned ? <button className="flex h-9 w-24 items-center justify-center rounded-full bg-yellow-400 text-white transition-colors hover:bg-yellow-300" onClick={() => setIsUnBanModalOpen(true)}>آزاد</button> : <button className="flex h-9 w-24 items-center justify-center rounded-full bg-yellow-400 text-white transition-colors hover:bg-yellow-300" onClick={() => setIsBanModalOpen(true)}>ممنوع</button>}
             <button className="flex h-9 w-24 items-center justify-center rounded-full bg-red-500 text-white transition-colors hover:bg-red-400" onClick={() => setIsRemoveModalOpen(true)}>حذف</button>
             <Link to={`/admin/users/${_id}`} className="flex h-9 w-24 items-center justify-center rounded-full bg-primary-900 text-white transition-colors hover:bg-primary-800">مشاهده</Link>
           </div>
         </td>
       </tr>
-      <Modal isOpen={isEditModalOpen} onClose={() => setIsEditModalOpen(false)}>
+      <Modal isOpen={isUpdateModalOpen} onClose={() => setIsUpdateModalOpen(false)}>
         <h6 className="text-center font-vazirmatn-medium text-2xl">ویرایش کاربر</h6>
         <form className="mt-6 flex flex-col gap-y-3 text-lg xs:w-80 [&>*]:w-full" onSubmit={(event) => {
           event.preventDefault();
 
-          updateUser({ firstName: newFirstName.trim(), lastName: newLastName.trim(), email: newEmail.trim(), password: newPassword ? newPassword.trim() : undefined, role: newRole.trim(), avatar: newAvatar }, { onSuccess: () => {
-            setIsEditModalOpen(false);
+          updateUser({ firstName: newFirstName, lastName: newLastName, email: newEmail, password: newPassword || undefined, role: newRole, avatar: newAvatar }, { onSuccess: () => {
+            setIsUpdateModalOpen(false);
             setNewPassword("");
           } });
         }}>
           <div className="relative mx-auto !size-32 shrink-0">
-            {file.current?.files.length ? <img ref={image} alt="User Avatar" className="size-full rounded-full object-cover" /> : <UserAvatar user={{ firstName, lastName, avatar }} className="size-full text-3xl" />}
+            {file.current?.files.length ? <img ref={image} alt={`${firstName} ${lastName}`} className="size-full rounded-full object-cover" /> : <UserAvatar user={{ firstName, lastName, avatar }} className="size-full text-3xl" />}
             <input
               ref={file}
               type="file"
