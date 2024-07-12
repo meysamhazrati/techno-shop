7235b11eb238c2598057fb5b1f96cbf970976ed0
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 16:51:05 +0330

    Update ProductCover component

--- client/src/components/ProductCover.jsx
+++ client/src/components/ProductCover.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-const ProductCover = ({ id, covers }) => {
+const ProductCover = ({ id, title, covers }) => {
   return (
     <Link to={`/products/${id}`} className="relative block size-full [&>img]:absolute [&>img]:size-full [&>img]:object-cover">
-      <img src={`${process.env.SERVER_URI}/images/products/${covers[1]}`} alt="Product Cover" loading="lazy" />
-      <img src={`${process.env.SERVER_URI}/images/products/${covers[0]}`} alt="Product Cover" loading="lazy" className="transition-opacity duration-300 hover:opacity-0" />
+      <img src={`${process.env.SERVER_URI}/images/products/${covers[1]}`} alt={title} loading="lazy" />
+      <img src={`${process.env.SERVER_URI}/images/products/${covers[0]}`} alt={title} loading="lazy" className="transition-opacity duration-300 hover:opacity-0" />
     </Link>
   );
 };
