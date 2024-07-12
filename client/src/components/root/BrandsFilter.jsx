dab8c581599402ccbd7176970a768ce396d2c316
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 17:17:27 +0330

    Update BrandsFilter component

--- client/src/components/root/BrandsFilter.jsx
+++ client/src/components/root/BrandsFilter.jsx
@@ -20,7 +20,7 @@ const BrandsFilter = ({ setBrands }) => {
       <div className={`mt-4 ${isOpen ? "max-h-28 pb-3" : "max-h-0"} flex flex-col gap-y-1 overflow-auto px-3 transition-all`}>
         {isBrandsError ? (
           <span className="text-center text-lg">برندی پیدا نشد!</span>
-        ) : isFetchingBrands ? Array(4).fill(0).map((brand, index) => (
+        ) : isFetchingBrands ? Array(4).fill().map((brand, index) => (
           <div key={index} className="flex items-center gap-x-3">
             <input type="checkbox" className="size-4 shrink-0 cursor-pointer appearance-none rounded bg-zinc-200" />
             <div className="flex w-full items-center justify-between gap-x-3">
