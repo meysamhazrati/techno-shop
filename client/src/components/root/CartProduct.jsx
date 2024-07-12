c19e2d2a6a35b3e8d588b2099b8732765f6d1a18
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 17:08:14 +0330

    Update CartProduct component

--- client/src/components/root/CartProduct.jsx
+++ client/src/components/root/CartProduct.jsx
@@ -9,7 +9,7 @@ const CartProduct = ({ product, color }) => {
   return (
     <div className={`grid grid-cols-[150px_1fr] items-center gap-x-4 gap-y-2 overflow-auto py-4 first:pt-0 last:pb-0 ${color.inventory > 0 ? "opacity-100" : "opacity-50"}`}>
       <div className="relative h-32 w-full">
-        <ProductCover id={product._id} covers={product.covers} />
+        <ProductCover {...product} />
         {color.inventory > 0 && Date.parse(product.offer?.expiresAt) > Date.now() && <div className="absolute right-0 top-4 z-10 flex h-6 w-11 items-center justify-center rounded-full bg-primary-900 font-vazirmatn-medium text-sm text-white">{product.offer.percent}%</div>}
       </div>
       <div>
@@ -21,15 +21,17 @@ const CartProduct = ({ product, color }) => {
             <div className="mx-[1.7px] size-5 rounded-full border border-zinc-500" style={{ backgroundColor: color.code }}></div>
             <span>{color.name}</span>
           </div>
-          <div>
-            <ShieldIcon className="size-6 shrink-0" />
-            <span>گارانتی {product.warranty} ماهه</span>
-          </div>
+          {product.warranty > 0 && (
+            <div>
+              <ShieldIcon className="size-6 shrink-0" />
+              <span>گارانتی {product.warranty} ماهه</span>
+            </div>
+          )}
         </div>
       </div>
-      {color.inventory > 0 && Date.parse(product.offer?.expiresAt) > Date.now() && <AmazingOfferTimer width="full" fontSize="sm" expiresAt={product.offer.expiresAt} />}
+      {color.inventory > 0 && Date.parse(product.offer?.expiresAt) > Date.now() && <AmazingOfferTimer expiresAt={product.offer.expiresAt} width="100%" fontSize="14px" />}
       {color.inventory > 0 ? <ProductButton id={product._id} color={color} disabled={!color.inventory} /> : <button></button>}
-      <ProductPrice price={color.price} offer={product.offer} priceFontSize="lg" discountedPriceFontSize="sm" gapX="[2px]" iconSize="5" hasInventory={color.inventory} />
+      <ProductPrice price={color.price} offer={product.offer} hasInventory={color.inventory} priceFontSize="18px" discountedPriceFontSize="14px" columnGap="2px" iconSize="20px" />
     </div>
   );
 };
