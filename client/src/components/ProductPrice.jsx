7c72613e445920cbe355bdf65b56e044d06f1f30
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 16:50:40 +0330

    Update ProductPrice component

--- client/src/components/ProductPrice.jsx
+++ client/src/components/ProductPrice.jsx
@@ -1,16 +1,16 @@
 import TomanIcon from "../icons/TomanIcon";
 
-const ProductPrice = ({ price, offer, priceFontSize, discountedPriceFontSize, gapX, iconSize, hasInventory }) => {
+const ProductPrice = ({ price, offer, hasInventory, priceFontSize, discountedPriceFontSize, columnGap, iconSize }) => {
   return hasInventory ? (
     <div className="flex flex-col">
-      {Date.parse(offer?.expiresAt) > Date.now() && <span className={`text-${discountedPriceFontSize} text-zinc-400 line-through`}>{price.toLocaleString()}</span>}
-      <span className={`flex items-center gap-x-${gapX} font-vazirmatn-bold text-${priceFontSize}`}>
+      {Date.parse(offer?.expiresAt) > Date.now() && <span className="text-zinc-400 line-through" style={{ fontSize: discountedPriceFontSize }}>{price.toLocaleString()}</span>}
+      <span className="flex items-center font-vazirmatn-bold" style={{ fontSize: priceFontSize, columnGap }}>
         {Date.parse(offer?.expiresAt) > Date.now() ? (price - price * (offer.percent / 100)).toLocaleString() : price.toLocaleString()}
-        <TomanIcon className={`size-${iconSize}`} />
+        <TomanIcon className={iconSize === "16px" ? "size-4" : "size-5"} />
       </span>
     </div>
   ) : (
-    <span className={`text-${priceFontSize} text-red-700`}>ناموجود</span>
+    <span className="text-red-700" style={{ fontSize: priceFontSize }}>ناموجود</span>
   );
 };
 
