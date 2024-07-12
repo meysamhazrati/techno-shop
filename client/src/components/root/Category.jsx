a188016af1c1607e6512814ebd9a7518ab20cff4
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 17:12:20 +0330

    Update Category component

--- client/src/components/root/Category.jsx
+++ client/src/components/root/Category.jsx
@@ -4,7 +4,7 @@ const Category = ({ title, englishTitle, logo }) => {
   return (
     <div className="flex flex-col items-center gap-y-2">
       <Link to={`/categories/${englishTitle.toLowerCase().split(" ").join("-")}`} className="size-32 overflow-hidden rounded-full">
-        <img src={`${process.env.SERVER_URI}/images/categories/${logo}`} alt="Category Logo" loading="lazy" className="size-full object-cover" />
+        <img src={`${process.env.SERVER_URI}/images/categories/${logo}`} alt={title} loading="lazy" className="size-full object-cover" />
       </Link>
       <h1 className="font-vazirmatn-medium text-lg">
         <Link to={`/categories/${englishTitle.toLowerCase().split(" ").join("-")}`} className="line-clamp-1">{title}</Link>
