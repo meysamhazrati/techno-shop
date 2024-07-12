96f604b8fd55f7fe96cfad6da5645802d42778a3
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 17:10:16 +0330

    Update AmazingOfferProduct component

--- client/src/components/root/AmazingOfferProduct.jsx
+++ client/src/components/root/AmazingOfferProduct.jsx
@@ -7,7 +7,7 @@ const AmazingOfferProduct = ({ _id, covers, title, colors, offer }) => {
   return (
     <div className="min-w-56 bg-white">
       <div className="h-48 w-full">
-        <ProductCover id={_id} covers={covers} />
+        <ProductCover id={_id} title={title} covers={covers} />
         <div className="absolute right-4 top-4 z-10 flex h-7 w-12 items-center justify-center rounded-full bg-primary-900 font-vazirmatn-medium text-white">{offer.percent}%</div>
       </div>
       <div className="p-3">
@@ -16,9 +16,9 @@ const AmazingOfferProduct = ({ _id, covers, title, colors, offer }) => {
         </h3>
         <div className="flex items-center justify-between gap-x-3 border-t border-zinc-200 pt-2">
           <div className="flex flex-col items-center">
-            <AmazingOfferTimer width="32" fontSize="xs" expiresAt={offer.expiresAt} />
+            <AmazingOfferTimer expiresAt={offer.expiresAt} width="128px" fontSize="12px" />
           </div>
-          <ProductPrice price={colors[0].price} offer={offer} priceFontSize="base" discountedPriceFontSize="xs" gapX="px" iconSize="4" hasInventory={colors[0].inventory} />
+          <ProductPrice price={colors[0].price} offer={offer} hasInventory={colors[0].inventory} priceFontSize="16px" discountedPriceFontSize="12px" columnGap="1px" iconSize="16px" />
         </div>
       </div>
     </div>
