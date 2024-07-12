61038e3fcbf27c065f9833a0f880877bbda6aa51
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 16:40:20 +0330

    Update Admin page

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
 import useMe from "../../hooks/authentication/useMe";
 import SideBar from "../../components/admin/SideBar";
+import MobileNavigation from "../../components/MobileNavigation";
 
 const Admin = () => {
   const navigate = useNavigate();
@@ -13,12 +14,15 @@ const Admin = () => {
   }, [isPendingMe, isMeError, me, navigate]);
 
   return !isPendingMe && !isMeError && me.role === "ADMIN" && (
-    <main className="relative flex min-h-max flex-col items-start gap-4 p-4 xs:gap-8 xs:p-8 lg:flex-row">
-      <SideBar />
-      <section className="w-full overflow-hidden rounded-3xl bg-white p-6">
-        <Outlet />
-      </section>
-    </main>
+    <>
+      <main className="relative flex min-h-max flex-col items-start gap-4 px-4 pb-[72px] pt-4 xs:gap-8 xs:px-8 xs:pb-[104px] xs:pt-8 lg:flex-row">
+        <SideBar />
+        <section className="w-full overflow-hidden rounded-3xl bg-white p-6">
+          <Outlet />
+        </section>
+      </main>
+      <MobileNavigation />
+    </>
   );
 };
 
