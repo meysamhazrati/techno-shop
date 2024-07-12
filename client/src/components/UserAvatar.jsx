7f2abf5a38095bdc73387a598dffdca231f5303a
MeySaM <imeysamhazrati@gmail.com>
2024-07-12 16:39:25 +0330

    Update UserAvatar component

--- client/src/components/UserAvatar.jsx
+++ client/src/components/UserAvatar.jsx
@@ -1,7 +1,7 @@
 const UserAvatar = ({ user, className }) => {
   return (
     <div className={`flex shrink-0 items-center justify-center overflow-hidden rounded-full ${user.avatar ? "bg-white" : "bg-primary-900 text-white"} ${className || ""}`}>
-      {user.avatar ? <img src={`${process.env.SERVER_URI}/images/users/${user.avatar}`} alt="User Avatar" className="size-full object-cover" /> : <span>{user.firstName[0]} {user.lastName[0]}</span>}
+      {user.avatar ? <img src={`${process.env.SERVER_URI}/images/users/${user.avatar}`} alt={`${user.firstName} ${user.lastName}`} className="size-full object-cover" /> : <span>{user.firstName[0]} {user.lastName[0]}</span>}
     </div>
   );
 };
